a2f1ea343b2c3ed9af6206602f6a70fee06f6ccc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:09:01 +0530

    Add CodeChef MAJORELEM (Find the majority element Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/MAJORELEM_Find_the_majority_element_Practice_Problem_in_Intermediate_Arrays_and_2D_Arrays.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MAJORELEM
+ * Problem: MAJORELEM - Find the majority element Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/MAJORELEM
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+2
+6
+7 1 7 7 3 7
+3
+5 5 2
