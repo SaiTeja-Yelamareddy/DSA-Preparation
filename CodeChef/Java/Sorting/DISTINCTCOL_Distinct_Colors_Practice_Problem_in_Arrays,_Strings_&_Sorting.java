12dba043d868468fb002750903c707336873c30f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 19:45:24 +0530

    Add CodeChef DISTINCTCOL (Distinct Colors Practice Problem in Arrays, Strings & Sorting) solution

--- CodeChef/Java/Sorting/DISTINCTCOL_Distinct_Colors_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
+++ CodeChef/Java/Sorting/DISTINCTCOL_Distinct_Colors_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -8,4 +8,15 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
