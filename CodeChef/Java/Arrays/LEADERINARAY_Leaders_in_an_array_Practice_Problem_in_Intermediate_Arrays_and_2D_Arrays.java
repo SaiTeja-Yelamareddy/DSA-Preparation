f5b1f285523a306e8534c2eb39fa4390da64b9f6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:18:23 +0530

    Add CodeChef LEADERINARAY (Leaders in an array Practice Problem in Intermediate Arrays and 2D Arrays) solution

--- CodeChef/Java/Arrays/LEADERINARAY_Leaders_in_an_array_Practice_Problem_in_Intermediate_Arrays_and_2D_Arrays.java
+++ CodeChef/Java/Arrays/LEADERINARAY_Leaders_in_an_array_Practice_Problem_in_Intermediate_Arrays_and_2D_Arrays.java
@@ -8,4 +8,23 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+class Solution {
+    public List<Integer> findLeaders(int[] nums) {
+        // write your code here 
+        
+        List<Integer> list = new ArrayList<>();
+        int max = nums[nums.length - 1];
+        list.add(max);
+        for(int i = nums.length - 2; i >= 0; i--)
+        {
+            if(nums[i] > max)
+            {
+                list.add(nums[i]);
+                max = nums[i];
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
+
