6069491755ee2854664453e04d03881546d98e5f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 19:47:47 +0530

    Add CodeChef DISTINCTCOL (Distinct Colors Practice Problem in Arrays, Strings & Sorting) solution

--- CodeChef/Java/Sorting/DISTINCTCOL_Distinct_Colors_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
+++ CodeChef/Java/Sorting/DISTINCTCOL_Distinct_Colors_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -11,12 +11,29 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int max = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int x = sc.nextInt();
+                if(x > max)
+                {
+                    max = x;
+                }
+            }
+            System.out.println(max);
+        }
+    }
 }
