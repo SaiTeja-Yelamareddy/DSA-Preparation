612cac5a5e8416687d3d8fba3de6331f7b57f826
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:00:05 +0530

    Add CodeChef EQUALELE (Equal Elements Practice Problem in Arrays, Strings & Sorting) solution

--- CodeChef/Java/Sorting/EQUALELE_Equal_Elements_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
+++ CodeChef/Java/Sorting/EQUALELE_Equal_Elements_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -8,4 +8,31 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int[] nums=new int[n];
+		     HashMap<Integer, Integer> map = new HashMap<>();
+		     int max=0;
+		     for(int i=0;i<n;i++)
+		     {
+		         nums[i]=sc.nextInt();
+		         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+		         max=Math.max(max,map.get(nums[i]));
+		     }
+		     System.out.println(n-max);
+		}
+
+	}
+}
