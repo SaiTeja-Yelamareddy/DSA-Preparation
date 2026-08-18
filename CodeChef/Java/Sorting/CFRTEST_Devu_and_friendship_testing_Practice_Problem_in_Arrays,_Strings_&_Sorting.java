dab78d07d53cfe234ad629fda4e043f4fa209472
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:01:15 +0530

    Add CodeChef CFRTEST (Devu and friendship testing Practice Problem in Arrays, Strings & Sorting) solution

---
+++ CodeChef/Java/Sorting/CFRTEST_Devu_and_friendship_testing_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: CFRTEST
+ * Problem: CFRTEST - Devu and friendship testing Practice Problem in Arrays, Strings & Sorting
+ * Problem Link: https://www.codechef.com/practice/course/arrays-strings-sorting/INTARR01/problems/CFRTEST
+ * Language: Java
+ * Concept: Sorting
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
