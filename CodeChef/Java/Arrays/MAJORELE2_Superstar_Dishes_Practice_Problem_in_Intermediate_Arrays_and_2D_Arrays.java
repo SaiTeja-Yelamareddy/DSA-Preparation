fb150fe281c1e08e0804aef0289b1a47bf451bc0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:12:28 +0530

    Add CodeChef MAJORELE2 (Superstar Dishes Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/MAJORELE2_Superstar_Dishes_Practice_Problem_in_Intermediate_Arrays_and_2D_Arrays.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MAJORELE2
+ * Problem: MAJORELE2 - Superstar Dishes Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/MAJORELE2
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
