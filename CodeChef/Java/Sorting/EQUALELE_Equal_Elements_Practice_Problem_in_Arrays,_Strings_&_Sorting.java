59f4856151b2f0b2853370ef982d7f4700719900
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 19:50:07 +0530

    Add CodeChef EQUALELE (Equal Elements Practice Problem in Arrays, Strings & Sorting) solution

---
+++ CodeChef/Java/Sorting/EQUALELE_Equal_Elements_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: EQUALELE
+ * Problem: EQUALELE - Equal Elements Practice Problem in Arrays, Strings & Sorting
+ * Problem Link: https://www.codechef.com/practice/course/arrays-strings-sorting/INTARR01/problems/EQUALELE
+ * Language: Java
+ * Concept: Sorting
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
