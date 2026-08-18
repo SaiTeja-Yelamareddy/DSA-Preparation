4c7fe352e704a13a007237650fabfea31bb3c3af
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 20:06:10 +0530

    Add CodeChef CFRTEST (Devu and friendship testing Practice Problem in Arrays, Strings & Sorting) solution

--- CodeChef/Java/Sorting/CFRTEST_Devu_and_friendship_testing_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
+++ CodeChef/Java/Sorting/CFRTEST_Devu_and_friendship_testing_Practice_Problem_in_Arrays,_Strings_&_Sorting.java
@@ -8,4 +8,26 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for(int i = 0; i < n; i++)
+            {
+                int day = sc.nextInt();
+                map.put(day, map.getOrDefault(day, 0) + 1);
+            }
+            System.out.println(map.size());
+        }
+    }
+}
