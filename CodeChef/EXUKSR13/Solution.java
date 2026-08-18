6ac07b564a143cf1c167f700c9c48635a12f933a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-18 18:30:54 +0530

    Solve CodeChef EXUKSR13 (Worked Example - Employee Roles in OOPs using Java)

---
+++ CodeChef/EXUKSR13/Solution.java
@@ -0,0 +1,7 @@
+/*
+ * Problem: https://www.codechef.com/learn/course/oops-java/YXOJPP/problems/EXUKSR13
+ * Language: {platform}/{problemId}/{filename}.{ext}
+ * Saved via CodeSync Extension
+ */
+
+# cook your dish here
