45eb468bfeee4acd43cb0592b686a174e54db39e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-19 12:52:02 +0530

    Migrate CodeChef CFRTEST to canonical path

---
+++ CodeChef/Java/Arrays/CFRTEST_Devu_and_friendship_testing_,_Strings_&_Sorting.java
@@ -0,0 +1,34 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: CFRTEST
+ * Problem: Devu and friendship testing Practice Problem in Arrays, Strings & Sorting
+ * Problem Link: https://www.codechef.com/practice/course/arrays-strings-sorting/INTARR01/problems/CFRTEST
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for(int i = 0; i < n; i++)
+            {
+                int day = sc.nextInt();
+                map.put(day, map.getOrDefault(day, 0) + 1);
+            }
+            System.out.println(map.size());
+        }
+    }
+}
