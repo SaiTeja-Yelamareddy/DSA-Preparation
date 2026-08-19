c3dc09580fd4ee429e258d91d090553f46bd725d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-19 21:03:31 +0530

    Add CodeChef KITCHENCOST (Cost of Groceries Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/KITCHENCOST_Cost_of_Groceries.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KITCHENCOST
+ * Problem: Cost of Groceries Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays/ARRAYSPRO/problems/KITCHENCOST
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
