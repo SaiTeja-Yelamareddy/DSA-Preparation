323db2521b2e293428519526cd3a143930b14afe
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:11:17 +0530

    Add CodeChef MAXCOUNT (Count of Maximum Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/MAXCOUNT_Count_of_Maximum.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MAXCOUNT
+ * Problem: Count of Maximum Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/MAXCOUNT
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
