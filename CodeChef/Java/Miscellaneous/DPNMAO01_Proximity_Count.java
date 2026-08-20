d30c811fcfa008af4ad535140898aa87b2cc5221
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:28:16 +0530

    Add CodeChef DPNMAO01 (Proximity Count Practice Problem in Accenture Interview Questions) solution

---
+++ CodeChef/Java/Miscellaneous/DPNMAO01_Proximity_Count.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DPNMAO01
+ * Problem: Proximity Count Practice Problem in Accenture Interview Questions
+ * Problem Link: https://www.codechef.com/practice/course/accenture-interview-questions/ACCNTRE05/problems/DPNMAO01
+ * Language: Java
+ * Concept: Miscellaneous
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
