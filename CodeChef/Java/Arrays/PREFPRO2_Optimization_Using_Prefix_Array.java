8f34388b735cc4518fd04ac8960696515903e19d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:58:31 +0530

    Add CodeChef PREFPRO2 (Optimization Using Prefix Array Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/PREFPRO2_Optimization_Using_Prefix_Array.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREFPRO2
+ * Problem: Optimization Using Prefix Array Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP03/problems/PREFPRO2
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+5
+1 2 3 4 5 
+5
+1 2
+2 3
+2 4
+4 4
+1 5
