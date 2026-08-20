8511d6034e2ef3543eb4cd92af23c680c9a02369
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:33:08 +0530

    Add CodeChef DPNMAO01 (Proximity Count Practice Problem in Accenture Interview Questions) solution

--- CodeChef/Java/Miscellaneous/DPNMAO01_Proximity_Count.java
+++ CodeChef/Java/Miscellaneous/DPNMAO01_Proximity_Count.java
@@ -8,4 +8,42 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        if (length <= 0) {
+            int num = sc.nextInt();
+            int diff = sc.nextInt();
+            System.out.println(-1);
+            sc.close();
+            return;
+        }
+
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int num = sc.nextInt();
+        int diff = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (Math.abs(arr[i] - num) <= diff) {
+                count++;
+            }
+        }
+
+        if (count == 0) System.out.println(-1);
+        else System.out.println(count);
+
+        sc.close();
+    }
+}
