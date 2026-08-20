51f1b09f5f6f40ea6de6eb3f166a4ddd76f74d07
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:23:33 +0530

    Add CodeChef SINGLENUM (Single number in multiple numbers Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/SINGLENUM_Single_number_in_multiple_numbers.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: SINGLENUM
+ * Problem: Single number in multiple numbers Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/SINGLENUM
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
