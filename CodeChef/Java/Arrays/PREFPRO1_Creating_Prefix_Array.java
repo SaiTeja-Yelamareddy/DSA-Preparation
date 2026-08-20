7cede3546e8d185e2d885e2a026f70883345025f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:04:04 +0530

    Add CodeChef PREFPRO1 (Creating Prefix Array Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/PREFPRO1_Creating_Prefix_Array.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREFPRO1
+ * Problem: Creating Prefix Array Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP03/problems/PREFPRO1
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
