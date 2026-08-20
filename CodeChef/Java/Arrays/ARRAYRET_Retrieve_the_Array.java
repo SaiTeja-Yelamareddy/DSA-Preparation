3f69cd03dd98eb579ba4d12a219af185e734d614
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:37:17 +0530

    Add CodeChef ARRAYRET (Retrieve the Array Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Arrays/ARRAYRET_Retrieve_the_Array.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ARRAYRET
+ * Problem: Retrieve the Array Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ARRAYRET
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
