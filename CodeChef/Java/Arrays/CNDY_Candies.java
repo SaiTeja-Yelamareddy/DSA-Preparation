479dd97a424ae72bae6ad017ece1e3b5763c9a49
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:37:19 +0530

    Add CodeChef CNDY (Candies Practice Problem in Arrays) solution

--- CodeChef/Java/Arrays/CNDY_Candies.java
+++ CodeChef/Java/Arrays/CNDY_Candies.java
@@ -8,4 +8,46 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            int[] A = new int[2 * N];
+            for(int i = 0; i < 2 * N; i++) {
+                A[i] = sc.nextInt();
+            }
+            HashMap<Integer, Integer> map1 = new HashMap<>();
+            HashMap<Integer, Integer> map2 = new HashMap<>();
+            boolean possible = true;
+            for(int i = 0; i < 2 * N; i++) {
+                int value = A[i];
+                if(!map1.containsKey(value)) {
+                    map1.put(value, 1);
+                }
+                else if(!map2.containsKey(value)) {
+                    map2.put(value, 1);
+                }
+                else {
+                    possible = false;
+                    break;
+                }
+            }
+            if(possible) {
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("No");
+            }
+        }
+
+	}
+}
