796177fe6b9ee59feec4ec67a27fd6d387af75d0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:41:36 +0530

    Add CodeChef MAXCOUNT (Count of Maximum Practice Problem in Arrays) solution

--- CodeChef/Java/Arrays/MAXCOUNT_Count_of_Maximum.java
+++ CodeChef/Java/Arrays/MAXCOUNT_Count_of_Maximum.java
@@ -8,4 +8,26 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+class Solution {
+    public int[] mostFrequent(int N, int[] A) {
+        //write your code  here
+         HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < N; i++) {
+
+            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
+        }
+        int max = 0;
+        int answer = Integer.MAX_VALUE;
+        for(int value : map.keySet()) {
+            int count = map.get(value);
+            if(count > max) {
+                max = count;
+                answer = value;
+            }
+            else if(count == max && value < answer) {
+                answer = value;
+            }
+        }
+        return new int[]{answer, max};
+    }
+}
