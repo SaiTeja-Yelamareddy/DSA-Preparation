c747495e3f722b68704b96f15fcf17e67bfea06f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:32:27 +0530

    Add CodeChef CNDY (Candies Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/CNDY_Candies.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: CNDY
+ * Problem: Candies Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/CNDY
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
