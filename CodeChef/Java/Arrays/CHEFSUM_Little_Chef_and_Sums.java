afaf97f4fb5b7763fe5445c6a3922e83c7f3ea51
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:56:11 +0530

    Add CodeChef CHEFSUM (Little Chef and Sums Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/CHEFSUM_Little_Chef_and_Sums.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: CHEFSUM
+ * Problem: Little Chef and Sums Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP03/problems/CHEFSUM
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
