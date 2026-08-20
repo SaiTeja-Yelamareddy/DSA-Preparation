469f48cddf4b2acb9e17eae7cc07c7314a80ecf0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:26:27 +0530

    Add CodeChef MISSINPERM (Missing number in permutation Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/MISSINPERM_Missing_number_in_permutation.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MISSINPERM
+ * Problem: Missing number in permutation Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/MISSINPERM
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+3
+2
+2 0
+4
+4 2 1 0
+3
+1 2 3
