25b963707737b97998dfcf4a1190ba0270e6931c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:22:42 +0530

    Add CodeChef PREP01 (Array - Pascals or Khayyams triangle Practice Problem in Arrays) solution

--- CodeChef/Java/Arrays/PREP01_Array_-_Pascals_or_Khayyams_triangle.java
+++ CodeChef/Java/Arrays/PREP01_Array_-_Pascals_or_Khayyams_triangle.java
@@ -8,4 +8,32 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            long[] row = new long[N];
+            row[0] = 1;
+            for(int i = 1; i < N; i++) {
+                row[i] = 1;
+                for(int j = i - 1; j > 0; j--) {
+                    row[j] = row[j] + row[j - 1];
+                }
+            }
+            for(int i = 0; i < N; i++) {
+                System.out.print(row[i] + " ");
+            }
+            System.out.println();
+        }
+	}
+}
