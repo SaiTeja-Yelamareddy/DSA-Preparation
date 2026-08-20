18582555c4a9d9918187a731f8c069d3b0249951
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 19:58:07 +0530

    Add CodeChef CHEFSUM (Little Chef and Sums Practice Problem in Arrays) solution

--- CodeChef/Java/Arrays/CHEFSUM_Little_Chef_and_Sums.java
+++ CodeChef/Java/Arrays/CHEFSUM_Little_Chef_and_Sums.java
@@ -8,4 +8,29 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CodeChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            int[] A = new int[N];
+            for(int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+            int min = A[0];
+            int answer = 1;
+            for(int i = 1; i < N; i++) {
+                if(A[i] < min) {
+                    min = A[i];
+                    answer = i + 1;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
