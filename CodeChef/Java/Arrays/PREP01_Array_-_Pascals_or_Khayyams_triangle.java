69641fd4eb2d08369c33811e56faeea91599df4c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-20 20:09:02 +0530

    Add CodeChef PREP01 (Array - Pascals or Khayyams triangle Practice Problem in Arrays) solution

---
+++ CodeChef/Java/Arrays/PREP01_Array_-_Pascals_or_Khayyams_triangle.java
@@ -0,0 +1,11 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREP01
+ * Problem: Array - Pascals or Khayyams triangle Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/PREP01
+ * Language: Java
+ * Concept: Arrays
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
