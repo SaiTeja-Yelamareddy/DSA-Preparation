41b59267213b095db62d4da15e2c719013803487
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 11:00:26 +0530

    Migrate CodeChef MISSINPERM to canonical path

---
+++ CodeChef/Java/Arrays/Medium/MISSINPERM_Missing_number_in_permutation.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MISSINPERM
+ * Problem: Missing number in permutation Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-new/ARRAYSP02/problems/MISSINPERM
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        // write your code here
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        return (n*(n+1))/2-sum;
+    }
+}
+
