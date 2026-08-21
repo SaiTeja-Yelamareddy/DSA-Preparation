eb59a64eb140059fe5f57bba1b49908fd3c64099
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 18:46:36 +0530

    Add CodeChef ANUUND (Ups and Downs Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/ANUUND_Ups_and_Downs.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ANUUND
+ * Problem: Ups and Downs Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ANUUND
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
