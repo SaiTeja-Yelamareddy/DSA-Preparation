b7f0c1589424d5f71eb14d7f2461e8200aa19a98
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 19:02:46 +0530

    Add GeeksforGeeks pyramid-patterns (Pyramid Pattern) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/pyramid-patterns_Pyramid_Pattern.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/pyramid-patterns_Pyramid_Pattern.java
@@ -22,6 +22,7 @@ class GFG {
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
+        }
             System.out.println();
         sc.close();
     }
