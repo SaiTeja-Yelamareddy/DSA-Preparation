7fa4cdbe0e88e4ce4a378fc33e5bd0173ed6c7d3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 11:00:41 +0530

    Migrate CodeChef ARRAYRET to canonical path

---
+++ CodeChef/Java/Arrays/Medium/ARRAYRET_Retrieve_the_Array.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ARRAYRET
+ * Problem: Retrieve the Array Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ARRAYRET
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
