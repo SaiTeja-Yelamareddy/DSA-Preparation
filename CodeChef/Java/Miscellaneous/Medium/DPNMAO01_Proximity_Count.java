d0ccc240349c913f90f1631e61e0d03b70c1d64a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 11:00:44 +0530

    Migrate CodeChef DPNMAO01 to canonical path

---
+++ CodeChef/Java/Miscellaneous/Medium/DPNMAO01_Proximity_Count.java
@@ -0,0 +1,50 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DPNMAO01
+ * Problem: Proximity Count Practice Problem in Accenture Interview Questions
+ * Problem Link: https://www.codechef.com/practice/course/accenture-interview-questions/ACCNTRE05/problems/DPNMAO01
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        if (length <= 0) {
+            int num = sc.nextInt();
+            int diff = sc.nextInt();
+            System.out.println(-1);
+            sc.close();
+            return;
+        }
+
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int num = sc.nextInt();
+        int diff = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (Math.abs(arr[i] - num) <= diff) {
+                count++;
+            }
+        }
+
+        if (count == 0) System.out.println(-1);
+        else System.out.println(count);
+
+        sc.close();
+    }
+}
