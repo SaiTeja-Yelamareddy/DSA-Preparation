583583e1943014904cf7bd94bb9aebade206e91c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-21 11:00:46 +0530

    Migrate CodeChef KITCHENCOST to canonical path

---
+++ CodeChef/Java/Arrays/Medium/KITCHENCOST_Cost_of_Groceries.java
@@ -0,0 +1,25 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KITCHENCOST
+ * Problem: Cost of Groceries Practice Problem in Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays/ARRAYSPRO/problems/KITCHENCOST
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public int compute(int n, int x, int[] a, int[] b) {
+        // write your code here 
+            int total = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(a[i] >= x)
+                {
+                    total = total + b[i];
+                }
+            }
+           return total;
+    }
+}
