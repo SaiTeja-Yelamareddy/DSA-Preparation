8cf9832248d4e87c5fffb6d81fd8ae8878adcbb1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-08-31 11:01:28 +0530

    Sync CodeChef GITPROB57 solution

---
+++ CodeChef/Java/Miscellaneous/Medium/GITPROB57_Handling_Merge_Conflicts_in_Git_in_GitGithub.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: GITPROB57
+ * Problem: Handling Merge Conflicts in Git in Git/Github
+ * Problem Link: https://www.codechef.com/learn/course/git-github/LOCALGIT/problems/GITPROB57
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+// Source code
