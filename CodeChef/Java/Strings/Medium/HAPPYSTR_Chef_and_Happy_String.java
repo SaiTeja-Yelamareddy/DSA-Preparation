de2992638fc2c6f418aae261de61c194c26ea754
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:57:11 +0530

    Add CodeChef HAPPYSTR (Chef and Happy String Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/HAPPYSTR_Chef_and_Happy_String.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: HAPPYSTR
+ * Problem: Chef and Happy String Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings/STRINGS/problems/HAPPYSTR
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
