e0ae54364f76473dc7a0bb4478b5c883e3e4e215
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:34:26 +0530

    Add GeeksforGeeks pangram-checking-1587115620 (Panagram Checking) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/pangram-checking-1587115620_Panagram_Checking.java
@@ -0,0 +1,32 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: pangram-checking-1587115620
+ * Problem: Panagram Checking
+ * Problem Link: https://www.geeksforgeeks.org/problems/pangram-checking-1587115620/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public static boolean checkPangram(String s) {
+        // code here
+        if(s.length()<26)
+                return false;
+                int[] freq=new int[26];
+                for(int i=0;i<s.length();i++)
+                {
+                    char ch=s.charAt(i);
+                    freq[ch-'a']++;
+                }
+                for(int i=0;i<26;i++)
+                {
+                    if(freq[i]==0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+    }
+}
