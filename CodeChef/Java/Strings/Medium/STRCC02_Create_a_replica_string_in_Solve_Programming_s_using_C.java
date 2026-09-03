e44912cff6b495257723874c1b9d50d9785aaab1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 20:06:00 +0530

    Add CodeChef STRCC02 (Create a replica string in Solve Programming problems using C) solution

--- CodeChef/Java/Strings/Medium/STRCC02_Create_a_replica_string_in_Solve_Programming_s_using_C.java
+++ CodeChef/Java/Strings/Medium/STRCC02_Create_a_replica_string_in_Solve_Programming_s_using_C.java
@@ -11,4 +11,35 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+// Update the code below to solve this problem
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+  int t;
+  scanf("%d", &t); 
+  while(t--)
+  {
+   char S[100], A[100], B[100];
+   scanf("%s", S);
+// Write the code below this line to generate String A
+
+
+// Write the code below this line to generate String A
+  
+
+  for(int i=0; i<strlen(S);i++)
+       {
+         printf("%c",A[i]);
+       }
+   printf("\n");
+   for(int i=0; i<strlen(S);i++)
+       {
+         printf("%c",B[i]);
+       }
+   printf("\n");
+  }
+ return 0;
+}
