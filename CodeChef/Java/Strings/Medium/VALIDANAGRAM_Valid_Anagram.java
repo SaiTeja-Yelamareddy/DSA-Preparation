3d052def6716318efda29625a92099e8f3fb854d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:18:00 +0530

    Add CodeChef VALIDANAGRAM (Valid Anagram Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/VALIDANAGRAM_Valid_Anagram.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VALIDANAGRAM
+ * Problem: Valid Anagram Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings/STRINGS/problems/VALIDANAGRAM
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
