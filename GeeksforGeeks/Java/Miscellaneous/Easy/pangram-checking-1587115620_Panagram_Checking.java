291c6c4221c88c5266f00560310f11ae7748ec6d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:37:28 +0530

    Add GeeksforGeeks pangram-checking-1587115620 (Panagram Checking) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/pangram-checking-1587115620_Panagram_Checking.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/pangram-checking-1587115620_Panagram_Checking.java
@@ -19,6 +19,7 @@ class Solution {
                 for(int i=0;i<s.length();i++)
                 {
                     char ch=s.charAt(i);
+                    if(ch >= 'a' && ch <= 'z')
                     freq[ch-'a']++;
                 }
                 for(int i=0;i<26;i++)
