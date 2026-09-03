58f15c35080356ecadabb42a1fb11932e4622e92
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:56:03 +0530

    Add CodeChef DPNMAO24 (Last Word Length Practice Problem in Accenture Interview Questions) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/DPNMAO24_Last_Word_Length.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DPNMAO24
+ * Problem: Last Word Length Practice Problem in Accenture Interview Questions
+ * Problem Link: https://www.codechef.com/practice/course/accenture-interview-questions/ACCNTRE05/problems/DPNMAO24
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
