14ad9538838c075089f8488b58524b8c8d4c73a9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:16:46 +0530

    Add CodeChef ANAGRAMP (Check if a string is anagram of another Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/ANAGRAMP_Check_if_a_string_is_anagram_of_another.java
+++ CodeChef/Java/Strings/Medium/ANAGRAMP_Check_if_a_string_is_anagram_of_another.java
@@ -9,4 +9,15 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
