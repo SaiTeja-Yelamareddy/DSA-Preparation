0a4fdde220a8d2a1fa0a7be6988bfe949d2dc9d0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:24:35 +0530

    Add CodeChef DPNMAO02 (Anagram Check Practice Problem in Accenture Interview Questions) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/DPNMAO02_Anagram_Check.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DPNMAO02
+ * Problem: Anagram Check Practice Problem in Accenture Interview Questions
+ * Problem Link: https://www.codechef.com/practice/course/accenture-interview-questions/ACCNTRE05/problems/DPNMAO02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
