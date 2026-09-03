e435d0c148f44b849d5fe52d12db0a4cf69079eb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:25:14 +0530

    Add CodeChef DPNMAO02 (Anagram Check Practice Problem in Accenture Interview Questions) solution

--- CodeChef/Java/Miscellaneous/Medium/DPNMAO02_Anagram_Check.java
+++ CodeChef/Java/Miscellaneous/Medium/DPNMAO02_Anagram_Check.java
@@ -9,4 +9,30 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        sc.close();
+
+        if (isAnagram(s, t)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
