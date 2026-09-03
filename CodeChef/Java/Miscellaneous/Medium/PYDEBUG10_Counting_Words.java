16609965791912338a9da7fa9db5e7aab1000bc4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:55:47 +0530

    Add CodeChef PYDEBUG10 (Counting Words Practice Problem in C) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PYDEBUG10_Counting_Words.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PYDEBUG10
+ * Problem: Counting Words Practice Problem in C
+ * Problem Link: https://www.codechef.com/practice/course/c/PPY10/problems/PYDEBUG10
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+4
+1 1
+4 2
+2 4
+95 42
