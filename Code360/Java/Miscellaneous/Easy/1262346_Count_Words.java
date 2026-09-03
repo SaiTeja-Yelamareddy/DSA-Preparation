7cc122ea9029b30df72b795d9876c8cd69a67021
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 19:59:59 +0530

    Add Code360 1262346 (Count Words) solution

---
+++ Code360/Java/Miscellaneous/Easy/1262346_Count_Words.java
@@ -0,0 +1,28 @@
+/*
+ * Platform: Code360
+ * Problem ID: 1262346
+ * Problem: Count Words
+ * Problem Link: https://www.naukri.com/code360/problems/count-words_1262346
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+import java.util.Scanner;
+class Solution {
+	
+    static int  countWords(String input) {
+        // Write your code here
+    }
+    
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int output = countWords(input);
+        System.out.println(output);
+		
+	}
+}
