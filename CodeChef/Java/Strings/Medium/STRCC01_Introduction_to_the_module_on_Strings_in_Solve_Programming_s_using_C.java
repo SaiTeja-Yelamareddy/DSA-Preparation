da020ae4b9ec4a4db147a538f18bed01cf0f4630
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 20:05:38 +0530

    Add CodeChef STRCC01 (Introduction to the module on Strings in Solve Programming problems using C) solution

---
+++ CodeChef/Java/Strings/Medium/STRCC01_Introduction_to_the_module_on_Strings_in_Solve_Programming_s_using_C.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRCC01
+ * Problem: Introduction to the module on Strings in Solve Programming problems using C
+ * Problem Link: https://www.codechef.com/learn/course/c-beginner/BC01BC21/problems/STRCC01
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: C Beginner
+ * Module: BC01BC21
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
