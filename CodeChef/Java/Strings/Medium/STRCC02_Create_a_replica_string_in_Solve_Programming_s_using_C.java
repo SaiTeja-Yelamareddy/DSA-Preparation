b6b4a46194ab54bb3f93dab6f2d08b637191c11d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-03 20:05:58 +0530

    Add CodeChef STRCC02 (Create a replica string in Solve Programming problems using C) solution

---
+++ CodeChef/Java/Strings/Medium/STRCC02_Create_a_replica_string_in_Solve_Programming_s_using_C.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRCC02
+ * Problem: Create a replica string in Solve Programming problems using C
+ * Problem Link: https://www.codechef.com/learn/course/c-beginner/BC01BC21/problems/STRCC02
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: C Beginner
+ * Module: BC01BC21
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
