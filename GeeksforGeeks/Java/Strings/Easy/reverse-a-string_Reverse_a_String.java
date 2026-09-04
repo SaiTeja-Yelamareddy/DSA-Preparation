1c2dbe36ab70da4173336c0c59147ba4c6fc9af0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-04 17:36:38 +0530

    Add GeeksforGeeks reverse-a-string (Reverse a String) solution

--- GeeksforGeeks/Java/Strings/Easy/reverse-a-string_Reverse_a_String.java
+++ GeeksforGeeks/Java/Strings/Easy/reverse-a-string_Reverse_a_String.java
@@ -13,7 +13,7 @@ class Solution {
     public static String reverseString(String s) {
         // code here
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++)
+        for(int i=s.length()-1;i>=0;i--)
         {
             char ch=s.charAt(i);
             sb.append(ch);
