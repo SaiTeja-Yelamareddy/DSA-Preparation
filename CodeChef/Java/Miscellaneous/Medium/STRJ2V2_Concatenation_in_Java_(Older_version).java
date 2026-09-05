5c037588e531df4325e1a39515001ade0da49ec4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:12:59 +0530

    Add CodeChef STRJ2V2 (Concatenation in Java (Older version)) solution

--- CodeChef/Java/Miscellaneous/Medium/STRJ2V2_Concatenation_in_Java_(Older_version).java
+++ CodeChef/Java/Miscellaneous/Medium/STRJ2V2_Concatenation_in_Java_(Older_version).java
@@ -11,4 +11,13 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Solution as follows
+		String x = "Hello";
+        String y = "World";
+        System.out.print(x + " " + y);
+	}
+}
