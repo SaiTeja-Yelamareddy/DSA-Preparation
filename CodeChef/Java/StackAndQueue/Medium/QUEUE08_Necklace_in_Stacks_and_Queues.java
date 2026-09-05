87303581f8eb796f4a435ea4d5d43612854b642b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:52:35 +0530

    Add CodeChef QUEUE08 (Practice problem - Necklace in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE08_Necklace_in_Stacks_and_Queues.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE08
+ * Problem: Practice problem - Necklace in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES02/problems/QUEUE08
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES02
+ * Status: ACCEPTED
+ */
+
+2
+5 3
+1 5 3 4 2
+6 5
+10 1 2 9 8 2
