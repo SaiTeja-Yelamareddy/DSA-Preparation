c624e583c88688c6577bce2d3a9538d89232e174
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:10:45 +0530

    Add CodeChef USRJ3V2 (Hello user in Java (Older version)) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/USRJ3V2_Hello_user_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: USRJ3V2
+ * Problem: Hello user in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP11/problems/USRJ3V2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP11
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
