d23f13356c84ec9ecc44e54528e65bb93647cd02
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:09:28 +0530

    Add CodeChef USRJ1AV2 (Scanner Methods in Java (Older version)) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/USRJ1AV2_Scanner_Methods_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: USRJ1AV2
+ * Problem: Scanner Methods in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP11/problems/USRJ1AV2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP11
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
