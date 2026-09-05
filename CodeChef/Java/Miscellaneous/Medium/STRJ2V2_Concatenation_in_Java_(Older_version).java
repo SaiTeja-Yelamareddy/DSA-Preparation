d1236ad21168976b1ea8b0aa24ccb2bb27282ffb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:12:57 +0530

    Add CodeChef STRJ2V2 (Concatenation in Java (Older version)) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/STRJ2V2_Concatenation_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRJ2V2
+ * Problem: Concatenation in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP08/problems/STRJ2V2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP08
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
