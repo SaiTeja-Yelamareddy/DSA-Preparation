313884bb5e91015f2a0f4d6d3490a505ffdc5caa
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:49:53 +0530

    Add CodeChef QUEUE06 (Queue - To do list in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE06_Queue_-_To_do_list_in_Stacks_and_Queues.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE06
+ * Problem: Queue - To do list in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES/problems/QUEUE06
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES
+ * Status: ACCEPTED
+ */
+
+1
+2
+4
+5
+1
+2
+3
+9
+4
+9
