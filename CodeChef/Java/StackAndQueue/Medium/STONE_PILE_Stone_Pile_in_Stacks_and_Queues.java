3eb6cd1eed0e119dbedc3871738980fbcc1433bc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:51:19 +0530

    Add CodeChef STONE_PILE (Stone Pile in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/STONE_PILE_Stone_Pile_in_Stacks_and_Queues.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STONE_PILE
+ * Problem: Stone Pile in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES02/problems/STONE_PILE
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES02
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
