971d17e02523dbc335cccb46ca9ec7546e6d79fa
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:49:24 +0530

    Add CodeChef QUEUE05 (Queue - Print Numbers with a Given Pattern in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE05_Queue_-_Print_Numbers_with_a_Given_Pattern_in_Stacks_and_Queues.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE05
+ * Problem: Queue - Print Numbers with a Given Pattern in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES/problems/QUEUE05
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
