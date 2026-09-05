77f613af8e2a7d77fc1669000a99244a1a8c51b3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:48:32 +0530

    Add CodeChef QUEUE10 (isEmpty, isFull, functions of Queue in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE10_isEmpty,_isFull,_functions_of_Queue_in_Stacks_and_Queues.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE10
+ * Problem: isEmpty, isFull, functions of Queue in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES/problems/QUEUE10
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
