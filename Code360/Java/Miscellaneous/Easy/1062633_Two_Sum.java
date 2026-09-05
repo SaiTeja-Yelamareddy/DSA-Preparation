7d25c96d36c6f8f8caf868efbe4502589d9b7bff
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 18:55:20 +0530

    Add Code360 1062633 (Two Sum) solution

---
+++ Code360/Java/Miscellaneous/Easy/1062633_Two_Sum.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: Code360
+ * Problem ID: 1062633
+ * Problem: Two Sum
+ * Problem Link: https://www.naukri.com/code360/problems/check-if-the-string-is-a-palindrome_1062633
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static boolean checkPalindrome(String str) {
+	
+	    // WRITE YOUR CODE HERE	
+	 
+	}
+}
