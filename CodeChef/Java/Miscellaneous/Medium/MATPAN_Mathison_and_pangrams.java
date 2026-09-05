49f7ac555c72076e44970013901aa49ad5cb0b0d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:04:41 +0530

    Add CodeChef MATPAN (Mathison and pangrams Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/MATPAN_Mathison_and_pangrams.java
+++ CodeChef/Java/Miscellaneous/Medium/MATPAN_Mathison_and_pangrams.java
@@ -9,4 +9,39 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+
+            int[] price = new int[26];
+
+            for (int i = 0; i < 26; i++) {
+                price[i] = sc.nextInt();
+            }
+            String s = sc.next();
+            boolean[] present = new boolean[26];
+            for (int i = 0; i < s.length(); i++) {
+                int index = s.charAt(i) - 'a';
+                present[index] = true;
+            }
+            int answer = 0;
+            for (int i = 0; i < 26; i++) {
+                if (!present[i]) {
+                    answer += price[i];
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
