88ce723178b00f249a04c18545d94c6b0df05925
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:20:09 +0530

    Add CodeChef STRJ1V2 (Creating a String in Java (Older version)) solution

---
+++ CodeChef/Java/Strings/Medium/STRJ1V2_Creating_a_String_in_Java_(Older_version).java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRJ1V2
+ * Problem: Creating a String in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP08/problems/STRJ1V2
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP08
+ * Status: ACCEPTED
+ */
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String fruit = "Apple";
+		System.out.print(fruit);
+	}
+}
