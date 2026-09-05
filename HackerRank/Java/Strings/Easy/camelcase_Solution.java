e080e2f6dfc8141ec7a2e63190d80d440a57fbf7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:01:23 +0530

    Add HackerRank camelcase (Solution) solution

---
+++ HackerRank/Java/Strings/Easy/camelcase_Solution.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: HackerRank
+ * Problem ID: camelcase
+ * Problem: Solution
+ * Problem Link: https://www.hackerrank.com/challenges/camelcase/problem
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Tags: Algorithms, Strings
+ * Status: ACCEPTED
+ */
+
+
+    return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
