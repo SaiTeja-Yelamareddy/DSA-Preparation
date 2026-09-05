e111685f0c8db21a29781c74ff26a8ea47d54198
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 18:58:06 +0530

    Add CodeChef DSAC29D (Check Palindrome using stack in Data Structures using C) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/DSAC29D_Check_Palindrome_using_stack_in_Data_Structures_using_C.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSAC29D
+ * Problem: Check Palindrome using stack in Data Structures using C
+ * Problem Link: https://www.codechef.com/learn/course/college-data-structures-c/CPDSC02/problems/DSAC29D
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: College Data Structures C
+ * Module: CPDSC02
+ * Status: ACCEPTED
+ */
+
+3
+5
+hello
+3
+bob
+7
+rotator
