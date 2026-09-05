5c40786f2eb7bc16c7226e6ea1ae4750726e9afb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:10:02 +0530

    Add CodeChef USRJ2V2 (Calculator in Java (Older version)) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/USRJ2V2_Calculator_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: USRJ2V2
+ * Problem: Calculator in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP11/problems/USRJ2V2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP11
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
