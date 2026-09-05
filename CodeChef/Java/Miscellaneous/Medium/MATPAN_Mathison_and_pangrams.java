8c73653cdb7085bbbba75d60cb5066b3c85978dd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:01:55 +0530

    Add CodeChef MATPAN (Mathison and pangrams Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/MATPAN_Mathison_and_pangrams.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATPAN
+ * Problem: Mathison and pangrams Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/MATPAN
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
