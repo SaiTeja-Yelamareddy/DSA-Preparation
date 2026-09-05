cd4baca93fa710870904272aa229e35d8b16b8a0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:15:18 +0530

    Add CodeChef STRJ5V2 (Outputting Characters from a String in Java (Older version)) solution

---
+++ CodeChef/Java/Strings/Medium/STRJ5V2_Outputting_Characters_from_a_String_in_Java_(Older_version).java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRJ5V2
+ * Problem: Outputting Characters from a String in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP08/problems/STRJ5V2
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP08
+ * Status: ACCEPTED
+ */
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		String word = "Programming";
+        System.out.println(word.charAt(2));
+        System.out.print(word.charAt(1));
+	}
+}
