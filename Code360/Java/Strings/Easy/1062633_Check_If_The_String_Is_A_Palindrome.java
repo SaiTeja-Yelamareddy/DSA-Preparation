db2e6b3513d8dae880f4ca553e22cfdca5c75593
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 18:57:26 +0530

    Add Code360 1062633 (Check If The String Is A Palindrome) solution

--- Code360/Java/Strings/Easy/1062633_Check_If_The_String_Is_A_Palindrome.java
+++ Code360/Java/Strings/Easy/1062633_Check_If_The_String_Is_A_Palindrome.java
@@ -13,9 +13,27 @@ import java.util.* ;
 import java.io.*; 
 public class Solution {
 
-	public static boolean checkPalindrome(String str) {
+	public static boolean checkPalindrome(String s) {
 	
 	    // WRITE YOUR CODE HERE	
+	    s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+		   if (Character.isLetterOrDigit(s.charAt(i))) {
+             sb.append(s.charAt(i));
+              }
+            }
+        String str = sb.toString();
+         int left = 0;
+        int right = str.length() - 1;
+         while (left < right) {
+         if (str.charAt(left) != str.charAt(right)) {
+           return false;
+         }
+         left++;
+        right--;
+        }
+         return true;
 	 
 	}
 }
