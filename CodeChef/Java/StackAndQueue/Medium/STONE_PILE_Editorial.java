e9544987321ecc732593eb5810cec880c1b90515
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:51:21 +0530

    Add CodeChef STONE_PILE (Editorial) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/STONE_PILE_Editorial.java
@@ -0,0 +1,25 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STONE_PILE
+ * Problem: Editorial
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES02/problems/STONE_PILE
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES02
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
