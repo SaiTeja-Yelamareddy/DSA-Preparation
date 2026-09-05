65d095ee63c7815277b7b5910a44bc4976762f8d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:26:59 +0530

    Add GeeksforGeeks reversing-the-vowels5304 (Reverse Vowels) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/reversing-the-vowels5304_Reverse_Vowels.java
@@ -0,0 +1,31 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: reversing-the-vowels5304
+ * Problem: Reverse Vowels
+ * Problem Link: https://www.geeksforgeeks.org/problems/reversing-the-vowels5304/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String modify(String s) {
+        LinkedHashMap<Integer, Character> map = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u') {
+                map.put(i, c);
+            }
+        }
+        ArrayList<Character> vowels = new ArrayList<>(map.values());
+        int j = vowels.size() - 1;
+        char[] arr = s.toCharArray();
+        for (int index : map.keySet()) {
+            arr[index] = vowels.get(j);
+            j--;
+        }
+        return new String(arr);
+    }
+}
