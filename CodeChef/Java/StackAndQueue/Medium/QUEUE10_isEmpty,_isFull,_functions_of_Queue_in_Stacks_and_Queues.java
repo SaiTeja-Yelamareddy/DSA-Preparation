2ac982b0dc3e2836bbc678b3b99623788be4f24c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:48:36 +0530

    Add CodeChef QUEUE10 (isEmpty, isFull, functions of Queue in Stacks and Queues) solution

--- CodeChef/Java/StackAndQueue/Medium/QUEUE10_isEmpty,_isFull,_functions_of_Queue_in_Stacks_and_Queues.java
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE10_isEmpty,_isFull,_functions_of_Queue_in_Stacks_and_Queues.java
@@ -11,4 +11,35 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+public class Main {
+    private static final int maxSize = 101;
+    private static int[] a = new int[maxSize];
+    private static int front = 0; // Index of the front element
+    private static int rear = -1;  // Index of the rear element
+    private static int currentSize;
+
+    private static boolean isEmpty() {
+        if(currentSize == 0)
+        {
+            return __;
+        }
+        else
+        {
+            return __;
+        }
+    }
+
+    private static boolean isFull() {
+        if(currentSize == maxSize)
+        {
+            return __;
+        }
+        else
+        {
+            return __;
+        }
+    }
+    
+    private static int size() {
+        return ___;
+    }
