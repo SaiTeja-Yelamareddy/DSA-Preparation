c85bf036b6efca9d475bf0561579a69baebaf48c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:07:03 +0530

    Add CodeChef USRJ1V2 (Taking user input in Java (Older version)) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/USRJ1V2_Taking_user_input_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: USRJ1V2
+ * Problem: Taking user input in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP11/problems/USRJ1V2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP11
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
