c5eb9ac06cb3ad0bd8d080db0e11281ee1201ed0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:50:40 +0530

    Add CodeChef QUEUE09 (Library used for queues implementation in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE09_Library_used_for_queues_implementation_in_Stacks_and_Queues.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE09
+ * Problem: Library used for queues implementation in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES02/problems/QUEUE09
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES02
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
