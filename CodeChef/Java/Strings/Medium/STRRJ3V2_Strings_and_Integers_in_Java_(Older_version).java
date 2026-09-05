2c16fc9a6dae6f06c8647c081e42c6698797688f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:14:48 +0530

    Add CodeChef STRRJ3V2 (Strings and Integers in Java (Older version)) solution

---
+++ CodeChef/Java/Strings/Medium/STRRJ3V2_Strings_and_Integers_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRRJ3V2
+ * Problem: Strings and Integers in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP08/problems/STRRJ3V2
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP08
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
