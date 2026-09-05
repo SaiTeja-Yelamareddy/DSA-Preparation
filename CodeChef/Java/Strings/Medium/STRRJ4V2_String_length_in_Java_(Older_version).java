c562abf263e528f95a62e00b8339099faf832833
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:14:57 +0530

    Add CodeChef STRRJ4V2 (String length in Java (Older version)) solution

---
+++ CodeChef/Java/Strings/Medium/STRRJ4V2_String_length_in_Java_(Older_version).java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: STRRJ4V2
+ * Problem: String length in Java (Older version)
+ * Problem Link: https://www.codechef.com/learn/course/java-previous/LTJPP08/problems/STRRJ4V2
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Course: With Java Previous
+ * Module: LTJPP08
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
