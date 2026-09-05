a6c6e896d6c96b66b22d181c413f349b56527c4a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:48:01 +0530

    Add CodeChef QUEUE11 (Enqueue and Dequeue Functions in Stacks and Queues) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/QUEUE11_Enqueue_and_Dequeue_Functions_in_Stacks_and_Queues.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: QUEUE11
+ * Problem: Enqueue and Dequeue Functions in Stacks and Queues
+ * Problem Link: https://www.codechef.com/learn/course/stacks-and-queues/LQUEUES/problems/QUEUE11
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Stacks And Queues
+ * Module: LQUEUES
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
