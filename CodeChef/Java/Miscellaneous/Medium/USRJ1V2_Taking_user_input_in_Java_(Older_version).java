b4484dcbbb1ddcc287ea03eb418f8511e389d35a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-05 19:07:12 +0530

    Add CodeChef USRJ1V2 (Taking user input in Java (Older version)) solution

--- CodeChef/Java/Miscellaneous/Medium/USRJ1V2_Taking_user_input_in_Java_(Older_version).java
+++ CodeChef/Java/Miscellaneous/Medium/USRJ1V2_Taking_user_input_in_Java_(Older_version).java
@@ -11,4 +11,15 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner objName = new Scanner(System.in);
+		String varName = objName.nextLine();
+		System.out.println(varName);
+	}
+}
