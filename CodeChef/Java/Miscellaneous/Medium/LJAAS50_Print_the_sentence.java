a4716c0c7fb35bf949acae150263ca6b30f6fd6e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:18:44 +0530

    Add CodeChef LJAAS50 (Print the sentence Practice Problem in Java) solution

--- CodeChef/Java/Miscellaneous/Medium/LJAAS50_Print_the_sentence.java
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS50_Print_the_sentence.java
@@ -9,4 +9,15 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.Scanner;
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String one = scanner.nextLine();
+        String two = scanner.nextLine();
+        System.out.println(one + " " + two);
+    }
+}
