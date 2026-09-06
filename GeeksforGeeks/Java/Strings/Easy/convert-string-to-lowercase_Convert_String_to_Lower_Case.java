0786c174ab444b9ca688bd1f1b13ef363935171f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 12:02:17 +0530

    Add GeeksforGeeks convert-string-to-lowercase (Convert String to Lower Case) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/convert-string-to-lowercase_Convert_String_to_Lower_Case.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: convert-string-to-lowercase
+ * Problem: Convert String to Lower Case
+ * Problem Link: https://www.geeksforgeeks.org/problems/convert-string-to-lowercase/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static String toLower(String s) {
+        // code here
+        return s.toLowerCase();
+    }
+}
