438fa91c0b5cbc1e7a53fe510a8fa1242f38dee8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:14:46 +0530

    Add CodeChef LJAAS39 (Identify the errors Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS39_Identify_the_errors.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS39
+ * Problem: Identify the errors Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS04/problems/LJAAS39
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
