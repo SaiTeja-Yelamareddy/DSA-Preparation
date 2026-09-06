8ae998b4fdcce5cbc55f42b4eacec49b3b01b615
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 12:16:36 +0530

    Add GeeksforGeeks string-with-numbers-at-its-end5749 (String with Number at End) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/string-with-numbers-at-its-end5749_String_with_Number_at_End.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: string-with-numbers-at-its-end5749
+ * Problem: String with Number at End
+ * Problem Link: https://www.geeksforgeeks.org/problems/string-with-numbers-at-its-end5749/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    int isSame(String s) {
+        // code here
+        int len=Integer.valueOf(s.charAt(s.length()-1));
+        int slen=s.length()-1;
+        if(len==slen)
+        return 1;
+        return 0;
+    }
+}
