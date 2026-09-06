343f51686d07aa0b2f8d08dadc43dcad72f667cd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:45:22 +0530

    Add CodeChef 1352981696 (Calculator) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1352981696_Calculator.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1352981696
+ * Problem: Calculator
+ * Problem Link: https://www.codechef.com/viewsolution/1352981696
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code below
+		Scanner read = new Scanner(System.in);
+		int a = read.nextInt();
+		int b = read.nextInt();
+		int sum = a+b;
+		int diff = a-b;
+		System.out.println("Sum is: " + sum);
+		System.out.println("Difference is: " + diff);
+	}
+}
