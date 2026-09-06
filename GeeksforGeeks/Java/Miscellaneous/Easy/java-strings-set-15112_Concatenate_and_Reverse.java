1b7d37107c21e0857de8b53f28c96c0f253110e7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 12:06:22 +0530

    Add GeeksforGeeks java-strings-set-15112 (Concatenate and Reverse) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/java-strings-set-15112_Concatenate_and_Reverse.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: java-strings-set-15112
+ * Problem: Concatenate and Reverse
+ * Problem Link: https://www.geeksforgeeks.org/problems/java-strings-set-15112/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static String conRevstr(String s1, String s2) {
+        // code here
+        Stringbuilder sb=new StringBulider(s1);
+        sb.append(s2);
+        sb.reverse();
+        return sb.toString();
+    }
+}
