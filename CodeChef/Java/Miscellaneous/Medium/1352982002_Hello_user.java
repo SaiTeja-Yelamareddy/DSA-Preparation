dde3dcbcd2654381a1ef1052f3b2e879b0e60de8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:45:08 +0530

    Add CodeChef 1352982002 (Hello user) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1352982002_Hello_user.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1352982002
+ * Problem: Hello user
+ * Problem Link: https://www.codechef.com/viewsolution/1352982002
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code below
+		Scanner read = new Scanner(System.in);
+		String x = read.nextLine();
+		System.out.println("Hello " + x);
+	}
+}
