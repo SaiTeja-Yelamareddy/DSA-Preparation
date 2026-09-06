d04c4004195505e6544ba64b96150cd697f50ec2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:27:26 +0530

    Add CodeChef 1353284515 (Fit in Data Type) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1353284515_Fit_in_Data_Type.java
@@ -0,0 +1,26 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1353284515
+ * Problem: Fit in Data Type
+ * Problem Link: https://www.codechef.com/viewsolution/1353284515
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0)
+        {
+            int N=sc.nextInt();
+            int X=sc.nextInt();
+            System.out.println(X%(N+1));
+        }
+    }
+}
