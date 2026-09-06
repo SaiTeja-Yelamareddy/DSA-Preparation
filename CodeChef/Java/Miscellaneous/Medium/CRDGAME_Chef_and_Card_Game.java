873510940a6429b9d9b56b5cea7d8b0911649624
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:37:32 +0530

    Add CodeChef CRDGAME (Chef and Card Game Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/CRDGAME_Chef_and_Card_Game.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: CRDGAME
+ * Problem: Chef and Card Game Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/CRDGAME
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
