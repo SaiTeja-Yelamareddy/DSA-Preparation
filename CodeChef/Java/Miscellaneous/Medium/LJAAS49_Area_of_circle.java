e7c491f418c4978087036cb53ddb5be8720f2f1e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:18:19 +0530

    Add CodeChef LJAAS49 (Area of circle Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS49_Area_of_circle.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS49
+ * Problem: Area of circle Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS05/problems/LJAAS49
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+
+	}
+}
