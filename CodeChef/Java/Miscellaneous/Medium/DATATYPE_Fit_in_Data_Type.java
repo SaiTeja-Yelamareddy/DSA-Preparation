15ceb6d3f1bbecdc9ada1f569fcc83dfff045c62
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:09:08 +0530

    Add CodeChef DATATYPE (Fit in Data Type Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/DATATYPE_Fit_in_Data_Type.java
+++ CodeChef/Java/Miscellaneous/Medium/DATATYPE_Fit_in_Data_Type.java
@@ -9,9 +9,23 @@
  * Status: ACCEPTED
  */
 
-5
-15 0
-15 10
-11 12
-27 37
-50 49
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int ans = x % (n + 1);
+            System.out.println(ans);
+        }
+	}
+}
