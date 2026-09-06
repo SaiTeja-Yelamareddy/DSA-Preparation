016b6b510c2defdcffe0538116c677d478bb62dc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:07:53 +0530

    Add CodeChef 1353273028 (Stone Pile) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1353273028_Stone_Pile.java
@@ -0,0 +1,71 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1353273028
+ * Problem: Stone Pile
+ * Problem Link: https://www.codechef.com/viewsolution/1353273028
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+
+import java.io.*;
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        StringBuilder sb = new StringBuilder();
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Deque<Integer> dq = new ArrayDeque<>();
+            for(int i = 0; i < n; i++){
+                dq.offer(Integer.parseInt(st.nextToken()));
+            }
+            int lastPlayer = -1; // 1 for Aman, 0 for Akshat
+            boolean isAmanTurn = true;
+            while(dq.size() > 1)
+            {
+                if(isAmanTurn)
+                {
+                    // Aman performs: move 1 then move 2
+                    if(dq.size() > 1)
+                    {
+                        int stone = dq.poll();
+                        dq.offer(stone);
+                    }
+                    if(dq.size() > 1){
+                        dq.poll();
+                        lastPlayer = 1;
+                    }
+                } 
+                else {
+                    if(dq.size() > 1)
+                    {
+                        int stone = dq.poll();
+                        dq.offer(stone);
+                    }
+                    if(dq.size() > 1)
+                    {
+                        int stone = dq.poll();
+                        dq.offer(stone);
+                    }
+                    if(dq.size() > 1)
+                    {
+                        dq.poll();
+                        lastPlayer = 0;
+                    }
+                }
+                isAmanTurn = !isAmanTurn;
+            }
+            int lastStone = dq.peek();
+            sb.append(lastPlayer).append(" ").append(lastStone).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
