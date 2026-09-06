eb70288e7eaafd6c6cee33c6f5fb17b838e8526c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:28:28 +0530

    Add CodeChef LAPIN (Lapindromes Practice Problem in Strings - Intermediate) solution

---
+++ CodeChef/Java/Strings/Medium/LAPIN_Lapindromes_-_Intermediate.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LAPIN
+ * Problem: Lapindromes Practice Problem in Strings - Intermediate
+ * Problem Link: https://www.codechef.com/practice/course/strings-intermediate/STRINGSP02/problems/LAPIN
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
