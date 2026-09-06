1db157c9da2427f722c97fd0c1690e3a2648611e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:16:18 +0530

    Add CodeChef LJAAS47 (Rectify the error Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS47_Rectify_the_error.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS47
+ * Problem: Rectify the error Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS05/problems/LJAAS47
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
