4da6126d638e0ea0e99f7122c8ca4336d993c429
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:15:11 +0530

    Add CodeChef LJAAS40 (Print the length Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS40_Print_the_length.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS40
+ * Problem: Print the length Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS04/problems/LJAAS40
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
