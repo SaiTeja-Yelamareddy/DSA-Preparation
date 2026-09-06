2c39f5fca24aca7a4f96ca4d7293af358f9b7bb7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:18:23 +0530

    Add CodeChef LJAAS49 (Area of circle Practice Problem in Java) solution

--- CodeChef/Java/Miscellaneous/Medium/LJAAS49_Area_of_circle.java
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS49_Area_of_circle.java
@@ -12,9 +12,11 @@
 import java.util.Scanner;
 class Codechef
 {
-	public static void main (String[] args) 
-	{
-		// your code goes here
-
-	}
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println(area);
+    }
 }
