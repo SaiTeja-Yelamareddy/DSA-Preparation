8097d9e836ee632c4d6af7331279d82739ec9f41
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 12:30:14 +0530

    Add GeeksforGeeks red-or-green5711 (Red OR Green) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/red-or-green5711_Red_OR_Green.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: red-or-green5711
+ * Problem: Red OR Green
+ * Problem Link: https://www.geeksforgeeks.org/problems/red-or-green5711/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public int RedOrGreen(int n, String s) {
+        int[] arr = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R')
+                arr[0]++;
+            else
+                arr[1]++;
+        }
+        return Math.min(arr[0], arr[1]);
+    }
+}
