e3d4dae6dcb6f30d46c8800eeb15dbe90dab5173
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:18:42 +0530

    Add CodeChef LJAAS50 (Print the sentence Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS50_Print_the_sentence.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS50
+ * Problem: Print the sentence Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS05/problems/LJAAS50
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
