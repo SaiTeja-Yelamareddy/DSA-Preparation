cffe3280240b768ff471528b29b9f6b532e9f34c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:27:27 +0530

    Add CodeChef KOL15A (Processing a string Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Strings/Medium/KOL15A_Processing_a_string.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KOL15A
+ * Problem: Processing a string Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/KOL15A
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
