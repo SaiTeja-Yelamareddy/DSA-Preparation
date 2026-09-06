4eecd5a697c20b5584f08b69dcb743c49087411c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:43:48 +0530

    Add CodeChef PETSTORE (Pet Store Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PETSTORE_Pet_Store.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PETSTORE
+ * Problem: Pet Store Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/PETSTORE
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+4
+3
+4 4 4
+4
+2 3 3 2
+4
+1 2 2 3
+6
+5 5 1 5 1 5
