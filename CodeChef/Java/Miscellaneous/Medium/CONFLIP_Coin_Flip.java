705b657fdb5fd98e868152681c2c67daf146a273
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:27:54 +0530

    Add CodeChef CONFLIP (Coin Flip Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/CONFLIP_Coin_Flip.java
+++ CodeChef/Java/Miscellaneous/Medium/CONFLIP_Coin_Flip.java
@@ -9,4 +9,44 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		  Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0)
+        {
+            int G = sc.nextInt();
+            while(G-- > 0)
+            {
+                int I = sc.nextInt();
+                int N = sc.nextInt();
+                int Q = sc.nextInt();
+                int answer;
+                if(N % 2 == 0)
+                {
+                    answer = N / 2;
+                }
+                else
+                {
+                    if(I == Q)
+                    {
+                        answer = N / 2;
+                    }
+                    else
+                    {
+                        answer = (N + 1) / 2;
+                    }
+                }
+                System.out.println(answer);
+            }
+        }
+
+	}
+}
