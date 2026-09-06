eee13f3c510ae1d481e99d82fcdc08775f35893e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 22:17:46 +0530

    Add CodeChef LJAAS48 (Debug this code Practice Problem in Java) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/LJAAS48_Debug_this_code.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LJAAS48
+ * Problem: Debug this code Practice Problem in Java
+ * Problem Link: https://www.codechef.com/practice/course/java/LPJAAS05/problems/LJAAS48
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
