b51254c4840e18056eb4cc95e1783b51e879f4b7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:37:03 +0530

    Add CodeChef KOL15A (Processing a string Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Strings/Medium/KOL15A_Processing_a_string.java
+++ CodeChef/Java/Strings/Medium/KOL15A_Processing_a_string.java
@@ -9,4 +9,33 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    String s=sc.nextLine();
+		    int sum=0;
+		    
+		    for(int i=0;i<s.length();i++)
+		    {
+		        char ch=s.charAt(i);
+		        if(Character.isDigit(ch))
+		        {
+		            sum+=ch-'0';
+		        }
+		    }
+		    System.out.println(sum);
+		}
+
+	}
+}
