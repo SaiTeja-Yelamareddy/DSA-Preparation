e6413ac44d93543ec210142d489fed7b5a3470e8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 12:32:35 +0530

    Add GeeksforGeeks remove-all-characters-other-than-alphabets4923 (Remove All Except Alphabets) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/remove-all-characters-other-than-alphabets4923_Remove_All_Except_Alphabets.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: remove-all-characters-other-than-alphabets4923
+ * Problem: Remove All Except Alphabets
+ * Problem Link: https://www.geeksforgeeks.org/problems/remove-all-characters-other-than-alphabets4923/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String removeChars(String s) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetter(ch)) {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+};
