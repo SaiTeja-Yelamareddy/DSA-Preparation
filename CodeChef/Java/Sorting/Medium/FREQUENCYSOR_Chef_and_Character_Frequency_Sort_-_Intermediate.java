8209c6c2665d0e71c1fe992f0f6047a8a43211ae
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:51:51 +0530

    Add CodeChef FREQUENCYSOR (Chef and Character Frequency Sort Practice Problem in Strings - Intermediate) solution

--- CodeChef/Java/Sorting/Medium/FREQUENCYSOR_Chef_and_Character_Frequency_Sort_-_Intermediate.java
+++ CodeChef/Java/Sorting/Medium/FREQUENCYSOR_Chef_and_Character_Frequency_Sort_-_Intermediate.java
@@ -9,6 +9,29 @@
  * Status: ACCEPTED
  */
 
-public static String sortByFrequency(String s) {
-    //write your code here...
-}
+public static String sortByFrequency(String s) 
+{
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) 
+        {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        List<Character> chars = new ArrayList<>(freq.keySet());
+        Collections.sort(chars, (a, b) -> {
+            int fa = freq.get(a);
+            int fb = freq.get(b);
+            if (fa != fb) {
+                return fb - fa; 
+            }
+            return a - b; 
+        });
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            int count = freq.get(c);
+            for (int i = 0; i < count; i++) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
