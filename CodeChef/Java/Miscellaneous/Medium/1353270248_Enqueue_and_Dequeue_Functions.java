8b0c585cc0a1b6a7f9c6541f8947bd0ac80f6178
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:49:49 +0530

    Add CodeChef 1353270248 (Enqueue and Dequeue Functions) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1353270248_Enqueue_and_Dequeue_Functions.java
@@ -0,0 +1,54 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1353270248
+ * Problem: Enqueue and Dequeue Functions
+ * Problem Link: https://www.codechef.com/viewsolution/1353270248
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+class CircularQueue {
+    private static final int maxSize = 101;
+    private int[] a = new int[maxSize];
+    private int front = 0;
+    private int rear = -1;  
+    private int currentSize;
+    public boolean isEmpty()
+    {
+        return currentSize == 0;
+    }
+    public boolean isFull() 
+    {
+        return currentSize == maxSize;
+    }
+    public int size()
+    {
+        return currentSize;
+    }
+    public void enqueue(int item)
+    {
+        if (isFull())
+        {
+            System.out.println("Queue is full. Cannot enqueue.");
+            return;
+        }
+        rear = (rear + 1) % maxSize; 
+        a[rear] = item;
+        currentSize++;
+    }
+    public int dequeue()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Queue is empty. Cannot dequeue.");
+            return -1; 
+        }
+        int removedItem = a[front];
+        front = (front + 1) % maxSize; // Circular increment
+        currentSize--;
+        return removedItem;
+    }
+
+   
