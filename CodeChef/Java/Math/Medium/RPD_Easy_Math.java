37376a1c55f5361096313e5163e91c1dceed2ef6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:00:35 +0530

    Add CodeChef RPD (Easy Math Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Math/Medium/RPD_Easy_Math.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: RPD
+ * Problem: Easy Math Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/RPD
+ * Language: Java
+ * Concept: Math
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
