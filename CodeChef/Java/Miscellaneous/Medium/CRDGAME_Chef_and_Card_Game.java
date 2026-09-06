3f2915f10e9cb4ab0105407936dfca2b683bc1bf
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 10:43:09 +0530

    Add CodeChef CRDGAME (Chef and Card Game Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/CRDGAME_Chef_and_Card_Game.java
+++ CodeChef/Java/Miscellaneous/Medium/CRDGAME_Chef_and_Card_Game.java
@@ -9,4 +9,73 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Codechef
+{
+    static int digitSum(int n)
+    {
+        int sum = 0;
+
+        while(n > 0)
+        {
+            sum += n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+
+            int chef = 0;
+            int morty = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                int sumA = digitSum(a);
+                int sumB = digitSum(b);
+
+                if(sumA > sumB)
+                {
+                    chef++;
+                }
+                else if(sumB > sumA)
+                {
+                    morty++;
+                }
+                else
+                {
+                    chef++;
+                    morty++;
+                }
+            }
+            if(chef > morty)
+            {
+                System.out.println("0 " + chef);
+            }
+            else if(morty > chef)
+            {
+                System.out.println("1 " + morty);
+            }
+            else
+            {
+                System.out.println("2 " + chef);
+            }
+        }
+    }
+}
