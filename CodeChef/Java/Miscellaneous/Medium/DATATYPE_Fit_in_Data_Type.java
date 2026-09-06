70d2e2112feeec848268bb354175a9f77fdfe227
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-06 11:05:26 +0530

    Add CodeChef DATATYPE (Fit in Data Type Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/DATATYPE_Fit_in_Data_Type.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DATATYPE
+ * Problem: Fit in Data Type Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/DATATYPE
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+5
+15 0
+15 10
+11 12
+27 37
+50 49
