4784010e8e8c9f1b8dd2eb7942bad72e30b84db4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:31:49 +0530

    Add CodeChef EZSPEAK (Easy Pronunciation Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/EZSPEAK_Easy_Pronunciation.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: EZSPEAK
+ * Problem: Easy Pronunciation Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/EZSPEAK
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
