908f215a1fea20472d3a1e31f8db31264b9076c3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:27:11 +0530

    Add CodeChef TITLECASE (Convert String to Title Case Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/TITLECASE_Convert_String_to_Title_Case.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TITLECASE
+ * Problem: Convert String to Title Case Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings/STRINGS/problems/TITLECASE
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
