7caa7d0a25e020235f698bf90e947e5488121ff4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 19:52:26 +0530

    Add CodeChef REVERSEWORD (Reverse Words in a String Practice Problem in Strings - Intermediate) solution

--- CodeChef/Java/Strings/Medium/REVERSEWORD_Reverse_Words_in_a_String_-_Intermediate.java
+++ CodeChef/Java/Strings/Medium/REVERSEWORD_Reverse_Words_in_a_String_-_Intermediate.java
@@ -5,10 +5,18 @@
  * Problem Link: https://www.codechef.com/practice/course/strings-intermediate/STRINGSP02/problems/REVERSEWORD
  * Language: Java
  * Concept: Strings
- * Difficulty: Medium
  * Status: ACCEPTED
  */
 
-public static String reverseWords(String s) {
-    //write code here...
-}
+ public static String reverseWords(String s) {
+        
+        s = s.trim();
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+
+        return sb.toString();
+    }
