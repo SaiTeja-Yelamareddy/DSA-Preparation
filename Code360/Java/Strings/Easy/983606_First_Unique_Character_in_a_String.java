4fe1f849b0633c3d3703bcb93371a1f5537f7877
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 20:41:47 +0530

    Add Code360 983606 (First Unique Character in a String) solution

---
+++ Code360/Java/Strings/Easy/983606_First_Unique_Character_in_a_String.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: Code360
+ * Problem ID: 983606
+ * Problem: First Unique Character in a String
+ * Problem Link: https://www.naukri.com/code360/problems/first-unique-character-in-a-string_983606
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static char firstNonRepeating(String str) {
+		// Write your code here
+	}
+}
