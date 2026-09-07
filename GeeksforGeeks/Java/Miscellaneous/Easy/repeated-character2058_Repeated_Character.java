5c505d9ae4079b7dc411f74f43848b43aab825dc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 20:37:22 +0530

    Add GeeksforGeeks repeated-character2058 (Repeated Character) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/repeated-character2058_Repeated_Character.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: repeated-character2058
+ * Problem: Repeated Character
+ * Problem Link: https://www.geeksforgeeks.org/problems/repeated-character2058/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    char firstRep(String S) {
+        // your code here
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+                for (int i = 0; i < s.length(); i++) {
+                    char ch = s.charAt(i);
+                    map.put(ch, map.getOrDefault(ch, 0) + 1);
+                }
+                for (char ch : map.keySet()) {
+                    if (map.get(ch) == 1) {
+                        return ch;
+                    }
+                }
+                return '$';
+    }
+}
