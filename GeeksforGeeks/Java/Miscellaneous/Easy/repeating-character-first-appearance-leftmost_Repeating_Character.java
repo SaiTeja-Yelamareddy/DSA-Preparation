481740ba1256120fa913da977004bb66eaf13536
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 20:40:21 +0530

    Add GeeksforGeeks repeating-character-first-appearance-leftmost (Repeating Character) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/repeating-character-first-appearance-leftmost_Repeating_Character.java
@@ -0,0 +1,30 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: repeating-character-first-appearance-leftmost
+ * Problem: Repeating Character
+ * Problem Link: https://www.geeksforgeeks.org/problems/repeating-character-first-appearance-leftmost/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static int repeatedCharacter(String S) {
+        // code here
+            HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+             for (int i = 0; i < s.length(); i++) {
+                 char ch = s.charAt(i);
+                if (!map.containsKey(ch))
+                map.put(ch, new ArrayList<>());
+                 map.get(ch).add(i);
+             }
+             for (int i = 0; i < s.length(); i++) {
+                 char ch = s.charAt(i);
+                 if (map.get(ch).size() > 1) {
+                     return i;
+                 }
+             }
+             return -1;   
+    }
+}
