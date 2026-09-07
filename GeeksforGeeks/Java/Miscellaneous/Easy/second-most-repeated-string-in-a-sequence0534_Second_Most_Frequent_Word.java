a585b29601192cfa9ecdb0dd03e76869112e4b68
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:03:06 +0530

    Add GeeksforGeeks second-most-repeated-string-in-a-sequence0534 (Second Most Frequent Word) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/second-most-repeated-string-in-a-sequence0534_Second_Most_Frequent_Word.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/second-most-repeated-string-in-a-sequence0534_Second_Most_Frequent_Word.java
@@ -16,8 +16,11 @@ class Solution {
               for (int i = 0; i < arr.length; i++) {
                   map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
               }
+              int count=0;
               for (int i = 0; i < arr.length; i++) {
                   if (map.get(arr[i]) > 1) {
+                      count++;
+                      if(count==2)
                       return i;
                   }
               }
