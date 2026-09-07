c8368b03ececbdc11290fd8238f54685eb8078e4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:55:25 +0530

    Add CodeChef EZSPEAK (Easy Pronunciation Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/EZSPEAK_Easy_Pronunciation.java
+++ CodeChef/Java/Miscellaneous/Medium/EZSPEAK_Easy_Pronunciation.java
@@ -18,6 +18,39 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		       Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            String S = sc.next();
+
+            int count = 0;
+            boolean hard = false;
+
+            for (int i = 0; i < N; i++) {
+                char ch = S.charAt(i);
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                    ch == 'o' || ch == 'u') {
+                    count = 0;
+                } else {
+                    count++;
+
+                    if (count >= 4) {
+                        hard = true;
+                        break;
+                    }
+                }
+            }
+
+            if (hard) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
 
 	}
 }
