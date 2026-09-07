4925d0f70af6fd7619cb9e3de6148864391b53a8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:29:27 +0530

    Add CodeChef DIET (Chef Diet Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/DIET_Chef_Diet.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DIET
+ * Problem: Chef Diet Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/DIET
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3
+4 5
+7 3 6 5
+3 4
+3 10 10
+3 4
+8 1 1
