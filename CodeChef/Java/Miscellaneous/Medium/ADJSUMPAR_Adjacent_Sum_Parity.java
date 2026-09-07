3b63a78d0d089f5a44d8aeaa5fb5d9bdb30e3f27
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:48:06 +0530

    Add CodeChef ADJSUMPAR (Adjacent Sum Parity Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/ADJSUMPAR_Adjacent_Sum_Parity.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ADJSUMPAR
+ * Problem: Adjacent Sum Parity Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ADJSUMPAR
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
