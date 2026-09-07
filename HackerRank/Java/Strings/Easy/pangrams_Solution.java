77b6ca96317b6f0f4b3c41fcfb3b1b84dcb2e2f4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 19:52:35 +0530

    Add HackerRank pangrams (Solution) solution

--- HackerRank/Java/Strings/Easy/pangrams_Solution.java
+++ HackerRank/Java/Strings/Easy/pangrams_Solution.java
@@ -5,17 +5,21 @@
  * Problem Link: https://www.hackerrank.com/challenges/pangrams/problem
  * Language: Java
  * Concept: Strings
- * Difficulty: Easy
- * Tags: Algorithms, Strings
  * Status: ACCEPTED
  */
 
-(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String s = bufferedReader.readLine();
-
-        String result = Result.pangrams(s);
-
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+…
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
