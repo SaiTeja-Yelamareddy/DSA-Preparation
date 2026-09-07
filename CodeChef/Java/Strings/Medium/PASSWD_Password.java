43c408e8892cfb789108009276584ddb8fae3a01
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:24:24 +0530

    Add CodeChef PASSWD (Password Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/PASSWD_Password.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PASSWD
+ * Problem: Password Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings/STRINGS/problems/PASSWD
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
