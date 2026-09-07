aeae550f5f3aa7727a209b4eef8d213edd3396f2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:39:08 +0530

    Add CodeChef DIET (Chef Diet Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/DIET_Chef_Diet.java
+++ CodeChef/Java/Miscellaneous/Medium/DIET_Chef_Diet.java
@@ -9,4 +9,35 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int protein = 0;
+            int failedDay = -1;
+            for (int i = 1; i <= N; i++) {
+                int A = sc.nextInt();
+                if (failedDay == -1) {
+                    protein += A;
+                    if (protein < K) {
+                        failedDay = i;
+                    } else {
+                        protein -= K;
+                    }
+                }
+            }
+            if (failedDay == -1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO " + failedDay);
+            }
+        }
+        sc.close();
+    }
+}
