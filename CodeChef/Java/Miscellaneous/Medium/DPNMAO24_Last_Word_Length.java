8b71e28a55c10240ade2accb49d0d7b036440f1e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 19:52:32 +0530

    Add CodeChef DPNMAO24 (Last Word Length Practice Problem in Accenture Interview Questions) solution

--- CodeChef/Java/Miscellaneous/Medium/DPNMAO24_Last_Word_Length.java
+++ CodeChef/Java/Miscellaneous/Medium/DPNMAO24_Last_Word_Length.java
@@ -5,7 +5,6 @@
  * Problem Link: https://www.codechef.com/practice/course/accenture-interview-questions/ACCNTRE05/problems/DPNMAO24
  * Language: Java
  * Concept: Miscellaneous
- * Difficulty: Medium
  * Status: ACCEPTED
  */
 
@@ -13,11 +12,23 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
 
-	}
+        int length = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        System.out.println(length);
+        sc.close();
+    }
 }
