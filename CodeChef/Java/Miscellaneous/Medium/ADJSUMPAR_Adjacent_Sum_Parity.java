817a9205808d3eb8c0b2668a8574e1240c2ac197
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:49:35 +0530

    Add CodeChef ADJSUMPAR (Adjacent Sum Parity Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/ADJSUMPAR_Adjacent_Sum_Parity.java
+++ CodeChef/Java/Miscellaneous/Medium/ADJSUMPAR_Adjacent_Sum_Parity.java
@@ -18,6 +18,24 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                int B = sc.nextInt();
+
+                if (B == 1) {
+                    count++;
+                }
+            }
+            if (count % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
 
 	}
 }
