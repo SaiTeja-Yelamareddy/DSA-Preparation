3fd7573a27fec3505582fd0a40a99fbc84956243
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:52:40 +0530

    Add CodeChef ATM2 (ATM Machine Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/ATM2_ATM_Machine.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ATM2
+ * Problem: ATM Machine Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ATM2
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+2
+5 10
+3 5 3 2 1
+4 6
+10 8 6 4
