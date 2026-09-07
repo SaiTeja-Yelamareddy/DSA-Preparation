f599076b6d9b7a7634273c40a83f655aae97fb81
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 21:53:48 +0530

    Add CodeChef ATM2 (ATM Machine Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/ATM2_ATM_Machine.java
+++ CodeChef/Java/Miscellaneous/Medium/ATM2_ATM_Machine.java
@@ -18,6 +18,23 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                int A = sc.nextInt();
+                if (K >= A) {
+                    ans.append("1");
+                    K = K - A;
+                } else {
+                    ans.append("0");
+                }
+            }
+            System.out.println(ans);
+        }
 
 	}
 }
