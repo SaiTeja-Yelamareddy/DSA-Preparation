57149183de52e113dabe4bdd00eac15986cbbb22
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-07 19:52:30 +0530

    Add GeeksforGeeks input-a-line (Input a Line) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/input-a-line_Input_a_Line.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/input-a-line_Input_a_Line.java
@@ -5,18 +5,17 @@
  * Problem Link: https://www.geeksforgeeks.org/problems/input-a-line/1
  * Language: Java
  * Concept: Miscellaneous
- * Difficulty: Easy
  * Status: ACCEPTED
  */
 
 import java.util.*;
 class Solution {
-    
+    static Scanner sc = new Scanner(System.in);
+
     public static void getLine() {
         String s;
         
         // Take input of 's' using nextLine()
-         Scanner sc = new Scanner(System.in);
          s=sc.nextLine();
 
         System.out.println(s);
