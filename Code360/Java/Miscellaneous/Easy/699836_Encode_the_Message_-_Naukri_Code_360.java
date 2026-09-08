297eaeb0608afb77729cda0f92541b9149ae545e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-08 20:39:08 +0530

    Add Code360 699836 (Encode the Message - Naukri Code 360) solution

---
+++ Code360/Java/Miscellaneous/Easy/699836_Encode_the_Message_-_Naukri_Code_360.java
@@ -0,0 +1,33 @@
+/*
+ * Platform: Code360
+ * Problem ID: 699836
+ * Problem: Encode the Message - Naukri Code 360
+ * Problem Link: https://www.naukri.com/code360/problems/encode-the-message_699836
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static String encode(String s) {
+		// Write your code here.
+		StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                sb.append(s.charAt(i - 1));
+                sb.append(count);
+                count = 1;
+            }
+        }
+        sb.append(s.charAt(s.length() - 1));
+        sb.append(count);
+
+        return sb.toString();
+	}
+}
