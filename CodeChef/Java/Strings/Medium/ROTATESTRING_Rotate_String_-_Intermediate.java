f3d4e3b37b622bbdac75cbe173271b248b593214
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-08 20:08:38 +0530

    Add CodeChef ROTATESTRING (Rotate String Practice Problem in Strings - Intermediate) solution

--- CodeChef/Java/Strings/Medium/ROTATESTRING_Rotate_String_-_Intermediate.java
+++ CodeChef/Java/Strings/Medium/ROTATESTRING_Rotate_String_-_Intermediate.java
@@ -9,4 +9,8 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+public static boolean canRotate(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        String doubled = s + s; // Concatenate s with itself
+        return doubled.contains(goal);
+    }
