faa37ed45b983501054d7c7fa5c36a4bf3023cc0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-08 20:01:30 +0530

    Add Code360 624400 (Two Sum) solution

---
+++ Code360/Java/Miscellaneous/Easy/624400_Two_Sum.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: Code360
+ * Problem ID: 624400
+ * Problem: Two Sum
+ * Problem Link: https://www.naukri.com/code360/problems/highest-occuring-character_624400
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+
+public class Solution {
+
+	public static char highestOccuringChar(String str) {
+		//Your code goes here
+	}
+
+}
