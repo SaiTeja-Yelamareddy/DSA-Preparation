0f3e212ae869099f1bca48af5fdc3a99b397f0e1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:02:25 +0530

    Add CodeChef ON_OFF (N Buttons 1 Bulb Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/ON_OFF_N_Buttons_1_Bulb.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ON_OFF
+ * Problem: N Buttons 1 Bulb Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/ON_OFF
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
