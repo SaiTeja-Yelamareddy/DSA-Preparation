f935d45a79333f2761723567f48a4efb9e1083e4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:50:33 +0530

    Add CodeChef WCC (Submissions for WCC) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/WCC_Submissions_for_WCC.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: WCC
+ * Problem: Submissions for WCC
+ * Problem Link: https://www.codechef.com/STRINGSP01/status/WCC
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
