1f59a61af5f8b584bc5dd2179475f57df61af9eb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:14:32 +0530

    Add Code360 799927 (Reverse the String - Naukri Code 360) solution

---
+++ Code360/Java/Strings/Easy/799927_Reverse_the_String_-_Naukri_Code_360.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: Code360
+ * Problem ID: 799927
+ * Problem: Reverse the String - Naukri Code 360
+ * Problem Link: https://www.naukri.com/code360/problems/reverse-the-string_799927
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {	
+	public static String reverseString(String str) {
+		// Write your code here.
+		    return new StringBuilder(str).reverse().toString();
+
+	}
+}
