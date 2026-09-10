b722dea2c86a8d47dc47b86a2e28842c69a7dde2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:53:56 +0530

    Add CodeChef DDMMORMMDD (DDMM or MMDD Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/DDMMORMMDD_DDMM_or_MMDD.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DDMMORMMDD
+ * Problem: DDMM or MMDD Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/DDMMORMMDD
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
