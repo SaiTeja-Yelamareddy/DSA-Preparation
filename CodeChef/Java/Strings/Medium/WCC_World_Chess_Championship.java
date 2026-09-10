b487012fe68065f3a2290621ffd6bd9434ffb199
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:53:17 +0530

    Add CodeChef WCC (World Chess Championship Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/WCC_World_Chess_Championship.java
+++ CodeChef/Java/Strings/Medium/WCC_World_Chess_Championship.java
@@ -9,12 +9,41 @@
  * Status: ACCEPTED
  */
 
-4
-100
-CCCCCCCCCCCCCC
-400
-CDCDCDCDCDCDCD
-30
-DDCCNNDDDCCNND
-1
-NNDNNDDDNNDNDN
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int X = sc.nextInt();
+            String S = sc.next();
+
+            int carlsenWins = 0;
+            int chefWins = 0;
+
+            for (char ch : S.toCharArray()) {
+                if (ch == 'C') {
+                    carlsenWins++;
+                } else if (ch == 'N') {
+                    chefWins++;
+                }
+            }
+
+            if (carlsenWins > chefWins) {
+                System.out.println(60 * X);
+            } 
+            else if (carlsenWins == chefWins) {
+                System.out.println(55 * X);
+            } 
+            else {
+                System.out.println(40 * X);
+            }
+        }
+    }
+}
