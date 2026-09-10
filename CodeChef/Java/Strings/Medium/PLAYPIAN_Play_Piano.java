c47b1daddfeb964be2f2f8874e58ebda81a25859
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:03:47 +0530

    Add CodeChef PLAYPIAN (Play Piano Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/PLAYPIAN_Play_Piano.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PLAYPIAN
+ * Problem: Play Piano Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/PLAYPIAN
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
