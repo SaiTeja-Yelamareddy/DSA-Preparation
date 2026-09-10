ed147786bb13cdb554b6ce79378d93ee161c23f2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:06:15 +0530

    Add CodeChef ERROR (Chef and Feedback Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/ERROR_Chef_and_Feedback.java
+++ CodeChef/Java/Strings/Medium/ERROR_Chef_and_Feedback.java
@@ -9,6 +9,26 @@
  * Status: ACCEPTED
  */
 
-2
-11111110
-10101010101010
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            String s = sc.next();
+            boolean good = false;
+            for (int i = 0; i <= s.length() - 3; i++) {
+                String sub = s.substring(i, i + 3);
+                if (sub.equals("010") || sub.equals("101")) {
+                    good = true;
+                    break;
+                }
+            }
+            if (good) {
+                System.out.println("Good");
+            } else {
+                System.out.println("Bad");
+            }
+        }
+    }
+}
