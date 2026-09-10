4c697aadb7ab4202580314d27c713f0ecf9540d7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:05:05 +0530

    Add CodeChef ERROR (Chef and Feedback Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/ERROR_Chef_and_Feedback.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ERROR
+ * Problem: Chef and Feedback Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/ERROR
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
