704e8365bb48aecdbd73da2251fc016f55cb1cba
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:57:51 +0530

    Add CodeChef LARGODDSTRIN (Largest Odd Substring Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/LARGODDSTRIN_Largest_Odd_Substring.java
+++ CodeChef/Java/Strings/Medium/LARGODDSTRIN_Largest_Odd_Substring.java
@@ -9,4 +9,15 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+public static String findLargestOddSubstring(String num) {
+
+    for (int i = num.length() - 1; i >= 0; i--) {
+        char ch = num.charAt(i);
+        if (ch == '1' || ch == '3' || ch == '5' ||
+            ch == '7' || ch == '9') {
+            return num.substring(0, i + 1);
+        }
+    }
+
+    return "-1";
+}
