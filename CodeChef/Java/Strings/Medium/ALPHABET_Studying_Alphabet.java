6a47fd9d332b36873bb505511ba90ac57995abe1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:02:12 +0530

    Add CodeChef ALPHABET (Studying Alphabet Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/ALPHABET_Studying_Alphabet.java
+++ CodeChef/Java/Strings/Medium/ALPHABET_Studying_Alphabet.java
@@ -9,4 +9,32 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        boolean[] known = new boolean[26];
+        for (char ch : S.toCharArray()) {
+            known[ch - 'a'] = true;
+        }
+        int N = sc.nextInt();
+        while (N-- > 0) {
+            String word = sc.next();
+            boolean canRead = true;
+            for (char ch : word.toCharArray()) {
+                if (!known[ch - 'a']) {
+                    canRead = false;
+                    break;
+                }
+            }
+            if (canRead) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
