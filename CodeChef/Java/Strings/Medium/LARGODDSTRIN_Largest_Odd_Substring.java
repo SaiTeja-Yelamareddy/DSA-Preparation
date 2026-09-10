b58af2ba000debb45005165c27016bfc67a7df77
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:55:18 +0530

    Add CodeChef LARGODDSTRIN (Largest Odd Substring Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/LARGODDSTRIN_Largest_Odd_Substring.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LARGODDSTRIN
+ * Problem: Largest Odd Substring Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/LARGODDSTRIN
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
