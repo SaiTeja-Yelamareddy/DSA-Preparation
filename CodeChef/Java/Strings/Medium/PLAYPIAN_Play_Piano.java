70f87f4143bc867b71500071f8dd708592c7ff7a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 19:04:56 +0530

    Add CodeChef PLAYPIAN (Play Piano Practice Problem in Strings) solution

--- CodeChef/Java/Strings/Medium/PLAYPIAN_Play_Piano.java
+++ CodeChef/Java/Strings/Medium/PLAYPIAN_Play_Piano.java
@@ -9,8 +9,27 @@
  * Status: ACCEPTED
  */
 
-4
-AB
-ABBA
-ABAABB
-AA
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            String s = sc.next();
+            boolean valid = true;
+            for (int i = 0; i < s.length(); i += 2) {
+                char first = s.charAt(i);
+                char second = s.charAt(i + 1);
+                if (first == second) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
