a530fcae96fbcd8a5fc87db7f3c86850f01fad5a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-10 18:50:03 +0530

    Add CodeChef WCC (World Chess Championship Practice Problem in Strings) solution

---
+++ CodeChef/Java/Strings/Medium/WCC_World_Chess_Championship.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: WCC
+ * Problem: World Chess Championship Practice Problem in Strings
+ * Problem Link: https://www.codechef.com/practice/course/strings-new/STRINGSP01/problems/WCC
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
