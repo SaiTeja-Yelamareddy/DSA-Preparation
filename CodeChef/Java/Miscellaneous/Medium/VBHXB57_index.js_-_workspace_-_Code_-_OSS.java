2fd5a7d4af87a81962ffab341026431314d00143
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 18:27:19 +0530

    Add CodeChef VBHXB57 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB57_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB57_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,13 @@
  * Status: ACCEPTED
  */
 
-const http = require('http');
+            });
 
-// Function to get the appropriate greeting based on current hour
-function getGreeting() {
-    const now = new Date();
-    const hour = now.getHours() +5;
-    
-    if (hour < 12) {
-        return "Good morning!";
-    } else if (hour >= 12 && hour < 18) {
+server.listen(3000, () => {
+        console.log('Server running on port 3000');
+        });
+// Start the server on port 3000
+
+
+
+// will be taught in future lessons
