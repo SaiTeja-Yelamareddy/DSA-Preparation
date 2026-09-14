9209024623c3341d580c95ef3e7d49cc0e6b314f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:35:52 +0530

    Add CodeChef MATREP (Matrix Representation Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATREP_Matrix_Representation_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATREP
+ * Problem: Matrix Representation Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRIXINTRO/problems/MATREP
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
