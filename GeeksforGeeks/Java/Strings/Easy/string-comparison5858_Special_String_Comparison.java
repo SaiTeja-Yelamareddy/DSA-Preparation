94f0b48b67f355072af933080d491f801d0b13ed
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:58:24 +0530

    Add GeeksforGeeks string-comparison5858 (Special String Comparison) solution

--- GeeksforGeeks/Java/Strings/Easy/string-comparison5858_Special_String_Comparison.java
+++ GeeksforGeeks/Java/Strings/Easy/string-comparison5858_Special_String_Comparison.java
@@ -10,30 +10,40 @@
  */
 
 class Solution {
-    public int compareStrings(String s1, String s2) {
-        int i = 0;
-        int j = 0;
+    public int compareString(String s1, String s2) {
+        int i = 0, j = 0;
+
         while (i < s1.length() && j < s2.length()) {
-            int rank1;
-            int rank2;
-            int len1 = 1;
-            int len2 = 1;
-            if (i + 1 < s1.length() &&
-                s1.charAt(i) == 'n' &&
-                s1.charAt(i + 1) == 'g') {
+
+            int rank1, rank2;
+            int len1 = 1, len2 = 1;
+
+            // s1
+            if (i + 1 < s1.length()
+                    && s1.charAt(i) == 'n'
+                    && s1.charAt(i + 1) == 'g') {
                 rank1 = 14;
                 len1 = 2;
             } else {
                 rank1 = s1.charAt(i) - 'a';
+
+                if (rank1 >= 14) {
+                    rank1++;
+                }
             }
 
-            if (j + 1 < s2.length() &&
-                s2.charAt(j) == 'n' &&
-                s2.charAt(j + 1) == 'g') {
+            // s2
+            if (j + 1 < s2.length()
+                    && s2.charAt(j) == 'n'
+                    && s2.charAt(j + 1) == 'g') {
                 rank2 = 14;
                 len2 = 2;
             } else {
                 rank2 = s2.charAt(j) - 'a';
+
+                if (rank2 >= 14) {
+                    rank2++;
+                }
             }
 
             if (rank1 > rank2) {
@@ -47,13 +57,11 @@ class Solution {
             i += len1;
             j += len2;
         }
+
         if (i == s1.length() && j == s2.length()) {
             return 0;
         }
-        if (i < s1.length()) {
-            return 1;
-        }
 
-        return -1;
+        return i < s1.length() ? 1 : -1;
     }
 }
