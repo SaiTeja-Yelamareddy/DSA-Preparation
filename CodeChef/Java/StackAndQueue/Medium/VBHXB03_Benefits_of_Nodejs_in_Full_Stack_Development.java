22755539d4e4de4f126ce7a9a6cd94ce92d59a2c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:55:57 +0530

    Add CodeChef VBHXB03 (Benefits of Nodejs in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/VBHXB03_Benefits_of_Nodejs_in_Full_Stack_Development.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB03
+ * Problem: Benefits of Nodejs in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA92/problems/VBHXB03
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA92
+ * Status: ACCEPTED
+ */
+
+// Define the Celsius temperature
+
+// Convert Celsius to Fahrenheit using the formula: F = (C * 9/5) + 32
+
+
+
+// Print the result
+console.log(`${celsius}°C is equal to ${fahrenheit}°F`);
