bceefc615aff59d645933867a21e69693a1bff79
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:36:39 +0530

    Add CodeChef MATADD (Add Two Matrices Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATADD_Add_Two_Matrices_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATADD
+ * Problem: Add Two Matrices Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATADD
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
