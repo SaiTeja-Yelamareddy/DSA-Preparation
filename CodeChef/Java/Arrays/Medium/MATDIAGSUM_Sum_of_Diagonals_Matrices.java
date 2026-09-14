8d73077081de295d7bef3b633377a85019c689c7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:38:40 +0530

    Add CodeChef MATDIAGSUM (Sum of Diagonals Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATDIAGSUM_Sum_of_Diagonals_Matrices.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATDIAGSUM
+ * Problem: Sum of Diagonals Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATDIAGSUM
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3
+3 1 5
+8 2 1
+4 6 0
