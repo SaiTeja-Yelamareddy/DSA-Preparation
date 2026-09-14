492e1b386309bf6a01d061aea6f0158cf4582a43
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 19:30:59 +0530

    Add CodeChef VBHXB64 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB64_Code_-_OSS.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB64
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA96/problems/VBHXB64
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA96
+ * Status: ACCEPTED
+ */
+
+4;
+                                                                                                            res.end('404 Not Found');
+                                                                                                                }
+                                                                                                                });
+
+                                                                                                                server.listen(3000);
