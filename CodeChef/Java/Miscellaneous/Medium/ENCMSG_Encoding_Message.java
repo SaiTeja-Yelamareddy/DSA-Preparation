0c99a64dfb6ff095d3c0531c7904c6be4c06bf0a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:39:19 +0530

    Add CodeChef ENCMSG (Encoding Message Practice Problem in 1000 to 1400 difficulty problems) solution

--- CodeChef/Java/Miscellaneous/Medium/ENCMSG_Encoding_Message.java
+++ CodeChef/Java/Miscellaneous/Medium/ENCMSG_Encoding_Message.java
@@ -9,4 +9,26 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            String s = sc.next();
+            char[] arr = s.toCharArray();
+            for (int i = 0; i < N - 1; i += 2) {
+                char temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+            for (int i = 0; i < N; i++) {
+                arr[i] = (char) ('z' - (arr[i] - 'a'));
+            }
+
+            System.out.println(new String(arr));
+        }
+    }
+}
