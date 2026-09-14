ce1d145835a36b194b4959ce49cb9d95bcde050e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:22:58 +0530

    Add GeeksforGeeks rearrange-string-as-alternate-x-and-y-occurences3835 (Make x times 0's and y times 1's) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/rearrange-string-as-alternate-x-and-y-occurences3835_Make_x_times_0's_and_y_times_1's.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: rearrange-string-as-alternate-x-and-y-occurences3835
+ * Problem: Make x times 0's and y times 1's
+ * Problem Link: https://www.geeksforgeeks.org/problems/rearrange-string-as-alternate-x-and-y-occurences3835/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String arrangeString(String s, int x, int y) {
+        // code here
+        
+    }
+}
