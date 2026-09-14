bb3e514aae728f1cefd8641a847821bcc64fc3ef
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:37:19 +0530

    Add CodeChef MATZIGZAG (Zig-zag traversal Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATZIGZAG_Zig-zag_traversal_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATZIGZAG
+ * Problem: Zig-zag traversal Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATZIGZAG
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
