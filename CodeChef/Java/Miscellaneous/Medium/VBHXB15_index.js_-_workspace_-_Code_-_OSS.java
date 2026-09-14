301b8592dd85873cf66af9a4cafdd5f0718e34ce
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:08:20 +0530

    Add CodeChef VBHXB15 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB15_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB15
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA93/problems/VBHXB15
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA93
+ * Status: ACCEPTED
+ */
+
+function grindBeans() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Ground coffee beans");
+      }, 500);
+    });
+  }
+  
+  function brewCoffee(groundBeans) {
+    return new Promise((resolve) => {
