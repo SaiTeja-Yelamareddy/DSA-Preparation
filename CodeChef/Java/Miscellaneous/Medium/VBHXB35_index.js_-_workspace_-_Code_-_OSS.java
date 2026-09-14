8adc22506f8b2e29d98f8b73ecee78e4fcc20197
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:37:24 +0530

    Add CodeChef VBHXB35 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB35_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB35
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA94/problems/VBHXB35
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA94
+ * Status: ACCEPTED
+ */
+
+function runCountdown() {
+  ____(() => { // TODO: run immediately after current code
+    console.log("Countdown is starting...");
+
+    let seconds = 3;
+
+    const intervalId = ____(() => { // TODO: run repeatedly
+      console.log(`${seconds} seconds left`);
+      seconds--;
+
