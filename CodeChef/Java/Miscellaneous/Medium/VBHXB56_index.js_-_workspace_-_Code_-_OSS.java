1b431fd244c8a5f96c1bbc80f34b833ce16917d7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 18:21:21 +0530

    Add CodeChef VBHXB56 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB56_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB56
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA96/problems/VBHXB56
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA96
+ * Status: ACCEPTED
+ */
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // complete the code to read the file
+  ________________________________________________ {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error reading file!');
+      return;
