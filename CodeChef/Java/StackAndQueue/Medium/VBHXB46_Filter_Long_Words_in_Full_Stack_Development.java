995632dd59708ce604a224d746efdeb614c911f4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:56:50 +0530

    Add CodeChef VBHXB46 (Filter Long Words in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/VBHXB46_Filter_Long_Words_in_Full_Stack_Development.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB46
+ * Problem: Filter Long Words in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB46
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+// write your code here
+
