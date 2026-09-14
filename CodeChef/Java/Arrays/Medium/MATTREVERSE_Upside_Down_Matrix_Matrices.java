8d18b64fa082c9e572700e290e596903035131b7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:37:55 +0530

    Add CodeChef MATTREVERSE (Upside Down Matrix Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATTREVERSE_Upside_Down_Matrix_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATTREVERSE
+ * Problem: Upside Down Matrix Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATTREVERSE
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
