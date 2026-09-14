7b93ccd649a27d8f9c44bd2103b938d7b6b6b2d6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:07:06 +0530

    Add GeeksforGeeks fizz-buzz (Fizz Buzz) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/fizz-buzz_Fizz_Buzz.java
@@ -0,0 +1,32 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: fizz-buzz
+ * Problem: Fizz Buzz
+ * Problem Link: https://www.geeksforgeeks.org/problems/fizz-buzz/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public static ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                result.add("Fizz");
+            }
+            else if (i % 5 == 0) {
+                result.add("Buzz");
+            }
+            else {
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
+    }
+}
