e6911dc8be5b0b2d746a83694d7af909d45956f9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:44:41 +0530

    Add CodeChef VBHXB40 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB40_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB40
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB40
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+const path = require('path');
+const os = require('os');
+
+const filePath = '/users/john/documents/report.txt';
+
+//
+
+console.log('Extracted filename:', fileName);
+console.log('Operating System Platform:', platform);
