438fc5c9f0af8a63adc81429abd7832033410c3e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:03:43 +0530

    Add CodeChef VBHXB12 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB12_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB12
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA93/problems/VBHXB12
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA93
+ * Status: ACCEPTED
+ */
+
+console.log("Hello!");
+
+// complete the code from here:
