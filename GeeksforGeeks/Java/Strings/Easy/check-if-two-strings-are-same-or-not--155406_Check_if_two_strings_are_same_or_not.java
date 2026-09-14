9676bd30d29eecb69b2f54efbea3730bb30b638c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:11:59 +0530

    Add GeeksforGeeks check-if-two-strings-are-same-or-not--155406 (Check if two strings are same or not) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/check-if-two-strings-are-same-or-not--155406_Check_if_two_strings_are_same_or_not.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: check-if-two-strings-are-same-or-not--155406
+ * Problem: Check if two strings are same or not
+ * Problem Link: https://www.geeksforgeeks.org/problems/check-if-two-strings-are-same-or-not--155406/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public static boolean areStringsSame(String s1, String s2) {
+        // code here
+         return s1.equals(s2);
+    }
+}
