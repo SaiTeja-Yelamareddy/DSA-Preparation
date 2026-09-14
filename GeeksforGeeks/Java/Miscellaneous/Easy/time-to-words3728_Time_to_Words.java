25888478295fe98494f1538c01fbc84d5fdd2f6a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:27:55 +0530

    Add GeeksforGeeks time-to-words3728 (Time to Words) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/time-to-words3728_Time_to_Words.java
@@ -0,0 +1,50 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: time-to-words3728
+ * Problem: Time to Words
+ * Problem Link: https://www.geeksforgeeks.org/problems/time-to-words3728/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+
+class Solution {
+    public String timeToWords(int h, int m) {
+
+        String[] numbers = {
+            "", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen",
+            "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen", "twenty", "twenty-one",
+            "twenty-two", "twenty-three", "twenty-four",
+            "twenty-five", "twenty-six", "twenty-seven",
+            "twenty-eight", "twenty-nine"
+        };
+        if (m == 0) {
+            return numbers[h] + " o' clock";
+        }
+        if (m == 15) {
+            return "quarter past " + numbers[h];
+        }
+        if (m == 30) {
+            return "half past " + numbers[h];
+        }
+        if (m < 30) {
+            return numbers[m] + " minutes past " + numbers[h];
+        }
+        int remaining = 60 - m;
+        int nextHour = h + 1;
+        if (nextHour == 12) {
+            nextHour = 12;
+        }
+
+        if (remaining == 15) {
+            return "quarter to " + numbers[nextHour];
+        }
+
+        return numbers[remaining] + " minutes to " + numbers[nextHour];
+    }
+}
