39f4af612ebabeb4a2010408b15b83267cafafcf
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:24:41 +0530

    Add GeeksforGeeks count-distinct-vowels-in-string (Count Distinct Vowels in String) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/count-distinct-vowels-in-string_Count_Distinct_Vowels_in_String.java
@@ -0,0 +1,25 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: count-distinct-vowels-in-string
+ * Problem: Count Distinct Vowels in String
+ * Problem Link: https://www.geeksforgeeks.org/problems/count-distinct-vowels-in-string/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+
+class Solution {
+    public int countVowels(String s) {
+        HashSet<Character> vowels = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                vowels.add(ch);
+            }
+        }
+        return vowels.size();
+    }
+}
