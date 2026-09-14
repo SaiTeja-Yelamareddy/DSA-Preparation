eeb9e7a4361d3f9a874c474bb218a599b1b9f9a7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:59:19 +0530

    Add CodeChef VBHXB05 (greeting.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/VBHXB05_greeting.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB05_greeting.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,12 @@
  * Status: ACCEPTED
  */
 
-const options = { timeZone: 'Asia/Kolkata', hour: 'numeric', hour12: false };
-const currentHour = new Intl.DateTimeFormat('en-US', options).format(new Date());
+        greeting = "Good Morning!";
+        } else if (currentHour < 18) {
+            greeting = "Good Afternoon!";
+            } else {
+                greeting = "Good Evening!";
+                }
 
-let greeting;
-
-if (currentHour < 12) {
-    greeting = "Good Morning!";
-}
-// complete your code from here
-
+console.log(currentHour);
+console.log(greeting);
