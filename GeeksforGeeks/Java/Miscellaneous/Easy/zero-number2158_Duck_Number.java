61b209a8ec485e6528e014e4a1927a81db4ab8a4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:53:43 +0530

    Add GeeksforGeeks zero-number2158 (Duck Number) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/zero-number2158_Duck_Number.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: zero-number2158
+ * Problem: Duck Number
+ * Problem Link: https://www.geeksforgeeks.org/problems/zero-number2158/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static boolean check_duck(String num) {
+        // code here
+        for (int i = 1; i < num.length(); i++) {
+                    if (num.charAt(i) == '0') {
+                        return "YES";
+                    }
+                }
+
+                return "NO";
+    }
+}
