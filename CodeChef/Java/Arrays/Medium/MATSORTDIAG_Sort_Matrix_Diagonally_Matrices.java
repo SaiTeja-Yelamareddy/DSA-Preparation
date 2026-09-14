9ec3dc93d646a4c1977c0b40c1400901e9c9f431
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:41:39 +0530

    Add CodeChef MATSORTDIAG (Sort Matrix Diagonally Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATSORTDIAG_Sort_Matrix_Diagonally_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATSORTDIAG
+ * Problem: Sort Matrix Diagonally Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATSORTDIAG
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
