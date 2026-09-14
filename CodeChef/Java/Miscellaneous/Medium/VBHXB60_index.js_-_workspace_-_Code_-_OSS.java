728c99a1f1f7c050ef51c8c675a13d4f25a4b1a9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 18:30:17 +0530

    Add CodeChef VBHXB60 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB60_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB60
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA96/problems/VBHXB60
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA96
+ * Status: ACCEPTED
+ */
+
+// write your code here
