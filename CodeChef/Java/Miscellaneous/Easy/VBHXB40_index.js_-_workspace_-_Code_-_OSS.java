d960e006701970826099ec8fedf725e3f4498b9d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:46:11 +0530

    Add CodeChef VBHXB40 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/VBHXB40_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB40_index.js_-_workspace_-_Code_-_OSS.java
@@ -17,6 +17,7 @@ const os = require('os');
 const filePath = '/users/john/documents/report.txt';
 
 //
+const fileName = path.basename(filePath);
+const platform = os.platform();
 
 console.log('Extracted filename:', fileName);
-console.log('Operating System Platform:', platform);
