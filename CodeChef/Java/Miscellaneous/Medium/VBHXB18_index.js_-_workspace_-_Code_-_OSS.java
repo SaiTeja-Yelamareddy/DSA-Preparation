c39276be9b8cf641465493d4f744dcd818f56a3b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:13:57 +0530

    Add CodeChef VBHXB18 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB18_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB18_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,6 @@
  * Status: ACCEPTED
  */
 
-function grindCoffeeBeans() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve("Coffee beans ground!");
-      }, 1000); // Simulate grinding for 1 second
-    });
-  }
-  
-  function brewCoffee() {
-    return new Promise(resolve => {
+                                                                  placeOrder().then(result => {
+                                                                                                                                                                                              console.log(result);
+                                                                                                                                                                                              });                                                                                                                                                                                   
