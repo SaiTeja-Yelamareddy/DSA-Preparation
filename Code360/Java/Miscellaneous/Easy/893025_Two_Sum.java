b9ea07240ce4aebae2c19b1177e73cf965a57181
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:17:55 +0530

    Add Code360 893025 (Two Sum) solution

---
+++ Code360/Java/Miscellaneous/Easy/893025_Two_Sum.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: Code360
+ * Problem ID: 893025
+ * Problem: Two Sum
+ * Problem Link: https://www.naukri.com/code360/problems/2-sum_893025
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+
+public class Solution {
+    public static int[] twoSum(int arr[], int n,int target) {
+        // Write your code here.
+        
+    }
+}
