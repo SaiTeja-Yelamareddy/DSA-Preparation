8894abb80680183489eb13967845675d59384aab
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:22:18 +0530

    Add CodeChef TWOSUMM (Chef and Pair of Elements Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/Medium/TWOSUMM_Chef_and_Pair_of_Elements.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TWOSUMM
+ * Problem: Chef and Pair of Elements Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/TWOSUMM
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
