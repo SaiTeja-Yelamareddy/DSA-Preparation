61592310b3cd850db5573cb807f20d8209be62f1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:39:28 +0530

    Add CodeChef MATNEGCOUNT (Count Negative Numbers Practice Problem in 2D Array / Matrices) solution

--- CodeChef/Java/Arrays/Medium/MATNEGCOUNT_Count_Negative_Numbers_Matrices.java
+++ CodeChef/Java/Arrays/Medium/MATNEGCOUNT_Count_Negative_Numbers_Matrices.java
@@ -9,7 +9,40 @@
  * Status: ACCEPTED
  */
 
-3 4
-8 7 6 -1
-7 7 -1 -2
-4 -5 -6 -7
+import java.util.Scanner;
+
+public class Main {
+    public static int countNegatives(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row_index = 0;
+        int col_index = cols - 1;
+        int negatives_count = 0;
+        
+        while (row_index < rows) {
+            while (col_index >= 0 && matrix[row_index][col_index] < 0) {
+                col_index--;
+            }
+            negatives_count += cols - col_index - 1;
+            row_index++;
+        }
+        
+        return negatives_count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] mat = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println(countNegatives(mat));
+    }
+}
