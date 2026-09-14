bdd93a47fe314d6d1aed96b7a41803658102d9fb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:38:15 +0530

    Add CodeChef ENCMSG (Encoding Message Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/ENCMSG_Encoding_Message.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ENCMSG
+ * Problem: Encoding Message Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ENCMSG
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
