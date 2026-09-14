b6ce3feb5ec18f24f8ab72786aed1abc84c05eac
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:55:59 +0530

    Add CodeChef VBHXB03 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB03_Code_-_OSS.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB03
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA92/problems/VBHXB03
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA92
+ * Status: ACCEPTED
+ */
+
+// Define the Celsius temperature
+
+// Convert Celsius to Fahrenheit using the formula: F = (C * 9/5) + 32
+
+
+
+// Print the result
+console.log(`${celsius}°C is equal to ${fahrenheit}°F`);
