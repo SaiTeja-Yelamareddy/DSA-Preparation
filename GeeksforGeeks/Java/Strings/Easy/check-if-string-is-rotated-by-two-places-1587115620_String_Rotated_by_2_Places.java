753c3866fde1c0efe996568fca57cd84e03c30bb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:43:20 +0530

    Add GeeksforGeeks check-if-string-is-rotated-by-two-places-1587115620 (String Rotated by 2 Places) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/check-if-string-is-rotated-by-two-places-1587115620_String_Rotated_by_2_Places.java
@@ -0,0 +1,28 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: check-if-string-is-rotated-by-two-places-1587115620
+ * Problem: String Rotated by 2 Places
+ * Problem Link: https://www.geeksforgeeks.org/problems/check-if-string-is-rotated-by-two-places-1587115620/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public static boolean isRotated(String s1, String s2) {
+        // code here
+        
+    }
+}
+class Solution {
+    public boolean isRotated(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int n = s1.length();
+        String left = s1.substring(2 % n) + s1.substring(0, 2 % n);
+        String right = s1.substring(n - (2 % n)) + s1.substring(0, n - (2 % n));
+        return s2.equals(left) || s2.equals(right);
+    }
+}
