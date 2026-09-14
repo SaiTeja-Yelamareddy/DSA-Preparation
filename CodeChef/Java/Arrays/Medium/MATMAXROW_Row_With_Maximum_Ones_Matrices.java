bce4474b0af201465f9bb5221ba3dfb31787e14b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:44:13 +0530

    Add CodeChef MATMAXROW (Row With Maximum Ones Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATMAXROW_Row_With_Maximum_Ones_Matrices.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATMAXROW
+ * Problem: Row With Maximum Ones Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATMAXROW
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3 3
+0 1 1
+0 1 1
+0 1 1
