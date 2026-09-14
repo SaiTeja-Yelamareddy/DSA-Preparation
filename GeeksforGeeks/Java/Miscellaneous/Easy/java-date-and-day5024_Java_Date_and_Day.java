23b2ee396c5fed76b919edbb9e1d1eadb83a36b7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:03:30 +0530

    Add GeeksforGeeks java-date-and-day5024 (Java Date and Day) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/java-date-and-day5024_Java_Date_and_Day.java
@@ -0,0 +1,31 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: java-date-and-day5024
+ * Problem: Java Date and Day
+ * Problem Link: https://www.geeksforgeeks.org/problems/java-date-and-day5024/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static String findDay(int Day, int Month, int Year) {
+        // Code Here
+        Calendar cal = Calendar.getInstance();
+
+               cal.set(year, month - 1, date);
+
+               String[] days = {
+                   "SUNDAY",
+                   "MONDAY",
+                   "TUESDAY",
+                   "WEDNESDAY",
+                   "THURSDAY",
+                   "FRIDAY",
+                   "SATURDAY"
+               };
+
+               return days[cal.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+}
