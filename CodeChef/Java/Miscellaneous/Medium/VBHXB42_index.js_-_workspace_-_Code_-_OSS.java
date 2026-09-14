761051e634cdfe41b654d2cd887148a9c2caf0be
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:46:24 +0530

    Add CodeChef VBHXB42 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB42_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB42
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB42
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+const fs = require('fs');
+
+const initialFilePath = 'file.txt';
+const renamedFilePath = 'newFile.txt';
+
+// Check if the initial file exists
+if () {
+    // Create the file if it doesn't exist
+    try {
+        fs.writeFileSync(initialFilePath, ''); // creates an empty file
