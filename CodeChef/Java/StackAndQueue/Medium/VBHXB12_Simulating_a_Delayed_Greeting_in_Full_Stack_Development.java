bfb5dc48d58a002362b8a283911ec164395ea9ed
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:05:35 +0530

    Add CodeChef VBHXB12 (Simulating a Delayed Greeting in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/VBHXB12_Simulating_a_Delayed_Greeting_in_Full_Stack_Development.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB12
+ * Problem: Simulating a Delayed Greeting in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA93/problems/VBHXB12
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA93
+ * Status: ACCEPTED
+ */
+
+
+console.log("Hello!");
+
+setTimeout(() => {
+    console.log("World!");
+    }, 1000);
+
+    console.log("Goodbye!");
+// complete the code from here:
