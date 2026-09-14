eb7162569db8dcc4cb6eaac8018fc2779ca126aa
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:46:56 +0530

    Add CodeChef VBHXB43 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB43_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB43
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB43
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+// Hardcoded file name
+const initialFileName = 'example.txt';
+const organizedFileName = 'organized_file.txt';
+
+const initialFilePath = path.resolve(__dirname, initialFileName);
+const organizedFilePath = path.resolve(__dirname, organizedFileName);
+
