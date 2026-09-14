15a81fba0979391deaf9d78b0ea959dcfb12ded5
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:44:41 +0530

    Add CodeChef MATEQRC (Equal Rows and Columns Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATEQRC_Equal_Rows_and_Columns_Matrices.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATEQRC
+ * Problem: Equal Rows and Columns Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATEQRC
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
