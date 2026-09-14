a5f00f0e82c2e07b1606b4e53a6f6b6113b074fa
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:30:38 +0530

    Add GeeksforGeeks snake-case0219 (Snake Case) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/snake-case0219_Snake_Case.java
@@ -0,0 +1,26 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: snake-case0219
+ * Problem: Snake Case
+ * Problem Link: https://www.geeksforgeeks.org/problems/snake-case0219/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String SnakeCase(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == ' ') {
+                result.append('_');
+            } else {
+                result.append(Character.toLowerCase(ch));
+            }
+        }
+        return result.toString();
+    }
+}
