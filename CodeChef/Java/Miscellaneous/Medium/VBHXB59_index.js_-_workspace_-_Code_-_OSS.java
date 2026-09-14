c16809a67db2ed8d944c924e036814835a783ae9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 18:29:12 +0530

    Add CodeChef VBHXB59 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB59_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB59
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA96/problems/VBHXB59
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA96
+ * Status: ACCEPTED
+ */
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Set the Content-Type header to HTML
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  // complete the code to write the HTML content
+  ____________(`
+    <!DOCTYPE html>
+    <html>
