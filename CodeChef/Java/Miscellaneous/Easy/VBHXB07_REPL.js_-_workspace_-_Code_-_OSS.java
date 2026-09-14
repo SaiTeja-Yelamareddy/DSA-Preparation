bb8429b26686ee01ff2bf8868bdefcec809d569a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:59:35 +0530

    Add CodeChef VBHXB07 (REPL.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB07_REPL.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB07
+ * Problem: REPL.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA92/problems/VBHXB07
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA92
+ * Status: ACCEPTED
+ */
+
+let width = 5;
+let height = 10;
+let area = width * height;
+console.log("The area of the rectangle is: " + area);
