9e442ecacf74e2a21e2d630857e87a0904732e1b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 19:31:46 +0530

    Add CodeChef BUY1GET1 (Buy1-Get1 Practice Problem in 1000 to 1400 difficulty problems) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/BUY1GET1_Buy1-Get1.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: BUY1GET1
+ * Problem: Buy1-Get1 Practice Problem in 1000 to 1400 difficulty problems
+ * Problem Link: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/BUY1GET1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
