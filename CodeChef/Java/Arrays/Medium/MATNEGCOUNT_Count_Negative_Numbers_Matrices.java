54cbc269d67a60907143635dea17d7c48dae050b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:39:07 +0530

    Add CodeChef MATNEGCOUNT (Count Negative Numbers Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATNEGCOUNT_Count_Negative_Numbers_Matrices.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATNEGCOUNT
+ * Problem: Count Negative Numbers Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATNEGCOUNT
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3 4
+8 7 6 -1
+7 7 -1 -2
+4 -5 -6 -7
