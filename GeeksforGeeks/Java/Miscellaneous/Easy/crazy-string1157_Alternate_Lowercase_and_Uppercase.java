59e40765266c07dabbf1c7377676ae5e4249e1f4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:23:15 +0530

    Add GeeksforGeeks crazy-string1157 (Alternate Lowercase and Uppercase) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/crazy-string1157_Alternate_Lowercase_and_Uppercase.java
@@ -0,0 +1,35 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: crazy-string1157
+ * Problem: Alternate Lowercase and Uppercase
+ * Problem Link: https://www.geeksforgeeks.org/problems/crazy-string1157/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String altCase(String s) {
+
+        StringBuilder result = new StringBuilder();
+        boolean firstUpper = Character.isUpperCase(s.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (firstUpper) {
+                if (i % 2 == 0) {
+                    result.append(Character.toUpperCase(ch));
+                } else {
+                    result.append(Character.toLowerCase(ch));
+                }
+            } else {
+                if (i % 2 == 0) {
+                    result.append(Character.toLowerCase(ch));
+                } else {
+                    result.append(Character.toUpperCase(ch));
+                }
+            }
+        }
+        return result.toString();
+    }
+}
