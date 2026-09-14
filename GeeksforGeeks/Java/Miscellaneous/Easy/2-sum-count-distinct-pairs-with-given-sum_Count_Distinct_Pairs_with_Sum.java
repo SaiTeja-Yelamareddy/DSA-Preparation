247f254c49f17928db92d15ca33207a53645cad8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:09:35 +0530

    Add GeeksforGeeks 2-sum-count-distinct-pairs-with-given-sum (Count Distinct Pairs with Sum) solution

---
+++ GeeksforGeeks/Java/Miscellaneous/Easy/2-sum-count-distinct-pairs-with-given-sum_Count_Distinct_Pairs_with_Sum.java
@@ -0,0 +1,33 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: 2-sum-count-distinct-pairs-with-given-sum
+ * Problem: Count Distinct Pairs with Sum
+ * Problem Link: https://www.geeksforgeeks.org/problems/2-sum-count-distinct-pairs-with-given-sum/1
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    static int countDistinctPairs(int arr[], int target) {
+        // code here
+        
+    }
+}
+class Solution {
+    public int countDistinctPairs(int[] arr, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<String> pairs = new HashSet<>();
+        for (int num : arr) {
+            int needed = target - num;
+            if (set.contains(needed)) {
+                int a = Math.min(num, needed);
+                int b = Math.max(num, needed);
+                pairs.add(a + "," + b);
+            }
+            set.add(num);
+        }
+        return pairs.size();
+    }
+}
