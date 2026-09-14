9ecb6cb16b4c912f9ecc2d4378f277aea50ec8fd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:38:01 +0530

    Add CodeChef TWOSUMM (Chef and Pair of Elements Practice Problem in Intermediate Arrays and 2D Arrays) solution

--- CodeChef/Java/Arrays/Medium/TWOSUMM_Chef_and_Pair_of_Elements.java
+++ CodeChef/Java/Arrays/Medium/TWOSUMM_Chef_and_Pair_of_Elements.java
@@ -10,5 +10,13 @@
  */
 
 public static int[] findPair(int[] nums, int target) {
-    // Write your code here
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[]{map.get(complement), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[]{}; 
 }
