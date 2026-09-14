951470c0e9adf74350622219c4b4d458aa81d25a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:02:58 +0530

    Add CodeChef VBHXB08 (package.json - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB08_package.json_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB08
+ * Problem: package.json - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA92/problems/VBHXB08
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA92
+ * Status: ACCEPTED
+ */
+
+{
+      "name": "my-node-app",
+        "version": "1.0.0",
+          "main": "index.js",
+            "scripts": {
+                "start": "node index.js"
+                  }
+                  }
