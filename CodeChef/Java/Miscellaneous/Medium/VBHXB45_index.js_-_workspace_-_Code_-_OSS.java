a1cc062e66eed87a41ed4629def67b9d1da003cb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:48:51 +0530

    Add CodeChef VBHXB45 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB45_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB45
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB45
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const inputFile = path.resolve(__dirname, 'fruits.txt');
+const outputFile = path.resolve(__dirname, 'filtered_fruits.txt');
+
+console.log('Reading fruits.txt...');
+
+// complete the code to read the inputFile
+________________________________________ {
