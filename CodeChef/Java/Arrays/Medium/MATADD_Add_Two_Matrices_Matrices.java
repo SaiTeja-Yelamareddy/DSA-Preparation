b0511a2e1a89143864406d2d1aebc8d146a269ad
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:37:02 +0530

    Add CodeChef MATADD (Add Two Matrices Practice Problem in 2D Array / Matrices) solution

--- CodeChef/Java/Arrays/Medium/MATADD_Add_Two_Matrices_Matrices.java
+++ CodeChef/Java/Arrays/Medium/MATADD_Add_Two_Matrices_Matrices.java
@@ -9,4 +9,34 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                b[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
