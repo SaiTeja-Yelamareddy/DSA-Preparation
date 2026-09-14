04ec322a3704045dc970ce5918ec34a20ef30477
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:08:58 +0530

    Add CodeChef VBHXB16 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB16_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB16
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA93/problems/VBHXB16
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA93
+ * Status: ACCEPTED
+ */
+
+function getIngredients() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Ingredients acquired");
+    }, 500);
+  });
+}
+
+function mixIngredients(ingredients) {
+  return new Promise((resolve) => {
