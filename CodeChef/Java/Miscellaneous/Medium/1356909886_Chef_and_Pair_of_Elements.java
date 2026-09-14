bab8c14f4501021ec16f00bf7ce8c6a11d87d796
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 15:23:10 +0530

    Add CodeChef 1356909886 (Chef and Pair of Elements) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/1356909886_Chef_and_Pair_of_Elements.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: 1356909886
+ * Problem: Chef and Pair of Elements
+ * Problem Link: https://www.codechef.com/viewsolution/1356909886
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+public static int[] findPair(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[]{map.get(complement), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[]{}; 
+}
+
