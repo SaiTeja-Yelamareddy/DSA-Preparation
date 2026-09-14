ccfe84245b7b34adfe9053336b23bb9917d957b9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 17:05:50 +0530

    Add CodeChef VBHXB13 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB13_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB13
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA93/problems/VBHXB13
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA93
+ * Status: ACCEPTED
+ */
+
+// Synchronous message
+
+// Asynchronous operation with 2-second delay
+
+// Synchronous message continues immediately
