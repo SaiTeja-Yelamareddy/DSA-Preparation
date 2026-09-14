e184528913c7a8b7f203f2ac3c5a580247e26cf7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:14:16 +0530

    Add GeeksforGeeks replace-a-word5553 (Replace Word) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/replace-a-word5553_Replace_Word.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/replace-a-word5553_Replace_Word.java
@@ -11,7 +11,24 @@
 
 class Solution {
     public String replaceAll(String s, String s1, String s2) {
-        // code here
-         return s.replace(s1, s2);
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+
+        while (i < s.length()) {
+
+            if (i + s1.length() <= s.length()
+                    && s.regionMatches(i, s1, 0, s1.length())) {
+
+                result.append(s2);
+                i += s1.length();
+
+            } else {
+                result.append(s.charAt(i));
+                i++;
+            }
+        }
+
+        return result.toString();
     }
 }
