ff080a0e2c9e4150e6b5e40f5ca33b8d7f3cef88
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:32:17 +0530

    Add GeeksforGeeks kth-boom-number5609 (Kth Boom Number) solution

--- GeeksforGeeks/Java/Miscellaneous/Easy/kth-boom-number5609_Kth_Boom_Number.java
+++ GeeksforGeeks/Java/Miscellaneous/Easy/kth-boom-number5609_Kth_Boom_Number.java
@@ -11,22 +11,19 @@
 
 class Solution {
     public String boomNumber(int k) {
-        StringBuilder ans = new StringBuilder();
         int length = 1;
         while (k > (1 << length)) {
             k -= (1 << length);
             length++;
         }
-        String binary = Integer.toBinaryString(k);
-        for (int i = 1; i < binary.length(); i++) {
-            if (binary.charAt(i) == '0') {
+        int value = k - 1;
+        StringBuilder ans = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            if ((value & (1 << i)) == 0) {
                 ans.append('2');
             } else {
                 ans.append('3');
             }
-        }
-        while (ans.length() < length) {
-            ans.insert(0, '2');
         }
 
         return ans.toString();
