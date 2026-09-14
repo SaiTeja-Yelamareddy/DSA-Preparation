e20ad30c8af104a1cd9c78228dda5d4deefa60ce
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 14:10:09 +0530

    Add GeeksforGeeks extraction-of-secret-message0616 (Replace Substring with Space) solution

---
+++ GeeksforGeeks/Java/Strings/Easy/extraction-of-secret-message0616_Replace_Substring_with_Space.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: extraction-of-secret-message0616
+ * Problem: Replace Substring with Space
+ * Problem Link: https://www.geeksforgeeks.org/problems/extraction-of-secret-message0616/1
+ * Language: Java
+ * Concept: Strings
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public String extractMessage(String s) {
+        // code here
+        return s.replace("LIE"," ");
+    }
+}
