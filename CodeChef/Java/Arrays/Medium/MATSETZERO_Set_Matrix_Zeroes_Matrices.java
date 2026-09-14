a9143441d606476e83852b73aa3a5b5562b14ccb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:42:16 +0530

    Add CodeChef MATSETZERO (Set Matrix Zeroes Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/Java/Arrays/Medium/MATSETZERO_Set_Matrix_Zeroes_Matrices.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MATSETZERO
+ * Problem: Set Matrix Zeroes Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/MATSETZERO
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3 3
+4 6 0
+8 2 1
+3 1 5
