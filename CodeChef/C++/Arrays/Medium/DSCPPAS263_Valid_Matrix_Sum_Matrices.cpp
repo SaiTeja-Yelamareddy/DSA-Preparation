8966902c448d74c7d5033736732f4f9debed5206
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 16:40:34 +0530

    Add CodeChef DSCPPAS263 (Valid Matrix Sum Practice Problem in 2D Array / Matrices) solution

---
+++ CodeChef/C++/Arrays/Medium/DSCPPAS263_Valid_Matrix_Sum_Matrices.cpp
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSCPPAS263
+ * Problem: Valid Matrix Sum Practice Problem in 2D Array / Matrices
+ * Problem Link: https://www.codechef.com/practice/course/matrices/MATRICES/problems/DSCPPAS263
+ * Language: C++
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
