9f688c0844d715ff405650a45d3271e8e1a707e4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-14 18:15:55 +0530

    Add CodeChef VBHXB49 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB49_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB49
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA95/problems/VBHXB49
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA95
+ * Status: ACCEPTED
+ */
+
+// write your code here
