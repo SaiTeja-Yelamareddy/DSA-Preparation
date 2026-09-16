26a4f6ace5bcb224575f68ec8e6fcd3088ebbd56
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-16 11:09:50 +0530

    Add CodeChef GITPROB67 (myFile.txt - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/GITPROB67_myFile.txt_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: GITPROB67
+ * Problem: myFile.txt - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/git-github/GITCHANGES/problems/GITPROB67
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Git Github
+ * Module: GITCHANGES
+ * Status: ACCEPTED
+ */
+
+Version 1
+Wrong change
