b53a37285ec62a21d6855aad97025512c99a4540
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-19 20:58:18 +0530

    Add CodeChef MERGESORTED (Merge two sorted arrays Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/Medium/MERGESORTED_Merge_two_sorted_arrays.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MERGESORTED
+ * Problem: Merge two sorted arrays Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/MERGESORTED
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
