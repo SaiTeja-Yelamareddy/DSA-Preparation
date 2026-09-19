dc9ec7a856eb099ac6a0f1f1846e84001e238b84
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-19 20:46:29 +0530

    Add GeeksforGeeks pair-sum-in-a-sorted-and-rotated-array (Pair Sum in a Sorted and Rotated Array) solution

---
+++ GeeksforGeeks/Java/Arrays/Easy/pair-sum-in-a-sorted-and-rotated-array_Pair_Sum_in_a_Sorted_and_Rotated_Array.java
@@ -0,0 +1,43 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: pair-sum-in-a-sorted-and-rotated-array
+ * Problem: Pair Sum in a Sorted and Rotated Array
+ * Problem Link: https://www.geeksforgeeks.org/problems/pair-sum-in-a-sorted-and-rotated-array/1
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public boolean pairInSortedRotated(int[] arr, int target) {
+
+        int n = arr.length;
+        int pivot = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                pivot = i + 1;
+                break;
+            }
+        }
+        int low = pivot;
+        int high = (pivot - 1 + n) % n;
+
+        while (low != high) {
+
+            int sum = arr[low] + arr[high];
+
+            if (sum == target) {
+                return true;
+            }
+
+            if (sum < target) {
+                low = (low + 1) % n;
+            } else {
+                high = (high - 1 + n) % n;
+            }
+        }
+
+        return false;
+    }
+}
