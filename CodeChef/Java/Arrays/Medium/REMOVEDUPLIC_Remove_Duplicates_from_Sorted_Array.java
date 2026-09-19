0f59db08274e92860faf7fa5a5fa3780ab4cbc72
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-19 20:59:32 +0530

    Add CodeChef REMOVEDUPLIC (Remove Duplicates from Sorted Array Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/Medium/REMOVEDUPLIC_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: REMOVEDUPLIC
+ * Problem: Remove Duplicates from Sorted Array Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/REMOVEDUPLIC
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
