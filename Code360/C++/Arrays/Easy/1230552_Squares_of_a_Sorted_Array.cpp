c00ff347da7e04359b3505e79d656825ee61f412
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-19 21:01:03 +0530

    Add Code360 1230552 (Squares of a Sorted Array) solution

---
+++ Code360/C++/Arrays/Easy/1230552_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+/*
+ * Platform: Code360
+ * Problem ID: 1230552
+ * Problem: Squares of a Sorted Array
+ * Problem Link: https://www.naukri.com/code360/problems/squares-of-a-sorted-array_1230552
+ * Language: C++
+ * Concept: Arrays
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+#include <bits/stdc++.h> 
+vector < int > sqsorted(vector < int > & arr) {
+    // Write your code here.
+}
