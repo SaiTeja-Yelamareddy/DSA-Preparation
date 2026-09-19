452537b97ebcba17da5fed24393a37e047a33542
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-19 20:58:52 +0530

    Add CodeChef LASTZERO (Move all the zeros to the last Practice Problem in Intermediate Arrays and 2D Arrays) solution

---
+++ CodeChef/Java/Arrays/Medium/LASTZERO_Move_all_the_zeros_to_the_last.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: LASTZERO
+ * Problem: Move all the zeros to the last Practice Problem in Intermediate Arrays and 2D Arrays
+ * Problem Link: https://www.codechef.com/practice/course/arrays-intermediate/ARRAYSP04/problems/LASTZERO
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
