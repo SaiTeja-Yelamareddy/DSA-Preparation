86136ac69f21655df5ece351c2ad1c76e6073b36
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:53:00 +0530

    Add CodeChef FYAOAZ09 (Display a Motivational Quote in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Easy/FYAOAZ09_Display_a_Motivational_Quote_in_Full_Stack_Development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ09
+ * Problem: Display a Motivational Quote in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ09
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+  res.render('quote', { quote: quote });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
