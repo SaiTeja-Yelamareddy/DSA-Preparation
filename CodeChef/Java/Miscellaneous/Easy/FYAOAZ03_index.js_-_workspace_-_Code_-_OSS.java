5ca8fde423e8e48012918a17807d54e27cd64809
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:49:45 +0530

    Add CodeChef FYAOAZ03 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/FYAOAZ03_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/FYAOAZ03_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,18 @@
  * Status: ACCEPTED
  */
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My First EJS Page</title>
-    </head>
-    <body>
-        <h1>Welcome to my website!</h1>
-            <p>The best place to learn web development!</p>
-            </body>
-            </html>
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
