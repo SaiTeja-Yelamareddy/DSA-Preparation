d3b269aa6202cef1068d3ad82bdf67d46fdebe92
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:54:48 +0530

    Add CodeChef FYAOAZ11 (navbar.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ11_navbar.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ11
+ * Problem: navbar.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ11
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+  
