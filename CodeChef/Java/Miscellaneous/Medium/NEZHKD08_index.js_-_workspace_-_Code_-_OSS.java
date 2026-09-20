075bd15e301500465bef2f06c3996c13295cc2c3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:15:44 +0530

    Add CodeChef NEZHKD08 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD08_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NEZHKD08
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/VWHEAQ/problems/NEZHKD08
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: VWHEAQ
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+// Define a route for GET requests to the root path ("/") that responds with "Welcome to the homepage!"
+___________________________________
+  res.send('Welcome to the homepage!');
+});
