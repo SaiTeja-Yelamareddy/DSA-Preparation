6dd74f4f4d292aa6be4c8473cb16dc3a8cbe474f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:25:22 +0530

    Add CodeChef TJEKQL06 (products.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/TJEKQL06_products.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TJEKQL06
+ * Problem: products.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/LNZVHH/problems/TJEKQL06
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: LNZVHH
+ * Status: ACCEPTED
+ */
+
+//  write your code here
