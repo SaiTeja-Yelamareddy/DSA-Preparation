60d6cd89cb0180e83237cb9404e6a1dfc94b1d64
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:44:43 +0530

    Add CodeChef FYAOAZ02 (index.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ02_index.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ02
+ * Problem: index.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/IUFUSV/problems/FYAOAZ02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: IUFUSV
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+</body>
+</html>
