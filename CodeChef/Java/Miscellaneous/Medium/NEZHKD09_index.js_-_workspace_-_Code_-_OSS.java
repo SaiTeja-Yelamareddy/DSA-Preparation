b6b69cc8830f3d398edef3ac658f93eed1ff572d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:18:34 +0530

    Add CodeChef NEZHKD09 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD09_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NEZHKD09
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/VWHEAQ/problems/NEZHKD09
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: VWHEAQ
+ * Status: ACCEPTED
+ */
+
+// write your code here.
+
