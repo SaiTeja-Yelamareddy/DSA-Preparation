73798f2ab0bc2ac2a61da30824e2366f0f6ef5bd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:13:41 +0530

    Add CodeChef VBHXB69 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/VBHXB69_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB69_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,8 @@
  * Status: ACCEPTED
  */
 
-const express = require('express');
-const app = express();
 
-// Use PORT to set the port number as 3000
-
-
-// Simple route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
