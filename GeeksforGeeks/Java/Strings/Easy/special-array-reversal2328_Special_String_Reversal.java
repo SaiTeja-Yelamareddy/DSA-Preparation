c973c7d54bbfe69b162ec00eb4b73b52b029bd66
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 17:54:32 +0530

    Add GeeksforGeeks special-array-reversal2328 (Special String Reversal) solution

--- GeeksforGeeks/Java/Strings/Easy/special-array-reversal2328_Special_String_Reversal.java
+++ GeeksforGeeks/Java/Strings/Easy/special-array-reversal2328_Special_String_Reversal.java
@@ -11,16 +11,23 @@
 
 class Solution {
     public String reverseString(String s) {
-        // code here
-        char[] sarr=s.toCharArray();
-        int l=0,r=sarr.length-1;
-        while(l<r)
-        {
-            char temp=sarr[l];
-            sarr[l]=sarr[r];
-            sarr[r]=temp;
-            l++;
-            r--;
+        char[] sarr = s.toCharArray();
+        int l = 0, r = sarr.length - 1;
+        while (l < r) {
+            if (Character.isLetter(sarr[l]) && Character.isLetter(sarr[r])) {
+                char temp = sarr[l];
+                sarr[l] = sarr[r];
+                sarr[r] = temp;
+
+                l++;
+                r--;
+            }
+            else if (!Character.isLetter(sarr[l])) {
+                l++;
+            }
+            else if (!Character.isLetter(sarr[r])) {
+                r--;
+            }
         }
         return new String(sarr);
     }
