8af6edf01b8ffa8d81be6036fb04e44acfd8057d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:22:56 +0530

    Add CodeChef NEZHKD15 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD15_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NEZHKD15
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/VWHEAQ/problems/NEZHKD15
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: VWHEAQ
+ * Status: ACCEPTED
+ */
+
+// write your code here.
+
