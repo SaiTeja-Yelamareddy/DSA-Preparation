ccd767685b04bf506c699d9df92db2c4320ba87e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:27:10 +0530

    Add CodeChef TJEKQL09 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/TJEKQL09_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TJEKQL09
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/LNZVHH/problems/TJEKQL09
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: LNZVHH
+ * Status: ACCEPTED
+ */
+
+// write your code here
+
+
+
+
+
+// our server only accept frontend running on port 3000.
+const port=3000;
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
