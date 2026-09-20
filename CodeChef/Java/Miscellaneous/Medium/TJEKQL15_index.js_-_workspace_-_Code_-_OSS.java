ba0b86bcd7da930afdec1d4cf10e6fc0c3f5feda
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:32:29 +0530

    Add CodeChef TJEKQL15 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/TJEKQL15_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TJEKQL15
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/LNZVHH/problems/TJEKQL15
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: LNZVHH
+ * Status: ACCEPTED
+ */
+
+// write your code here
