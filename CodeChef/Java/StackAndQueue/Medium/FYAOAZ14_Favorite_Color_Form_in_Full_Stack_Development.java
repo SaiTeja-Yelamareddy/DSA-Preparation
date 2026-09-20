4c3ac5988f015efe4126cd25be4ddbb9af09d20f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:56:55 +0530

    Add CodeChef FYAOAZ14 (Favorite Color Form in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/FYAOAZ14_Favorite_Color_Form_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ14
+ * Problem: Favorite Color Form in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ14
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: false }));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
