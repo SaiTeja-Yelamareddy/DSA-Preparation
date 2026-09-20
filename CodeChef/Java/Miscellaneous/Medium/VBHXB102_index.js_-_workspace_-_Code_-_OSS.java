511dd95c728e97495b7956888baba8a23709ac45
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:37:56 +0530

    Add CodeChef VBHXB102 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB102_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB102
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA99/problems/VBHXB102
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA99
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON request bodies
+
+app.post('/products', (req, res) => {
+
+  // Check if all required fields are present
+
+
