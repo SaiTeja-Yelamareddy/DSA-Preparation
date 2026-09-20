7a8e2b3ecf93da3d1cdc7ae6e9eb0a3c1c8d84bd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 17:56:24 +0530

    Add Code360 1235236 (Reverse Only Letters) solution

--- Code360/Java/Miscellaneous/Easy/1235236_Reverse_Only_Letters.java
+++ Code360/Java/Miscellaneous/Easy/1235236_Reverse_Only_Letters.java
@@ -14,5 +14,24 @@ import java.io.*;
 public class Solution {
     public static String reverseOnlyLetters(String s) {
         // Write your code here.
+        char[] sarr = s.toCharArray();
+        int l = 0, r = sarr.length - 1;
+        while (l < r) {
+            if (Character.isLetter(sarr[l]) && Character.isLetter(sarr[r])) {
+                char temp = sarr[l];
+                sarr[l] = sarr[r];
+                sarr[r] = temp;
+
+                l++;
+                r--;
+            }
+            else if (!Character.isLetter(sarr[l])) {
+                l++;
+            }
+            else if (!Character.isLetter(sarr[r])) {
+                r--;
+            }
+        }
+        return new String(sarr);
     }
 }
