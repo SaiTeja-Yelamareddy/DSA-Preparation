fc57e0f4ba313f7c33246a618ad2c9edf67c35a6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:20:23 +0530

    Add CodeChef VBHXB77 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB77_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB77
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA97/problems/VBHXB77
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA97
+ * Status: ACCEPTED
+ */
+
+// write your code here.
