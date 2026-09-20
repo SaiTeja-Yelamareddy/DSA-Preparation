cbebc02cd080afb6eabdfe7ff0058414b1cf4ab7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 11:42:36 +0530

    Add CodeChef VBHXB66 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB66_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB66_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,13 @@
  * Status: ACCEPTED
  */
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+    const imagePath = path.join(__dirname, 'public', 'nodejs.png');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome to the server!');
-  } else if (req.url === '/image') {
-    // complete the code to get the image path
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(data);
+      }
