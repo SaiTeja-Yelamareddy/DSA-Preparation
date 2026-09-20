842e5d9418f367990b54c6638cbecc9c5034143e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:25:21 +0530

    Add CodeChef VBHXB85 (products.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB85_products.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB85
+ * Problem: products.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA98/problems/VBHXB85
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA98
+ * Status: ACCEPTED
+ */
+
+//  write your code here
