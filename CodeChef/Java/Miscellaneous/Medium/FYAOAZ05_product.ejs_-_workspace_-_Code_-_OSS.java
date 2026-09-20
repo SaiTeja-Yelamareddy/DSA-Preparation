571636fade194b0598df0d04c567b0ee944debab
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:46:23 +0530

    Add CodeChef FYAOAZ05 (product.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ05_product.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ05
+ * Problem: product.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/IUFUSV/problems/FYAOAZ05
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: IUFUSV
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Product Details</title>
+</head>
+<body>
+  <h1><%= product.name %></h1>
+  <p>Price: $<%= product.price %></p>
+</body>
