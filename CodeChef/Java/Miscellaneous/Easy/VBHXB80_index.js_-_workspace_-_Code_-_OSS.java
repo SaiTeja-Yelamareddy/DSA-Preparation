28a442cda1047980b13fdb3b9fdf837badb95d74
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:22:58 +0530

    Add CodeChef VBHXB80 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB80_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB80
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA97/problems/VBHXB80
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA97
+ * Status: ACCEPTED
+ */
+
+// write your code here.
+
