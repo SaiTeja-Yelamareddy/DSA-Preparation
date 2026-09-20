8fa151da44e7baa98da332d0499df2b54f0010d4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 11:56:01 +0530

    Add CodeChef VBHXB46 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB46_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB46_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,5 +11,6 @@
  * Status: ACCEPTED
  */
 
-// write your code here
-
+                                            });
+                                            });
+                                            
