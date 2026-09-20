323c8645fd870bf92743d967eecc2d13866077ab
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:54:16 +0530

    Add CodeChef FYAOAZ11 (layout.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ11_layout.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ11
+ * Problem: layout.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ11
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title><%= title %></title>
+</head>
+<body>
+  <!-- this is how we import components from other files in other directory -->
+  <%- include('partials/navbar') %>
+
+  <%- body %>
