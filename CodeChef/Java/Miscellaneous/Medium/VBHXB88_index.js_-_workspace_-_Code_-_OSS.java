85898af0b020a5a567c3c901b06636562334a670
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:27:08 +0530

    Add CodeChef VBHXB88 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB88_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB88
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA98/problems/VBHXB88
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA98
+ * Status: ACCEPTED
+ */
+
+// write your code here
+
+
+
+
+
+// our server only accept frontend running on port 3000.
+const port=3000;
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
