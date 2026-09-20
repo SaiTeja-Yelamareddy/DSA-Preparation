89827b4e350c40574f06e02ee8b937e4de56919d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:22:21 +0530

    Add CodeChef NEZHKD14 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD14_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NEZHKD14
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/VWHEAQ/problems/NEZHKD14
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: VWHEAQ
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+
+const PORT = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
