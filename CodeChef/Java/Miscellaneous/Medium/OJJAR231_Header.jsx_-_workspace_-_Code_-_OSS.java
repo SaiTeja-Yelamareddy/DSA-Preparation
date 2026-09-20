a62747105f17c0a532a0d3f97ca598e2f9c4c692
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:27:05 +0530

    Add CodeChef OJJAR231 (Header.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR231_Header.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR231
+ * Problem: Header.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA91/problems/OJJAR231
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA91
+ * Status: ACCEPTED
+ */
+
+                                                                  </header>
+                                                                    );
+                                                                    }
+
+                                                                    export default Header;
