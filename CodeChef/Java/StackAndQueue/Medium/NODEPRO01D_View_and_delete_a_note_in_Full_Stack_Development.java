aa39618ce288d7526ec5b70f38a2117eeb8161e1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:13:10 +0530

    Add CodeChef NODEPRO01D (View and delete a note in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/NODEPRO01D_View_and_delete_a_note_in_Full_Stack_Development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NODEPRO01D
+ * Problem: View and delete a note in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA101/problems/NODEPRO01D
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA101
+ * Status: ACCEPTED
+ */
+
+          <button type="submit">🗑 Delete Note</button>
+        </form>
+        <a href="/notes">⬅ Back to Notes</a>
+        
+</body>
+</html>
