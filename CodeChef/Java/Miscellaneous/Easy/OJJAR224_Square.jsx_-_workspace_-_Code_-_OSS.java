4215ed8bda584f20952b11b61f97908762c1efa8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:18:05 +0530

    Add CodeChef OJJAR224 (Square.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR224_Square.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR224
+ * Problem: Square.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA90/problems/OJJAR224
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA90
+ * Status: ACCEPTED
+ */
+
+// Implemented before
+function Square(____________) {
+  return null; 
+}
+
+export default Square;
