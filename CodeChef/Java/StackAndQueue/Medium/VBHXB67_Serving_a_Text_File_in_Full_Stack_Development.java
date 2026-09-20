fc6bb837cd2f0520226bb8737dee91dbcec83e96
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:00:45 +0530

    Add CodeChef VBHXB67 (Serving a Text File in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/VBHXB67_Serving_a_Text_File_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB67
+ * Problem: Serving a Text File in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA96/problems/VBHXB67
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA96
+ * Status: ACCEPTED
+ */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the server!');
+  } 
+//   add your else if block here
