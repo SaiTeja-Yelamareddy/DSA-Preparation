a481f8f6c42e78504507f97ba6358986828cf646
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:19:11 +0530

    Add CodeChef REACTCHA01 (Accordion.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/REACTCHA01_Accordion.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: REACTCHA01
+ * Problem: Accordion.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/REACTPRJ01/problems/REACTCHA01
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: REACTPRJ01
+ * Status: ACCEPTED
+ */
+
+import { useState } from 'react';
+import AccordionItem from './AccordionItem';
+
+// USER IMPLEMENTATION AREA
+function Accordion({ items }) {
+  // TODO: Create state to track active index
+  // TODO: Implement click handler to toggle items
+  // Requirements:
+  // 1. Only one item should be open at a time
+  // 2. Clicking an open item should close it
