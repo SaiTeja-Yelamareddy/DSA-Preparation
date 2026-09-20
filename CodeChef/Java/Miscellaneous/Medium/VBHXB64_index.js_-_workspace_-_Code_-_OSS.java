19ad65d86bd94439ef7d3074be845cf8af61ca02
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:02:51 +0530

    Add CodeChef VBHXB64 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB64_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB64_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,13 @@
  * Status: ACCEPTED
  */
 
+const http = require('http');
+const url = require('url');
 
-res.end('Error: Product ID is required');
-return;
-}
+const products = {
+'123': { name: 'Example Product', price: '$25' },
+'456': { name: 'Another Product', price: '$50' }
+};
 
-res.writeHead(404, { 'Content-Type': 'text/plain' });
-res.end('404 Not Found');
-});
-
-server.listen(3000);
+const server = http.createServer((req, res) => {
+const parsedUrl = url.parse(req.url, true);
