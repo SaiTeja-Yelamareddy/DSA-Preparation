a43d6bb0df9e3b3981af92b18b060ed17f9ec147
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:57:44 +0530

    Add CodeChef FYAOAZ15 (Name and Age Form in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/FYAOAZ15_Name_and_Age_Form_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ15
+ * Problem: Name and Age Form in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ15
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Name and Age Form</title>
+</head>
+<body>
+    <h1>Please enter your name and age:</h1>
+    <form action="/submit-name-age" method="POST">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br><br>
