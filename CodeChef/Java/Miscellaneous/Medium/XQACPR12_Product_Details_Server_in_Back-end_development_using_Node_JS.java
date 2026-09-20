aabb0706e14c9742c7e1b6123372dd5f28d8a5ba
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:03:14 +0530

    Add CodeChef XQACPR12 (Product Details Server in Back-end development using Node JS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/XQACPR12_Product_Details_Server_in_Back-end_development_using_Node_JS.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: XQACPR12
+ * Problem: Product Details Server in Back-end development using Node JS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/MMVQCY/problems/XQACPR12
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: MMVQCY
+ * Status: ACCEPTED
+ */
+
+const http = require('http');
+const url = require('url');
+
+const products = {
+  '123': { name: 'Example Product', price: '$25' },
+  '456': { name: 'Another Product', price: '$50' }
+};
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+…});
