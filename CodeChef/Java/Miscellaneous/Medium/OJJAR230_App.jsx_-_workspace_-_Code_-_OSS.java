8018d55338051756cb233b52630b6e3f325e664e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:24:21 +0530

    Add CodeChef OJJAR230 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR230_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR230
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA91/problems/OJJAR230
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA91
+ * Status: ACCEPTED
+ */
+
+import  { useState } from 'react';
+import Tab from './Tab.jsx';
+
+const App = () => {
+  // TODO: Step 1 - Create state to track active tab
+
+  const renderContent = () => {
+    // TODO: Step 2 - Based on active tab, show appropriate content
+    <p>This is the Frontend Project section.</p>;
+    <p>This is the Frontend Project section.</p>;
