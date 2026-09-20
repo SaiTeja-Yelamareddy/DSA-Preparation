68c7668f7f1acda7fe90d4bd041e5b585910042c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 11:55:03 +0530

    Add CodeChef ZTESUI09 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/ZTESUI09_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,15 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: ZTESUI09
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/JNKWIK/problems/ZTESUI09
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: JNKWIK
+ * Status: ACCEPTED
+ */
+
+// write your code here
+
