549a60bf0a49f76c69d50e7987478e956712cfad
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:11:49 +0530

    Add CodeChef NODEPRO01D (view-note.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NODEPRO01D_view-note.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NODEPRO01D
+ * Problem: view-note.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA101/problems/NODEPRO01D
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA101
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Notes App</title>
+        <link rel="stylesheet" href="/styles.css" />
+    </head>
+    <body>
+        <!-- complete the code to show the title and content of the note and option to delete it -->
+        
+
