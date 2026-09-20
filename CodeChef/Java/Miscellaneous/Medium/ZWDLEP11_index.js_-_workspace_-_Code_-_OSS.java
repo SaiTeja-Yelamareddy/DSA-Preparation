b8f4f43c85b1cc157325a4e2eee2d6fe2dba821e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:42:18 +0530

    Add CodeChef ZWDLEP11 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/ZWDLEP11_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/ZWDLEP11_index.js_-_workspace_-_Code_-_OSS.java
@@ -16,8 +16,12 @@ const app = express();
 
 // Route 1: Welcome message at root route
 app.get('/', (req, res) => {
-  __________('<h1>Welcome to our store!</h1>');
+  res.send('<h1>Welcome to our store!</h1>');
 });
 
 // Route 2: Product data as JSON
-app.get('/products', (___, ___) => {
+app.get('/products', (req, res) => {
+…const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
