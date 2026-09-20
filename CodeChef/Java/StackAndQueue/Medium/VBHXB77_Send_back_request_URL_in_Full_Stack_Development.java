f1938d538e20d8976c85f1e983552e962443b7ea
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:21:54 +0530

    Add CodeChef VBHXB77 (Send back request URL in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/VBHXB77_Send_back_request_URL_in_Full_Stack_Development.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB77
+ * Problem: Send back request URL in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA97/problems/VBHXB77
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA97
+ * Status: ACCEPTED
+ */
+
+    console.log(`Server is running on port ${PORT}`);
+    });
+    
