93ec2165511a4db149a31dd852881446bf759329
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:50:38 +0530

    Add CodeChef FYAOAZ05 (Display Product Details in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/FYAOAZ05_Display_Product_Details_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ05
+ * Problem: Display Product Details in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ05
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Product Details</title>
+</head>
+<body>
+  <h1><%= product.name %></h1>
+  <p>Price: $<%= product.price %></p>
+</body>
