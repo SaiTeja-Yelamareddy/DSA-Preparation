aa841748ae23a74476103b7ba043a804c0d69523
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:51:06 +0530

    Add CodeChef FYAOAZ06 (greeting.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/FYAOAZ06_greeting.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ06
+ * Problem: greeting.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ06
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Greeting Page</title>
+</head>
+<body>
+  <h1>Hello,__________________________________!</h1>
+</body>
+</html>
