9f43b23318c48609bb743958e690f971d55498d0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:44:41 +0530

    Add CodeChef FYAOAZ02 (Display Welcome Message with EJS in Back-end development using Node JS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ02_Display_Welcome_Message_with_EJS_in_Back-end_development_using_Node_JS.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ02
+ * Problem: Display Welcome Message with EJS in Back-end development using Node JS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/IUFUSV/problems/FYAOAZ02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: IUFUSV
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+</body>
+</html>
