fdd53fba724aadacd43143f6467aca965088209a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:11:03 +0530

    Add CodeChef NODEPRO01C (notes.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NODEPRO01C_notes.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NODEPRO01C
+ * Problem: notes.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA101/problems/NODEPRO01C
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA101
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const notesDir = path.join(__dirname, '..', 'notes');
+
+// View All Notes
+
+
