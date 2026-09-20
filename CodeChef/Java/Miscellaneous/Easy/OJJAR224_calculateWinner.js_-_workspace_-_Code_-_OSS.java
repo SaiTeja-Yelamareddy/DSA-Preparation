7036508001c0d75396425ebe4c816f5dbceb48bc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:18:32 +0530

    Add CodeChef OJJAR224 (calculateWinner.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR224_calculateWinner.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR224
+ * Problem: calculateWinner.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA90/problems/OJJAR224
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA90
+ * Status: ACCEPTED
+ */
+
+                                    }
+
+                                    export default Square;
+}
