2b6da9eec080b40a96714374e6c81a4ea1a7247e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:19:29 +0530

    Add CodeChef OJJAR225 (Accordion.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR225_Accordion.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR225
+ * Problem: Accordion.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA90/problems/OJJAR225
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA90
+ * Status: ACCEPTED
+ */
+
+import { useState } from 'react';
+import AccordionItem from './AccordionItem';
+
+// USER IMPLEMENTATION AREA
+function Accordion({ items }) {
+  // TODO: Create state to track active index
+  // TODO: Implement click handler to toggle items
+  // Requirements:
+  // 1. Only one item should be open at a time
+  // 2. Clicking an open item should close it
