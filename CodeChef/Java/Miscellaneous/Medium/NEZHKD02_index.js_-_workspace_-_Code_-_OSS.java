a747786a3f22316944c0377ee60b7e5a2cb2576f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:13:40 +0530

    Add CodeChef NEZHKD02 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/NEZHKD02_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD02_index.js_-_workspace_-_Code_-_OSS.java
@@ -15,9 +15,14 @@ const express = require('express');
 const app = express();
 
 // Use PORT to set the port number as 3000
-
+const PORT = 3000;
 
 // Simple route
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
