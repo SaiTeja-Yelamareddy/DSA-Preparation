d1d6a5d9bd4bb491ccd2ab9c6dc34645197a4ced
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:35:00 +0530

    Add CodeChef VBHXB99 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB99_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB99
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA99/problems/VBHXB99
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA99
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware to parse JSON data
+app.use(express.json());
