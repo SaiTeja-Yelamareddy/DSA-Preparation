dd289778a360a1731cda7656fb77a3c1b1bf4848
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:46:59 +0530

    Add CodeChef FYAOAZ03 (Display a Website Tagline in Back-end development using Node JS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ03_Display_a_Website_Tagline_in_Back-end_development_using_Node_JS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ03
+ * Problem: Display a Website Tagline in Back-end development using Node JS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/IUFUSV/problems/FYAOAZ03
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: IUFUSV
+ * Status: ACCEPTED
+ */
+
+// write your code here
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+    </head>
+    <body>
+        <h1>Welcome to my website!</h1>
+            <p>The best place to learn web development!</p>
+            </body>
