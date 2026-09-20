72e68738db056a3eb2aa71f593006ed9e9086379
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:21:38 +0530

    Add CodeChef VBHXB76 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB76_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB76
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA97/problems/VBHXB76
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA97
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const userAgent = req.headers['user-agent'];
+  res.send(`Your User-Agent is: ${userAgent}`);
+});
+
+app.listen(PORT, () => {
