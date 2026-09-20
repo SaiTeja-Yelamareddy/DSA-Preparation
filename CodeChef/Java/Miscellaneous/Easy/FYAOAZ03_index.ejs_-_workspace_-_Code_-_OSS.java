6ba7f51b6a59b975b9fd80f0ca3bcc1a2b3bac6f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:45:30 +0530

    Add CodeChef FYAOAZ03 (index.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/FYAOAZ03_index.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ03
+ * Problem: index.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ03
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+    <p>The best place to learn web development!</p>
+</body>
+</html>
