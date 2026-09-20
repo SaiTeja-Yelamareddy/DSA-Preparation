a77a3f587efab9a803d3e963c324822c14b0cc5f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 11:59:26 +0530

    Add CodeChef XQACPR12 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/XQACPR12_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: XQACPR12
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/MMVQCY/problems/XQACPR12
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: MMVQCY
+ * Status: ACCEPTED
+ */
+
+// write your code here
