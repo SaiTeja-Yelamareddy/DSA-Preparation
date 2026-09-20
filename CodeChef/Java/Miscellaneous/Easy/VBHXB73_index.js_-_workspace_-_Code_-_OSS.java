e2ee06c68c28ed4e0099139e86a54d90dae28f85
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:16:06 +0530

    Add CodeChef VBHXB73 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB73_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB73
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA97/problems/VBHXB73
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA97
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+// Define a route for GET requests to the root path ("/") that responds with "Welcome to the homepage!"
+___________________________________
+  res.send('Welcome to the homepage!');
+});
