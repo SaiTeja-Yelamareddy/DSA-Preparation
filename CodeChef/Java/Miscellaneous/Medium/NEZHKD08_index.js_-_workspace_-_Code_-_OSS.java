c952dd5fdc1fc411f17e188ada84af092fabdc2c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:15:54 +0530

    Add CodeChef NEZHKD08 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/NEZHKD08_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/NEZHKD08_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,12 @@
  * Status: ACCEPTED
  */
 
-const express = require('express');
 
-const app = express();
+// Define a route for GET requests to the path "/products" that responds with "Our products are coming soon!"
+app.get('/products', (req, res) => {
+  res.send('Our products are coming soon!');
+});
 
-const PORT = 3000;
-
-// Define a route for GET requests to the root path ("/") that responds with "Welcome to the homepage!"
-___________________________________
-  res.send('Welcome to the homepage!');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
