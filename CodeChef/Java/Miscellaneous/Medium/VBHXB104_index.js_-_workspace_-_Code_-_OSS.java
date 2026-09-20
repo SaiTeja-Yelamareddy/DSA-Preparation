6365c13a05cc83f571e6d34709df479551c90687
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:43:08 +0530

    Add CodeChef VBHXB104 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/VBHXB104_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/VBHXB104_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,8 @@
  * Status: ACCEPTED
  */
 
-const express = require('express');
-const app = express();
-
-// Route 1: Welcome message at root route
-app.get('/', (req, res) => {
-  __________('<h1>Welcome to our store!</h1>');
-});
-
-// Route 2: Product data as JSON
-app.get('/products', (___, ___) => {
+                    const PORT = 3000;
+                    app.listen(PORT, () => {
+                      console.log(`Server is running on http://localhost:${PORT}`);
+                      });
+                      
