32ca0d55563b66c2ff49d24a25eebb7308c3d1c0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:25:17 +0530

    Add CodeChef OJJAR230 (Tab.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR230_Tab.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR230
+ * Problem: Tab.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA91/problems/OJJAR230
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA91
+ * Status: ACCEPTED
+ */
+
+                                                                                                        </button>
+                                                                                                                        );
+                                                                                                                        };
+
+                                                                                                                        export default Tab;
