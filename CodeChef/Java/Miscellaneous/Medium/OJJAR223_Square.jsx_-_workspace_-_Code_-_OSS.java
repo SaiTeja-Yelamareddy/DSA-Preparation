8f8282fd581214b290c602a3f05888fe07346000
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:14:05 +0530

    Add CodeChef OJJAR223 (Square.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR223_Square.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR223
+ * Problem: Square.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA90/problems/OJJAR223
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA90
+ * Status: ACCEPTED
+ */
+
+// TODO: Implement this component
+// Requirements:
+// 1. Should display the value (prop) inside the button
+// 2. Should call onClick prop when clicked
+// 3. Should have appropriate styling (use className "square")
+function Square(____________) {
+  return null; // User will implement this
+}
+
+export default Square;
