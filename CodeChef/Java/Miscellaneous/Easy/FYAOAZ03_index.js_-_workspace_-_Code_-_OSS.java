b5d4748746f03caafc7e23d96121954537116952
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:45:25 +0530

    Add CodeChef FYAOAZ03 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/FYAOAZ03_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/FYAOAZ03_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,4 +11,12 @@
  * Status: ACCEPTED
  */
 
-// write your code here
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+</body>
+</html>
