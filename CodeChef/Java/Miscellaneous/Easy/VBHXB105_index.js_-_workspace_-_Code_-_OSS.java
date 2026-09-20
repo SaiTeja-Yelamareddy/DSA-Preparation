f1d2a3e4fd4a776d9fbc0a2976c9abe367f701cb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:43:39 +0530

    Add CodeChef VBHXB105 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB105_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB105
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA99/problems/VBHXB105
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA99
+ * Status: ACCEPTED
+ */
+
+// write your code here
