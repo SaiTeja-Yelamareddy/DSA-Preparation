a5de556fbe479bc6a4ce7527d4dcfb84fad8a3d4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 17:56:12 +0530

    Add Code360 1235236 (Two Sum) solution

---
+++ Code360/Java/Miscellaneous/Easy/1235236_Two_Sum.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: Code360
+ * Problem ID: 1235236
+ * Problem: Two Sum
+ * Problem Link: https://www.naukri.com/code360/problems/reverse-only-letters_1235236
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static String reverseOnlyLetters(String s) {
+        // Write your code here.
+    }
+}
