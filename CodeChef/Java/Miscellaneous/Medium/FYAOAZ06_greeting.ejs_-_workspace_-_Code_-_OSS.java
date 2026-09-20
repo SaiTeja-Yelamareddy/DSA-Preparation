0d951efe04dfe22e5a706b8e9dad9f18d63bcf7d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:51:02 +0530

    Add CodeChef FYAOAZ06 (greeting.ejs - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/FYAOAZ06_greeting.ejs_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ06
+ * Problem: greeting.ejs - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/IUFUSV/problems/FYAOAZ06
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: IUFUSV
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Greeting Page</title>
+</head>
+<body>
+  <h1>Hello,__________________________________!</h1>
+</body>
+</html>
