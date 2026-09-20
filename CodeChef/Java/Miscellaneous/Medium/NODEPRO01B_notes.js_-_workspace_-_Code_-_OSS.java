03561bcea5f55452b0861aab39e7f6fdb81d981d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:10:43 +0530

    Add CodeChef NODEPRO01B (notes.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NODEPRO01B_notes.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NODEPRO01B
+ * Problem: notes.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA101/problems/NODEPRO01B
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA101
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const path = require('path');
+const notesRouter = require('./routes/notes');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
