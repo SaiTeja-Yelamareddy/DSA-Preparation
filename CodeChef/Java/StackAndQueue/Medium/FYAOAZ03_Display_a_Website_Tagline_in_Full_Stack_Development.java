99d414faacdbffae1152fd8abe2ce6e6aa0676ac
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:45:50 +0530

    Add CodeChef FYAOAZ03 (Display a Website Tagline in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/FYAOAZ03_Display_a_Website_Tagline_in_Full_Stack_Development.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: FYAOAZ03
+ * Problem: Display a Website Tagline in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA100/problems/FYAOAZ03
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA100
+ * Status: ACCEPTED
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First EJS Page</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+</body>
+</html>
