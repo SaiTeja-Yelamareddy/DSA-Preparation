de8cc7027538161e36d0b42dd392e797692f4572
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 13:18:08 +0530

    Add CodeChef TICTACTOE02 (Square.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/TICTACTOE02_Square.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: TICTACTOE02
+ * Problem: Square.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/REACTPRJ01/problems/TICTACTOE02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: REACTPRJ01
+ * Status: ACCEPTED
+ */
+
+// Implemented before
+function Square(____________) {
+  return null; 
+}
+
+export default Square;
