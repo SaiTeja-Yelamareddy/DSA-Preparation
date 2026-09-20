67e441c3f411dc2886286bb5477b95353960ad9b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:26:34 +0530

    Add CodeChef VBHXB87 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB87_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VBHXB87
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA98/problems/VBHXB87
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA98
+ * Status: ACCEPTED
+ */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
