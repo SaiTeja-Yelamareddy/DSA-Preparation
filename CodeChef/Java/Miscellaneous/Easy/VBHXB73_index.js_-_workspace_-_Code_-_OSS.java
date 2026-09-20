6a1eba5edc82275bac85167b01e9e30971cc9d96
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-20 12:16:15 +0530

    Add CodeChef VBHXB73 (index.js - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/VBHXB73_index.js_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/VBHXB73_index.js_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,11 @@
  * Status: ACCEPTED
  */
 
-const express = require('express');
 
-const app = express();
-
-const PORT = 3000;
-
-// Define a route for GET requests to the root path ("/") that responds with "Welcome to the homepage!"
-___________________________________
-  res.send('Welcome to the homepage!');
+// Define a route for GET requests to the path "/products" that responds with "Our products are coming soon!"
+app.get('/products', (req, res) => {
+  res.send('Our products are coming soon!');
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT
