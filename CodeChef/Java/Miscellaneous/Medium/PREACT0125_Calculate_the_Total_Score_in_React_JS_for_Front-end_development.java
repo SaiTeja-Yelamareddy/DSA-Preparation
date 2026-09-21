265dbfc5afca780cedb97c64ec34be0c5cc1ce27
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:02:57 +0530

    Add CodeChef PREACT0125 (Calculate the Total Score in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0125_Calculate_the_Total_Score_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0125
+ * Problem: Calculate the Total Score in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT027/problems/PREACT0125
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT027
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
