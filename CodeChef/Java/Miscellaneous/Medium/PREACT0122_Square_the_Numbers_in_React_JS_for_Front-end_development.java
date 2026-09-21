f671eb0c58e2d8e5a0069051690400d14bb604df
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:53:40 +0530

    Add CodeChef PREACT0122 (Square the Numbers in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0122_Square_the_Numbers_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0122
+ * Problem: Square the Numbers in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT026/problems/PREACT0122
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT026
+ * Status: ACCEPTED
+ */
+
+const numbers = [1, 2, 3, 4, 5];
+
+// complete the code 
+const squaredNumbers = numbers.map(num => num * num); 
+
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
