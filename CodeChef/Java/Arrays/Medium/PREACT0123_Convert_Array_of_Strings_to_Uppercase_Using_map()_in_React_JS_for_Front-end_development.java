0f9f496d23681ad92952fae52255d4b1d25802ef
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:54:40 +0530

    Add CodeChef PREACT0123 (Convert Array of Strings to Uppercase Using map() in React JS for Front-end development) solution

---
+++ CodeChef/Java/Arrays/Medium/PREACT0123_Convert_Array_of_Strings_to_Uppercase_Using_map()_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0123
+ * Problem: Convert Array of Strings to Uppercase Using map() in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT026/problems/PREACT0123
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT026
+ * Status: ACCEPTED
+ */
+
+const words = ['hello', 'world', 'javascript'];
+
+// complete the code 
+const uppercaseWords ;
+
+console.log(uppercaseWords); 
