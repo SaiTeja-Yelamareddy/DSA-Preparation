c4a6f79254bb57efcecf97b3f704d37656e5b123
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:13:22 +0530

    Add CodeChef OJJAR53 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR53_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR53
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA62/problems/OJJAR53
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA62
+ * Status: ACCEPTED
+ */
+
+// update the function
+export function App() { 
+  const username = "";  
+  const currentYear = new Date().getFullYear();  
+  
+  return (
+    <div>
+
+
+
