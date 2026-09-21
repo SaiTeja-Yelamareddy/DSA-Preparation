5a144f04c956aa1419ecd5bff06fa01d79bc7a2f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:53:32 +0530

    Add CodeChef OJJAR34 (Square the Numbers in Full Stack Development) solution

--- CodeChef/Java/StackAndQueue/Medium/OJJAR34_Square_the_Numbers_in_Full_Stack_Development.java
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR34_Square_the_Numbers_in_Full_Stack_Development.java
@@ -11,4 +11,9 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+const numbers = [1, 2, 3, 4, 5];
+
+// complete the code 
+const squaredNumbers = numbers.map(num => num * num); 
+
+console.log(squaredNumbers);
