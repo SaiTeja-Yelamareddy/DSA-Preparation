002351cb0b8da51295ccd62db832c5819e9bf42c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:03:01 +0530

    Add CodeChef PREACT0125 (Calculate the Total Score in React JS for Front-end development) solution

--- CodeChef/Java/Miscellaneous/Medium/PREACT0125_Calculate_the_Total_Score_in_React_JS_for_Front-end_development.java
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0125_Calculate_the_Total_Score_in_React_JS_for_Front-end_development.java
@@ -11,4 +11,14 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+let scores = [10, 45, 23, 8];
+let totalScore = 0; 
+
+// complete the code 
+
+// Use forEach to go through each score
+
+// Add the current score to the totalScore
+  
+
+console.log("Total Score:", totalScore); 
