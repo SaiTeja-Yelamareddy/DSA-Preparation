74a0bc144a7046403b3a435fc55be76b0d0df19f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:25:20 +0530

    Add CodeChef PREACT011 (The Whitespace Gotcha in JSX in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT011_The_Whitespace_Gotcha_in_JSX_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT011
+ * Problem: The Whitespace Gotcha in JSX in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT002/problems/PREACT011
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT002
+ * Status: ACCEPTED
+ */
+
+const eventDays = 30;
+
+export function App() {
+  return (
+    <div>
+      <strong>Days left:</strong>
+
+      {eventDays}
+    </div>
+  );
