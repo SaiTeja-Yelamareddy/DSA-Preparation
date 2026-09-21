7078d4a74492739c34752626013031fa4e4e89c3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:06:23 +0530

    Add CodeChef PREACT0129 (Filter Out Long Words in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0129_Filter_Out_Long_Words_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0129
+ * Problem: Filter Out Long Words in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT028/problems/PREACT0129
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT028
+ * Status: ACCEPTED
+ */
+
+let words = ["apple", "banana", "pear", "kiwi", "grape", "orange"];
+
+// complete the code 
