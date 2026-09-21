93136cc8616a332bec155ad5f23b35758ea95324
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:12:04 +0530

    Add CodeChef PREACT027 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT027_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT027
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT006/problems/PREACT027
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT006
+ * Status: ACCEPTED
+ */
+
+import styles from './Notification.module.css';
+
+export function Notification({ message }) {
+  return (
+    <>
+
+
+
+
+
