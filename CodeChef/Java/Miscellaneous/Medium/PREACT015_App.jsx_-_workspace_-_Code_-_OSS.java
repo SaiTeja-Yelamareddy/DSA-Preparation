26a1f44966b2af8690d7161efdd768f02cfbeded
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:43:16 +0530

    Add CodeChef PREACT015 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT015_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT015
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT003/problems/PREACT015
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT003
+ * Status: ACCEPTED
+ */
+
+// UserProfile component: A reusable component to display user information
+function UserProfile(props) {
+  return (
+    <div>
+      {/* Heading for the user profile section */}
+      {/* Displaying the user's name from props */}
+      {/* Displaying the user's age from props */}
+    </div>
+  );
+}
