ac4b9fbcd64b617bda969b0488eff56797b8ea9e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:08:52 +0530

    Add CodeChef PREACT025 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT025_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT025
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT005/problems/PREACT025
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT005
+ * Status: ACCEPTED
+ */
+
+// This component displays the light status based on the `isLightOn` prop.
+export function LightStatus({ isLightOn }) {
+  {/* If `isLightOn` is true, it shows "Lights ON", otherwise "Lights OFF" */}
+  return (
+
+
+      
+
+  );
+}
