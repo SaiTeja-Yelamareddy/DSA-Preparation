f9959add8aaf4439f8680c0c7fbfc6293ae92ec6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:03:41 +0530

    Add CodeChef OJJAR39 (Calculate the Total Score in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR39_Calculate_the_Total_Score_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR39
+ * Problem: Calculate the Total Score in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA58/problems/OJJAR39
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA58
+ * Status: ACCEPTED
+ */
+
+let scores = [10, 45, 23, 8];
+let totalScore = 0; // Start with a total of 0
+
+// Use forEach to go through each score
+scores.forEach((score) => {
+  // Add the current score to the totalScore
+  totalScore = totalScore + score;
+});
+
+console.log("Total Score:", totalScore); 
