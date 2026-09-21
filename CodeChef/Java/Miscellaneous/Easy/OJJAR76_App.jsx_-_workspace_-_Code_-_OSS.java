8f6e51d6fd68201331305475975f78cc2e3be193
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:12:00 +0530

    Add CodeChef OJJAR76 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR76_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR76
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA66/problems/OJJAR76
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA66
+ * Status: ACCEPTED
+ */
+
+import styles from './Notification.module.css';
+
+export function Notification({ message }) {
+  return (
+    <>
+
+
+
+
+
