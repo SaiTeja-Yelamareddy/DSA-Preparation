69dbfd9bf93579d3b30e2ac25c1a0b68abd1b364
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:06:20 +0530

    Add CodeChef OJJAR44 (Filter Out Long Words in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR44_Filter_Out_Long_Words_in_Full_Stack_Development.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR44
+ * Problem: Filter Out Long Words in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA59/problems/OJJAR44
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA59
+ * Status: ACCEPTED
+ */
+
+let words = ["apple", "banana", "pear", "kiwi", "grape", "orange"];
+
+let shortWords = words.filter((word) => {
+    return word.length <= 4; // Complete this line
+});
+
+console.log(shortWords);
