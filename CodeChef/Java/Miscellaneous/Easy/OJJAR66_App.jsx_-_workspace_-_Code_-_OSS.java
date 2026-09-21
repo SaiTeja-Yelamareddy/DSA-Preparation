99d63dad28e04444b0c41545d401faee6f4a6aa5
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:02:48 +0530

    Add CodeChef OJJAR66 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR66_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR66
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA63/problems/OJJAR66
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA63
+ * Status: ACCEPTED
+ */
+
+const Message = ({ text }) => {
+  return <p>{text}</p>;
+};
+
+function App() {
+  return (
+    <div>
+      <Message text="Hello, this is a message!" />
+    </div>
+  );
