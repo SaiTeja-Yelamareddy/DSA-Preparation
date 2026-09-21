38cd6b9ccd36f7a7ea6de6a4041dc113174a66e9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:13:30 +0530

    Add CodeChef OJJAR80 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR80_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR80
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA68/problems/OJJAR80
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA68
+ * Status: ACCEPTED
+ */
+
+function App() {
+  let count = 0 ;
+  return (
+    <>
+      <p>You clicked {count} times</p>
+      <button onClick={() => count++}>
+        Click me
+      </button>
+    </>
+  );
