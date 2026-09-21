b717871c7066001aa14ea7bf5268512083db35dd
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:09:21 +0530

    Add CodeChef OJJAR73 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR73_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR73
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA65/problems/OJJAR73
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA65
+ * Status: ACCEPTED
+ */
+
+// This component displays the light status based on the `isLightOn` prop.
+export function LightStatus({ isLightOn }) {
+  {/* If `isLightOn` is true, it shows "Lights ON", otherwise "Lights OFF" */}
+  return (
+
+
+      
+
+  );
+}
