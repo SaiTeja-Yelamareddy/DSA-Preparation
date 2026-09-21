abd00f5a2bdf90a1b51b7509156fe6c2871abce2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:43:20 +0530

    Add CodeChef OJJAR63 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR63_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR63
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA63/problems/OJJAR63
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA63
+ * Status: ACCEPTED
+ */
+
+// UserProfile component: A reusable component to display user information
+function UserProfile(props) {
+  return (
+    <div>
+      {/* Heading for the user profile section */}
+      {/* Displaying the user's name from props */}
+      {/* Displaying the user's age from props */}
+    </div>
+  );
+}
