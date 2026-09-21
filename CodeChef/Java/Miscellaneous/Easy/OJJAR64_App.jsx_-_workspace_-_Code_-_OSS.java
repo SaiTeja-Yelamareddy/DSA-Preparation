91884966107f0915d03d77963d9b083cc7658a65
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:44:22 +0530

    Add CodeChef OJJAR64 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR64_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR64
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA63/problems/OJJAR64
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA63
+ * Status: ACCEPTED
+ */
+
+function App() {
+  return (
+    <div>
+      <h3>Laptop</h3>
+      <p>Price: $1000</p>
+      <p>High-performance laptop with 16GB RAM and 512GB SSD.</p>
+    </div>
+  );
+} 
+
