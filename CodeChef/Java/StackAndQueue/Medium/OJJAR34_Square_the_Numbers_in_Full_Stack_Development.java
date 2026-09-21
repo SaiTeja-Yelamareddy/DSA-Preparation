0c6de3b1cc01d131d4c26efb8b20fc9bf1b04fc0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:52:07 +0530

    Add CodeChef OJJAR34 (Square the Numbers in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR34_Square_the_Numbers_in_Full_Stack_Development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR34
+ * Problem: Square the Numbers in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA57/problems/OJJAR34
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA57
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
