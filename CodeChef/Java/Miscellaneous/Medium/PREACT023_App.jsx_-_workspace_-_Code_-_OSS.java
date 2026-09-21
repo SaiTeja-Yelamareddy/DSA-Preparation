ea92ef4dcbe934ee6540ceb8c6e2ff5c0c133ba1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:07:23 +0530

    Add CodeChef PREACT023 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT023_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,31 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT023
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT005/problems/PREACT023
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT005
+ * Status: ACCEPTED
+ */
+
+function WelcomeMessage ({ isLoggedIn }){
+  if (isLoggedIn) {
+    return <h2>Welcome back, User! 🎉</h2>;
+  } else {
+    return <h2>Please log in to continue.</h2>;
+  }
+};
+
+function App() {
+  const userLoggedIn = false;
+  
+  return (
+    <div>
+      <WelcomeMessage isLoggedIn={userLoggedIn} />
+    </div>
+  );
+}
+export default App;
