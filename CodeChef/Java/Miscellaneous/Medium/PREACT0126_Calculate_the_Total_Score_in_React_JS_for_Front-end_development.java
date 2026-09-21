cf346ddfef07b5d68be02c148e72519edc9389f6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:03:38 +0530

    Add CodeChef PREACT0126 (Calculate the Total Score in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0126_Calculate_the_Total_Score_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0126
+ * Problem: Calculate the Total Score in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT027/problems/PREACT0126
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT027
+ * Status: ACCEPTED
+ */
+
+let scores = [10, 45, 23, 8];
+let totalScore = 0; 
+
+// complete the code 
+
+// Use forEach to go through each score
+
+// Add the current score to the totalScore
+  
+
+console.log("Total Score:", totalScore); 
