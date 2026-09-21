9a4ea97053c3803791e6d23fe9c6947b5196424f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:44:40 +0530

    Add CodeChef REACTCHA06 (Improve your aim in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/REACTCHA06_Improve_your_aim_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: REACTCHA06
+ * Problem: Improve your aim in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/REACTPRJ02/problems/REACTCHA06
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: REACTPRJ02
+ * Status: ACCEPTED
+ */
+
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+
+const getRandomPosition = () => {
+  const x = Math.floor(Math.random() * 90); // percent
+  const y = Math.floor(Math.random() * 80);
+  return { x, y };
+};
+
+function Circle({ x, y, onClick }) {
+…export default App;
