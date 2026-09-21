cac6b81e2b699e6523294ad3cd4e3a4e6b32c365
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:07:11 +0530

    Add CodeChef OJJAR46 (Total Price Calculation in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR46_Total_Price_Calculation_in_Full_Stack_Development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR46
+ * Problem: Total Price Calculation in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA60/problems/OJJAR46
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA60
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
