6e8db4c26834d85ea57fe673378f6eeb7d3bf57b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:41:04 +0530

    Add CodeChef OJJAR62 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR62_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR62
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA63/problems/OJJAR62
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA63
+ * Status: ACCEPTED
+ */
+
+function Welcome() {
+  return <h2>Welcome to My React App! 🎉</h2>;
+}
+
+// Creating the new Goodbye component
+
+
+
+
+function App() {
