97d8b99ca313205595130d33b679c619e8f3109f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:05:32 +0530

    Add CodeChef OJJAR42 (Filter Long Names in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR42_Filter_Long_Names_in_Full_Stack_Development.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR42
+ * Problem: Filter Long Names in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA59/problems/OJJAR42
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA59
+ * Status: ACCEPTED
+ */
+
+let names = ["John", "Emily", "Michael", "Sara", "Chris", "Kate"];
+let shortNames = names.filter((name) => {
+    return name.length <= 5;
+});
+console.log(shortNames);
