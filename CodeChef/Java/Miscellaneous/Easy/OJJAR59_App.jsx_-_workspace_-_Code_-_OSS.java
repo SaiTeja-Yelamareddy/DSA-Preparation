823e7f2a2221cf690fd22821587124e1e5af9ed4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:26:04 +0530

    Add CodeChef OJJAR59 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR59_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR59
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA62/problems/OJJAR59
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA62
+ * Status: ACCEPTED
+ */
+
+const eventDays = 30;
+
+export function App() {
+  return (
+    <div>
+      <strong>Days left:</strong>
+
+      {eventDays}
+    </div>
+  );
