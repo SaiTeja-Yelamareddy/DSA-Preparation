2769e1324b0c467619407e26d9fbb1bd2a8fe61c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:07:47 +0530

    Add CodeChef KELIQP04 (Sum Total Amount Spent in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/KELIQP04_Sum_Total_Amount_Spent_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KELIQP04
+ * Problem: Sum Total Amount Spent in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/PQPYIA/problems/KELIQP04
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: PQPYIA
+ * Status: ACCEPTED
+ */
+
+const cartPrices = [29.99, 9.99, 4.99, 14.99, 25.00];
+
+// complete the code 
+// Using reduce to calculate the total price
+const totalPrice ;
