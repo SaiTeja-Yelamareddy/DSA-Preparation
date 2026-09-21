3d9a3a78fe776484e9f9294165e1425426fc1d3f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:11:46 +0530

    Add CodeChef OJJAR75 (App.jsx - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Easy/OJJAR75_App.jsx_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR75_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,5 @@
  * Status: ACCEPTED
  */
 
-const range = (start, end, step = 1) => {
-  let result = [];
-
-  if (typeof end === 'undefined') {
-    end = start;
-    start = 0;
-  }
-
-  for (let i = start; i < end; i += step) {
-    result.push(i);
+                                                                                    );
+                                                                                    }
