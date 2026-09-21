1a989e6131d2a22fa83ac3fb04f6be5b865e470e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:04:58 +0530

    Add CodeChef OJJAR69 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR69_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR69
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA64/problems/OJJAR69
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA64
+ * Status: ACCEPTED
+ */
+
+function GreetingCard({ name, age, greeting, favoriteColor }) {
+  const cardStyle = {
+    backgroundColor: "#fff",
+    padding: "30px",
+    borderRadius: "12px",
+    width: "300px",
+    textAlign: "left",
+    boxShadow: "0 6px 20px rgba(0, 0, 0, 0.1)",
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    margin: "20px",
