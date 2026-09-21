4772d5061f32156867cca7935547e7c4ad0b6853
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:40:01 +0530

    Add CodeChef PREACT014 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT014_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT014
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT003/problems/PREACT014
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT003
+ * Status: ACCEPTED
+ */
+
+function Welcome() {
+  return <h2>Welcome to My React App! 🎉</h2>;
+}
+
+// Creating the new Goodbye component
+
+
+
+
+function App() {
