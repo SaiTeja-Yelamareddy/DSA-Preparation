1319e152f3bd989a977b41349432a831adc197b1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:40:11 +0530

    Add CodeChef PREACT014 (App.jsx - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/PREACT014_App.jsx_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/PREACT014_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -16,8 +16,18 @@ function Welcome() {
 }
 
 // Creating the new Goodbye component
-
-
-
+function Goodbye() {
+  return <h3>Thanks for visiting! See you soon! 👋</h3>;
+}
 
 function App() {
+  return (
+    <div>
+      <Welcome /> {/* Using the Welcome component */}
+      <p>React components make the UI more structured and reusable.</p>
+      <Goodbye /> {/* Using the Goodbye component */}
+    </div>
+  );
+}
+
+export default App;
