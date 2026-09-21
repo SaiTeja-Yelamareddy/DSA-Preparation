57f8c1bc08a778c2b95231d42e814ee594cf4130
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:04:12 +0530

    Add CodeChef PREACT0130 (Double the Numbers in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0130_Double_the_Numbers_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,25 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0130
+ * Problem: Double the Numbers in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT027/problems/PREACT0130
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT027
+ * Status: ACCEPTED
+ */
+
+let originalNumbers = [1, 5, 10];
+
+// Create an empty array to store the results
+
+// Iterate over the original numbers
+
+// Calculate the doubled value
+
+// Add the doubled value to our new array
+
+
+console.log("Doubled:" );
