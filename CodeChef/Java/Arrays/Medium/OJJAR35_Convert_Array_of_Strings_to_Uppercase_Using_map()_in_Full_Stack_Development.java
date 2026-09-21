e5d9183804e1e0a572d86207e1fbb1d68e0cab29
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:53:39 +0530

    Add CodeChef OJJAR35 (Convert Array of Strings to Uppercase Using map() in Full Stack Development) solution

---
+++ CodeChef/Java/Arrays/Medium/OJJAR35_Convert_Array_of_Strings_to_Uppercase_Using_map()_in_Full_Stack_Development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR35
+ * Problem: Convert Array of Strings to Uppercase Using map() in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA57/problems/OJJAR35
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA57
+ * Status: ACCEPTED
+ */
+
+const numbers = [1, 2, 3, 4, 5];
+
+// complete the code 
+const squaredNumbers = numbers.map(num => num * num); 
+
+console.log(squaredNumbers);
