ee1dd955c7a17af2d39b096949a91a90af6e045b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:45:18 +0530

    Add CodeChef OJJAR65 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR65_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR65
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA63/problems/OJJAR65
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA63
+ * Status: ACCEPTED
+ */
+
+// Task: Convert this code to use a reusable component `<GreetingCard />`  
+// Instead of hardcoding values, pass `name`, `age`, and `greeting` as props  
+// Use instances of `<GreetingCard />` inside `App` 
+
+export function GreetingCard({ name, age, greeting }) {
+    // Inline CSS styling for the card
+
+    return (
+        
+            // Greeting message
