ae5b0c45a7d5a2ea80f53e9ff05df0237f7863ef
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 20:52:39 +0530

    Add CodeChef KELIQP02 (Square the Numbers in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/KELIQP02_Square_the_Numbers_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KELIQP02
+ * Problem: Square the Numbers in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT026/problems/KELIQP02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT026
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
