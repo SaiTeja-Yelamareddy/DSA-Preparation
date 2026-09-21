3dac503345a9faa8f444c1d33672d6eae1db8d6e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:05:19 +0530

    Add CodeChef PREACT0128 (JavaScript Filter() Method in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0128_JavaScript_Filter()_Method_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0128
+ * Problem: JavaScript Filter() Method in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT028/problems/PREACT0128
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT028
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
