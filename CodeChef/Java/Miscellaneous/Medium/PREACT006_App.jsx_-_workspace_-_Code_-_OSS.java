5f57e5644f58f4356c57d7d365d5a2cf175b9e74
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:11:27 +0530

    Add CodeChef PREACT006 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT006_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT006
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT002/problems/PREACT006
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT002
+ * Status: ACCEPTED
+ */
+
+// update the function 
+export function App() {
+  return (
+    // write you code here 
+  );
+}
