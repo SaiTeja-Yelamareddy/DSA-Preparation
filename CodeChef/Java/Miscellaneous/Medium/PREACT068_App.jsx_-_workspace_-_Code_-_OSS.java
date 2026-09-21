374c77cd86a90d5b657d0cb991d032f0ab7cc509
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:06:18 +0530

    Add CodeChef PREACT068 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT068_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT068
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT004/problems/PREACT068
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT004
+ * Status: ACCEPTED
+ */
+
+import React from "react";
+
+const StudentList = () => {
+  const students = [
+    { name: "John", age: 20 },
+    { name: "Emma", age: 22 },
+    { name: "Liam", age: 19 },
+    { name: "Olivia", age: 21 }
+  ];
+
