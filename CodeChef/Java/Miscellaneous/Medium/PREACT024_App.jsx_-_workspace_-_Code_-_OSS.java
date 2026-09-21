b0c1504a93ba616ea2e406793a30584ce5629515
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:09:45 +0530

    Add CodeChef PREACT024 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT024_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT024
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT005/problems/PREACT024
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT005
+ * Status: ACCEPTED
+ */
+
+// WelcomeMessage component: Displays a message based on login and premium status
+export function WelcomeMessage({ isLoggedIn, isPremiumUser }) {
+  return (
+    <>
+      {/* Application title  */}
+      
+
+      {/* Conditional rendering: Show different messages based on user status  */}
+      
+      
