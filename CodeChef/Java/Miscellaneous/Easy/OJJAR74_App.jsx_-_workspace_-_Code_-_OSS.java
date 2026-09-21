4d4d29d529aea3c88756e9b450e421c1853d5709
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:10:05 +0530

    Add CodeChef OJJAR74 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR74_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR74
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA65/problems/OJJAR74
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA65
+ * Status: ACCEPTED
+ */
+
+// WelcomeMessage component: Displays a message based on login and premium status
+export function WelcomeMessage({ isLoggedIn, isPremiumUser }) {
+  return (
+    <>
+      {/* Application title  */}
+      
+
+      {/* Conditional rendering: Show different messages based on user status  */}
+      
+      
