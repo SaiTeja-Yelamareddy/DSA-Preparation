cab04dfff961fb1d24d46bb42d051d94451506dc
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:07:13 +0530

    Add CodeChef KELIQP03 (JavaScript Reduce Method in undefined) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/KELIQP03_JavaScript_Reduce_Method_in_undefined.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: KELIQP03
+ * Problem: JavaScript Reduce Method in undefined
+ * Problem Link: https://www.codechef.com/learn/course/react-js/PQPYIA/problems/KELIQP03
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: PQPYIA
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
