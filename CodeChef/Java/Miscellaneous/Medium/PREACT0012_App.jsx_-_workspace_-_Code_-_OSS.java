eb3f10472310ca8c3436acae778a7ad573096058
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:14:20 +0530

    Add CodeChef PREACT0012 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0012_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0012
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT002/problems/PREACT0012
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT002
+ * Status: ACCEPTED
+ */
+
+export default function App() {
+  return (
+    <div
+      style={{
+        // add the width here 
+        // add the height here 
+
+        backgroundColor: 'lightblue',
+        color: 'white',
+        display: 'flex',
