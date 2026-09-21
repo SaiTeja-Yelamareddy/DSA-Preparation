ad5696eff48d6a3e86df9a498c5643db3c883519
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:14:19 +0530

    Add CodeChef OJJAR56 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR56_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR56
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA62/problems/OJJAR56
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA62
+ * Status: ACCEPTED
+ */
+
+export default function App() {
+  return (
+    <div
+      style={{
+        // add the width here 
+        // add the height here 
+
+        backgroundColor: 'lightblue',
+        color: 'white',
+        display: 'flex',
