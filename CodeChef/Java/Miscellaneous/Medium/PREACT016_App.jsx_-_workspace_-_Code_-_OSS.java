db23a0e3a7b7b26740d4d704533171048bbcfe6a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:45:13 +0530

    Add CodeChef PREACT016 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT016_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT016
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT003/problems/PREACT016
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT003
+ * Status: ACCEPTED
+ */
+
+// Task: Convert this code to use a reusable component `<GreetingCard />`  
+// Instead of hardcoding values, pass `name`, `age`, and `greeting` as props  
+// Use instances of `<GreetingCard />` inside `App` 
+
+export function GreetingCard({ name, age, greeting }) {
+    // Inline CSS styling for the card
+
+    return (
+        
+            // Greeting message
