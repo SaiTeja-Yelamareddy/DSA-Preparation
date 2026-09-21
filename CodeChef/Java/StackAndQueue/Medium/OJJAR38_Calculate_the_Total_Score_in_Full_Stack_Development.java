8e87feba5af2f277499c2034851cd16773d3772e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:02:39 +0530

    Add CodeChef OJJAR38 (Calculate the Total Score in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR38_Calculate_the_Total_Score_in_Full_Stack_Development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR38
+ * Problem: Calculate the Total Score in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA58/problems/OJJAR38
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA58
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
