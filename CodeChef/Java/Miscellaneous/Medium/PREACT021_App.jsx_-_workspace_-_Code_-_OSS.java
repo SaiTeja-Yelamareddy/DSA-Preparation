0fced87c17bf448618a7a0ae33944398eec8090c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:05:04 +0530

    Add CodeChef PREACT021 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT021_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT021
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT004/problems/PREACT021
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT004
+ * Status: ACCEPTED
+ */
+
+function GreetingCard({ name, age, greeting, favoriteColor }) {
+  const cardStyle = {
+    backgroundColor: "#fff",
+    padding: "30px",
+    borderRadius: "12px",
+    width: "300px",
+    textAlign: "left",
+    boxShadow: "0 6px 20px rgba(0, 0, 0, 0.1)",
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    margin: "20px",
