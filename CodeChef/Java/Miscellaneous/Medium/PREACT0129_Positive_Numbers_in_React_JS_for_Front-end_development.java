627fa50aaba88fc0155433b9b347c58c472e8ed6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:06:29 +0530

    Add CodeChef PREACT0129 (Positive Numbers in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0129_Positive_Numbers_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0129
+ * Problem: Positive Numbers in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT028/problems/PREACT0129
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT028
+ * Status: ACCEPTED
+ */
+
+let numbers = [10, -5, 0, 3, -2, 7, -1, 4];
+
+// complete the code 
