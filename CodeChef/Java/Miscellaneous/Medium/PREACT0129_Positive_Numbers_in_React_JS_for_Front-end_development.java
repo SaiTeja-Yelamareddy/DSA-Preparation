47cce42c1a5412921d570f240fa2ed8f1ab8a91a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:06:30 +0530

    Add CodeChef PREACT0129 (Positive Numbers in React JS for Front-end development) solution

--- CodeChef/Java/Miscellaneous/Medium/PREACT0129_Positive_Numbers_in_React_JS_for_Front-end_development.java
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0129_Positive_Numbers_in_React_JS_for_Front-end_development.java
@@ -12,5 +12,8 @@
  */
 
 let numbers = [10, -5, 0, 3, -2, 7, -1, 4];
+let positiveNumbers = numbers.filter((number) => {
+    return number >= 0;
+});
 
-// complete the code 
+console.log(positiveNumbers)
