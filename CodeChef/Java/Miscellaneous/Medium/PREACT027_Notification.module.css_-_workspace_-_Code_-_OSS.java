4646ae03dbdc28846444bb1684f08d28e35d73f8
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 22:12:14 +0530

    Add CodeChef PREACT027 (Notification.module.css - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT027_Notification.module.css_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT027
+ * Problem: Notification.module.css - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT006/problems/PREACT027
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT006
+ * Status: ACCEPTED
+ */
+
+.notification {
+    background-color: #4CAF50; /* Green background */
+    color: white;
+    padding: 12px;
+    border-radius: 4px;
+    margin: 10px 0;
+}
+  
+.notification:hover {
+    background-color: #45a049; /* Darker green on hover */
+}
