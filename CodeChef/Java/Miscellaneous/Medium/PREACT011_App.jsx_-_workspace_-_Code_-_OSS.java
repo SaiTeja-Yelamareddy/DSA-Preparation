771c19fdde20b797b66a812d12d49faa5fad2ad6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-21 21:15:02 +0530

    Add CodeChef PREACT011 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT011_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT011
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT002/problems/PREACT011
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT002
+ * Status: ACCEPTED
+ */
+
+const eventDays = 30;
+
+export function App() {
+  return (
+    <div>
+      <strong>Days left:</strong>
+
+      {eventDays}
+    </div>
+  );
