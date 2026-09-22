26cdcbf33d741fb712c9c53aa50dd09136a15dbf
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-22 21:51:20 +0530

    Add Code360 6922132 (Three Sum) solution

--- Code360/Java/Miscellaneous/Medium/6922132_Three_Sum.java
+++ Code360/Java/Miscellaneous/Medium/6922132_Three_Sum.java
@@ -10,8 +10,61 @@
  */
 
 import java.util.*;
+
 public class Solution {
-    public static List< List < Integer > > triplet(int n, int []arr) {
-        // Write your code here.
+
+    public static ArrayList<ArrayList<Integer>> triplet(int n, int[] arr) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+         Arrays.sort(arr);
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate first element
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+
+                int sum = arr[i] + arr[l] + arr[r];
+
+                if (sum == 0) {
+
+                    ArrayList<Integer> temp = new ArrayList<>();
+
+                    temp.add(arr[i]);
+                    temp.add(arr[l]);
+                    temp.add(arr[r]);
+
+                    ans.add(temp);
+
+                    l++;
+                    r--;
+
+                    // Skip duplicate left values
+                    while (l < r && arr[l] == arr[l - 1]) {
+                        l++;
+                    }
+
+                    // Skip duplicate right values
+                    while (l < r && arr[r] == arr[r + 1]) {
+                        r--;
+                    }
+
+                } 
+                else if (sum < 0) {
+                    l++;
+                } 
+                else {
+                    r--;
+                }
+            }
+        }
+
+        return ans;
     }
 }
