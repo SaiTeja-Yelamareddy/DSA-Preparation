763dc3a131dde0fd3144e651a8cc377b8e2d7621
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-22 21:50:48 +0530

    Add Code360 6922132 (Three Sum) solution

---
+++ Code360/Java/Miscellaneous/Medium/6922132_Three_Sum.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: Code360
+ * Problem ID: 6922132
+ * Problem: Three Sum
+ * Problem Link: https://www.naukri.com/code360/problems/three-sum_6922132
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+import java.util.*;
+public class Solution {
+    public static List< List < Integer > > triplet(int n, int []arr) {
+        // Write your code here.
+    }
+}
