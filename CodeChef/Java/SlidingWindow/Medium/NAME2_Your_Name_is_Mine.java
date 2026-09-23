f81693e37e71d1040ad18e46e0ccf630299608f2
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:29:37 +0530

    Add CodeChef NAME2 (Your Name is Mine Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/NAME2_Your_Name_is_Mine.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NAME2
+ * Problem: Your Name is Mine Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT01/problems/NAME2
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
