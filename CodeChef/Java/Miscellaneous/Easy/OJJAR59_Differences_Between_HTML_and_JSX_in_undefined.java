4cf79de75c7378a810b17c846eb3353c7f6d70a3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:04:42 +0530

    Add CodeChef OJJAR59 (Differences Between HTML and JSX in undefined) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR59_Differences_Between_HTML_and_JSX_in_undefined.java
@@ -0,0 +1,24 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR59
+ * Problem: Differences Between HTML and JSX in undefined
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA62/problems/OJJAR59
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA62
+ * Status: ACCEPTED
+ */
+
+const eventDays = 30;
+
+export function App() {
+  return (
+    <div>
+      <strong>Days left:</strong>
+
+      {eventDays}
+    </div>
+  );
+}
