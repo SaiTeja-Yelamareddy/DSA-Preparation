b1b61102438dc9df3b61af0208a8acf16f0a6fe0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:43:36 +0530

    Add CodeChef COVID19 (Coronavirus Spread Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/COVID19_Coronavirus_Spread.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: COVID19
+ * Problem: Coronavirus Spread Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT01/problems/COVID19
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
