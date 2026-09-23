c6f7cd58904e2518e8eaf5824999a926c228283c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:41:30 +0530

    Add CodeChef DSAPROB16 (Sort Array by Parity Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/DSAPROB16_Sort_Array_by_Parity.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSAPROB16
+ * Problem: Sort Array by Parity Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT01/problems/DSAPROB16
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
