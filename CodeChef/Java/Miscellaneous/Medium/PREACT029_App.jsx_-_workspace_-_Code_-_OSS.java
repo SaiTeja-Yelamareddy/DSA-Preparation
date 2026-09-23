0a2fe1d35cb9e3ca7af755ccba6fcbe6e24c121a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:18:17 +0530

    Add CodeChef PREACT029 (App.jsx - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/PREACT029_App.jsx_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/PREACT029_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,13 @@
  * Status: ACCEPTED
  */
 
-function App() {
-  function addPoints(points) {
-    console.log(`Added ${points} points!`);  // Corrected log message
-  }
+      {/* Subtracting points */}
+      <button onClick={() => subtractPoints(3)}>-3 Points</button>
 
-  function subtractPoints(points) {
-    console.log(`Subtracted ${points} points!`); // Corrected log message
-  }
+      {/* Resetting Score */}
+      <button onClick={resetScore}>Reset</button>
+    </>
+  );
+}
 
-  function resetScore() {
+export default App;
