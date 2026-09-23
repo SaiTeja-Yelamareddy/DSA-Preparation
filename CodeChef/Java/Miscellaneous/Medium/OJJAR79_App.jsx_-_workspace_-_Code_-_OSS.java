3c4bba84278042fe31fc1408ceff437ec697ce5d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:25:06 +0530

    Add CodeChef OJJAR79 (App.jsx - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/OJJAR79_App.jsx_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR79_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,6 @@
  * Status: ACCEPTED
  */
 
-import "./App.css";
+                                                                                                                                                                                                                  }
 
-// Our list of products - you don't need to change this
-const products = [
-  { id: 1, name: "Laptop", price: 900 },
-  { id: 2, name: "Mouse", price: 20 },
-  { id: 3, name: "Keyboard", price: 40 },
-  { id: 4, name: "Monitor", price: 150 },
-  { id: 5, name: "USB Cable", price: 10 },
-];
+                                                                                                                                                                                                                                                                            export default App;
