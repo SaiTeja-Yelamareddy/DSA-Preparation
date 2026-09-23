fdd702b08e6dc4994354739c42fed21952e7a828
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:22:10 +0530

    Add CodeChef OJJAR79 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR79_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR79
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA67/problems/OJJAR79
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA67
+ * Status: ACCEPTED
+ */
+
+import "./App.css";
+
+// Our list of products - you don't need to change this
+const products = [
+  { id: 1, name: "Laptop", price: 900 },
+  { id: 2, name: "Mouse", price: 20 },
+  { id: 3, name: "Keyboard", price: 40 },
+  { id: 4, name: "Monitor", price: 150 },
+  { id: 5, name: "USB Cable", price: 10 },
+];
