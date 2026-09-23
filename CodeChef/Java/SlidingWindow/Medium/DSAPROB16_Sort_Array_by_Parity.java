d6c69665ca5d5404d4b3e271f51d501e23baa08d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:43:21 +0530

    Add CodeChef DSAPROB16 (Sort Array by Parity Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/DSAPROB16_Sort_Array_by_Parity.java
+++ CodeChef/Java/SlidingWindow/Medium/DSAPROB16_Sort_Array_by_Parity.java
@@ -9,4 +9,42 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+
+public class Main {
+    public static void sortArrayByParity(int[] nums) {
+        // Write your code here
+         int[] result = new int[nums.length];
+        int left = 0, right = nums.length - 1;
+
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                result[left++] = num;
+            } else {
+                result[right--] = num;
+            }
+        }
+        for (int i = left, j = nums.length - 1; i < j; i++, j--) {
+            int temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+        }
+
+        System.arraycopy(result, 0, nums, 0, nums.length);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        sortArrayByParity(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
