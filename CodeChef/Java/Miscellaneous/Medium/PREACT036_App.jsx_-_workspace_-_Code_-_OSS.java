133e6a37a04a54c912d8d599752a04b92444b543
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:33:56 +0530

    Add CodeChef PREACT036 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT036_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT036
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT009/problems/PREACT036
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT009
+ * Status: ACCEPTED
+ */
+
+import { useState } from "react";
+import "./App.css";
+
+// Update the Main functional component
+export default function App() {
+  // Declare a state variable 'value' with an initial value of an empty string
+  
+
+  // Function to generate a random string and update the state
+  const generateRandomString = () => {
