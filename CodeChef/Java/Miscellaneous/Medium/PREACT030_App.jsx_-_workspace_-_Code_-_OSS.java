e66fa9e6225e887b7b202d62dfc1a8dfdf0269ec
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:24:14 +0530

    Add CodeChef PREACT030 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT030_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT030
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT007/problems/PREACT030
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT007
+ * Status: ACCEPTED
+ */
+
+import "./App.css";
+
+// Our list of products - you don't need to change this
+const products = [
+  { id: 1, name: "Laptop", price: 900 },
+  { id: 2, name: "Mouse", price: 20 },
+  { id: 3, name: "Keyboard", price: 40 },
+  { id: 4, name: "Monitor", price: 150 },
+  { id: 5, name: "USB Cable", price: 10 },
+];
