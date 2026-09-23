e7c66f6a0b2cf3d1863158f798cbe789ffbfcef1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:21:19 +0530

    Add CodeChef OJJAR78 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR78_Code_-_OSS.java
@@ -0,0 +1,20 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR78
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA67/problems/OJJAR78
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA67
+ * Status: ACCEPTED
+ */
+
+           {/* Resetting Score */}
+                                                                          <button onClick={resetScore}>Reset</button>
+                                                                              </>
+                                                                                );
+                                                                                }
+
+                                                                                export default App;
