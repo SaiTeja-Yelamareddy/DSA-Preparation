dd11e1ef0455fa372f53910b641ddd15e2542a07
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:14:27 +0530

    Add CodeChef PREP69 (Remove Duplicates Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/PREP69_Remove_Duplicates.java
@@ -0,0 +1,18 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREP69
+ * Problem: Remove Duplicates Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT01/problems/PREP69
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+3
+2
+5 10
+4
+1 5 5 10
+5
+4 4 6 6 8
