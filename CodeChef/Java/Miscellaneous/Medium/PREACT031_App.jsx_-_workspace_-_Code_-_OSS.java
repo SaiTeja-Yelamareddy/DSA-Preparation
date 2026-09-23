676b933b286607e092ea04127573a56607083be5
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:27:44 +0530

    Add CodeChef PREACT031 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT031_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT031
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT008/problems/PREACT031
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT008
+ * Status: ACCEPTED
+ */
+
+function App() {
+  let count = 0 ;
+  return (
+    <>
+      <p>You clicked {count} times</p>
+      <button onClick={() => count++}>
+        Click me
+      </button>
+    </>
+  );
