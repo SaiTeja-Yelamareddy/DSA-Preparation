629d9c6356356be45d4cd94929263389dc43503d
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:18:04 +0530

    Add CodeChef OJJAR78 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR78_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR78
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA67/problems/OJJAR78
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA67
+ * Status: ACCEPTED
+ */
+
+function App() {
+  function addPoints(points) {
+    console.log(`Added ${points} points!`);  // Corrected log message
+  }
+
+  function subtractPoints(points) {
+    console.log(`Subtracted ${points} points!`); // Corrected log message
+  }
+
+  function resetScore() {
