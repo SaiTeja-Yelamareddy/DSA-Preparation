4b2b06b64eb37b2ee07081fcde8e8d7a63c40d73
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 21:29:20 +0530

    Add CodeChef PREP69 (Remove Duplicates Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/PREP69_Remove_Duplicates.java
+++ CodeChef/Java/SlidingWindow/Medium/PREP69_Remove_Duplicates.java
@@ -9,10 +9,39 @@
  * Status: ACCEPTED
  */
 
-3
-2
-5 10
-4
-1 5 5 10
-5
-4 4 6 6 8
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int[] a=new int[n];
+            ArrayList<Integer> ans=new ArrayList<>();
+            int prev=-1;
+            for(int i=0;i<n;i++)
+            {
+                a[i]=sc.nextInt();
+                if(prev!=a[i])
+                {
+                    ans.add(a[i]);
+                    prev=a[i];
+                }
+            }
+            System.out.println(ans.size());
+            for(int i=0;i<ans.size();i++)
+            {
+                System.out.print(ans.get(i)+" ");
+            }
+            System.out.println();
+        
+        }
+	}
+}
