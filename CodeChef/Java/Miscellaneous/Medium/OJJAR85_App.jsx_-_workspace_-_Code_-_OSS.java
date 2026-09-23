51cfe6033b7c955394f3b23ca21929b2c7414ff1
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:34:20 +0530

    Add CodeChef OJJAR85 (App.jsx - workspace - Code - OSS) solution

--- CodeChef/Java/Miscellaneous/Medium/OJJAR85_App.jsx_-_workspace_-_Code_-_OSS.java
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR85_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -11,13 +11,8 @@
  * Status: ACCEPTED
  */
 
-import { useState } from "react";
-import "./App.css";
-
-// Update the Main functional component
-export default function App() {
-  // Declare a state variable 'value' with an initial value of an empty string
-  
-
-  // Function to generate a random string and update the state
-  const generateRandomString = () => {
+} className="btn">Generate Random String</button>
+                                                                                                            </div>
+                                                                                                              );
+                                                                                                              }
+                                                                                                              
