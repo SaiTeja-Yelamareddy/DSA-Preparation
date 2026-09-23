6f7a7003acc523ce5e685ee40bdff0492b3ec7f5
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:23:30 +0530

    Add CodeChef OJJAR79 (Task - Complete the Product List App in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR79_Task_-_Complete_the_Product_List_App_in_Full_Stack_Development.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR79
+ * Problem: Task - Complete the Product List App in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA67/problems/OJJAR79
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA67
+ * Status: ACCEPTED
+ */
+
+import "./App.css";
+
+// Our list of products - you don't need to change this
+const products = [
+  { id: 1, name: "Laptop", price: 900 },
+  { id: 2, name: "Mouse", price: 20 },
+  { id: 3, name: "Keyboard", price: 40 },
+  { id: 4, name: "Monitor", price: 150 },
+  { id: 5, name: "USB Cable", price: 10 },
+];
