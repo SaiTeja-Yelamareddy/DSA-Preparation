1020ad899e9ce2d62ede1e92bf19ad6e382afb6e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 12:51:32 +0530

    Add CodeChef OJJAR95 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR95_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR95
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA70/problems/OJJAR95
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA70
+ * Status: ACCEPTED
+ */
+
+import { useState } from "react";
+
+function App() {
+  // Lifted state in parent
+  // Initialize state for tasks here (empty array)
+
+  // Function to add a new task
+  function addTask() {
+    // This should add new task to tasks array
+    // Prevent adding empty tasks
