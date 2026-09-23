3be76d26c0b5c7bb5297d8d59ff9db3f6bf32539
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 12:43:55 +0530

    Add CodeChef OJJAR93 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR93_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR93
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA70/problems/OJJAR93
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA70
+ * Status: ACCEPTED
+ */
+
+import React from 'react';
+import './App.css'; 
+
+function App() {
+  // update the code 
+  const [guests, setGuests] = React.useState([
+    'Bruce Wayne', 'Clark Kent', 'Diana Prince'
+  ]);
+
+  return (
