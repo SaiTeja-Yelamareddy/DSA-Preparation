6ee55025cf98b946c53b4e74972e6e1fd123760a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:16:52 +0530

    Add CodeChef NIPFVX02 (index.js - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/NIPFVX02_index.js_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: NIPFVX02
+ * Problem: index.js - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/nodejs/KVHFAE/problems/NIPFVX02
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Nodejs
+ * Module: KVHFAE
+ * Status: ACCEPTED
+ */
+
+// Simulated incoming data from req.body
+const bookData = {
+  name: "The Great Gatsby",
+  price: 299,
+  description: "A classic novel by F. Scott Fitzgerald",
+  author: "F. Scott Fitzgerald",
+  pages: 180
+};
+
+const tshirtData = {
