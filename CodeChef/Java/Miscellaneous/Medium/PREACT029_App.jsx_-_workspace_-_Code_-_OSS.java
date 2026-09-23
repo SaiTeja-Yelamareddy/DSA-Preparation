242b016fbfff1b6a7150ca351efca962c4a24355
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-23 11:17:57 +0530

    Add CodeChef PREACT029 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT029_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT029
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT007/problems/PREACT029
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT007
+ * Status: ACCEPTED
+ */
+
+function App() {
+  function addPoints(points) {
+    console.log(`Added ${points} points!`);  // Corrected log message
+  }
+
+  function subtractPoints(points) {
+    console.log(`Subtracted ${points} points!`); // Corrected log message
+  }
+
+  function resetScore() {
