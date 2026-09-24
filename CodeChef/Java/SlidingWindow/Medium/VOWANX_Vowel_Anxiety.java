edb3c48b9547cd9ef73188201fc0cbea976e4e06
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:56:21 +0530

    Add CodeChef VOWANX (Vowel Anxiety Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/VOWANX_Vowel_Anxiety.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: VOWANX
+ * Problem: Vowel Anxiety Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/VOWANX
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+2
+10
+abcdefghij
+7
+bcadage
