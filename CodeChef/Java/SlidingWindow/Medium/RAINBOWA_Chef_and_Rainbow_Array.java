0cbe80aa787a35d0ab62270c08a68d25c9c5cd31
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:01:08 +0530

    Add CodeChef RAINBOWA (Chef and Rainbow Array Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/RAINBOWA_Chef_and_Rainbow_Array.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: RAINBOWA
+ * Problem: Chef and Rainbow Array Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/RAINBOWA
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
