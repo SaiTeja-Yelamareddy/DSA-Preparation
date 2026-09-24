e8167b55ae9c0d377688cc0343d3b7cd93c56c0f
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:58:50 +0530

    Add CodeChef OJJAR117 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR117_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR117
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR117
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+import Tabs from './Tabs';  
+import './App.css';       
+
+function App() {
+  // complete the code 
+  return (
+    <></>
+  );
+}
+
