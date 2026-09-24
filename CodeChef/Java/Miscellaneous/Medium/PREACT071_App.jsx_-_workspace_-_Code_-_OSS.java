4af25665682b210bdf6b1626cbacc62fad0397ba
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:57:34 +0530

    Add CodeChef PREACT071 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT071_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT071
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT015/problems/PREACT071
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT015
+ * Status: ACCEPTED
+ */
+
+import React from 'react';
+
+function GitHubProfileFinder() {
+  // State variables: username input, API response data, loading status, error message
+  // username (string), profileData (object/null), isLoading (boolean), error (string)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Fetch GitHub user data from API and handle response/errors
+  };
