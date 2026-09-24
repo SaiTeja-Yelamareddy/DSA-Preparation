d347e5e235af7a1c98d283174ed2ba5a855dff1b
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:53:15 +0530

    Add CodeChef COLGLF5 (College Life 5 Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/COLGLF5_College_Life_5.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: COLGLF5
+ * Problem: College Life 5 Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/COLGLF5
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
