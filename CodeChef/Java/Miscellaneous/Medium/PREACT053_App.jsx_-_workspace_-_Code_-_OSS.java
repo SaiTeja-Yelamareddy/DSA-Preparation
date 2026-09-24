a7d0fd4011ac40d020b41b476c480a22cd729651
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:50:17 +0530

    Add CodeChef PREACT053 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT053_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT053
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT014/problems/PREACT053
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT014
+ * Status: ACCEPTED
+ */
+
+import { useState, useEffect } from "react";
+import './App.css'
+
+function BackgroundChanger() {
+  const [count, setCount] = useState(0);
+
+  // Add useEffect here
+  
+    // Check if count reaches 5 or more
+    
