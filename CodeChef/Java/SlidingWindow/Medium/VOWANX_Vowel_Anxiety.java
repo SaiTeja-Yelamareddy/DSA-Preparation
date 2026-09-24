75f5f55dee1b5452aef85f63c26096d345c00900
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:59:21 +0530

    Add CodeChef VOWANX (Vowel Anxiety Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/VOWANX_Vowel_Anxiety.java
+++ CodeChef/Java/SlidingWindow/Medium/VOWANX_Vowel_Anxiety.java
@@ -9,8 +9,35 @@
  * Status: ACCEPTED
  */
 
-2
-10
-abcdefghij
-7
-bcadage
+import java.util.*;
+
+class Codechef {
+    static boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
+
+    static String rearrangeString(String s) {
+        int n = s.length();
+        char[] t = new char[n];
+        int l = 0, r = n - 1;
+        boolean start = false;
+        for (int i = n - 1; i >= 0; i--) {
+            if (start)
+                t[l++] = s.charAt(i);
+            else
+                t[r--] = s.charAt(i);
+            if (isVowel(s.charAt(i)))
+                start = !start;
+        }
+        return new String(t);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next(); 
+            System.out.println(rearrangeString(s));
+        }
+    }
+}
