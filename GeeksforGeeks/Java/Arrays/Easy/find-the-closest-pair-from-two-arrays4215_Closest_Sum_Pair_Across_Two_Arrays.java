3cea70a8320505a1b4e53c5f5c9d53413bf4d34a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:40:10 +0530

    Add GeeksforGeeks find-the-closest-pair-from-two-arrays4215 (Closest Sum Pair Across Two Arrays) solution

---
+++ GeeksforGeeks/Java/Arrays/Easy/find-the-closest-pair-from-two-arrays4215_Closest_Sum_Pair_Across_Two_Arrays.java
@@ -0,0 +1,36 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: find-the-closest-pair-from-two-arrays4215
+ * Problem: Closest Sum Pair Across Two Arrays
+ * Problem Link: https://www.geeksforgeeks.org/problems/find-the-closest-pair-from-two-arrays4215/1
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public ArrayList<Integer> closestPair(int[] arr1, int[] arr2, int x) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = arr2.length - 1;
+        int minDiff = Integer.MAX_VALUE;
+        while (i < arr1.length && j >= 0) {
+            int sum = arr1[i] + arr2[j];
+            int diff = Math.abs(sum - x);
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans.clear();
+                ans.add(arr1[i]);
+                ans.add(arr2[j]);
+            }
+            if (sum < x) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return ans;
+    }
+}
