adb55233b233b49337b5b8e55431c4b297cc90b7
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:09:38 +0530

    Add CodeChef OJJAR123 (Tabs.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR123_Tabs.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR123
+ * Problem: Tabs.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR123
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+                              }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                              export default Tabs;
