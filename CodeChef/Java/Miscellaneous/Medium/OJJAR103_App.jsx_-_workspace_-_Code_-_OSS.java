3113bc5a21f15e4ca97035509be3d4aec24795f6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:50:19 +0530

    Add CodeChef OJJAR103 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR103_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR103
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA73/problems/OJJAR103
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA73
+ * Status: ACCEPTED
+ */
+
+import { useState, useEffect } from "react";
+import './App.css'
+
+function BackgroundChanger() {
+  const [count, setCount] = useState(0);
+
+  // Add useEffect here
+  
+    // Check if count reaches 5 or more
+    
