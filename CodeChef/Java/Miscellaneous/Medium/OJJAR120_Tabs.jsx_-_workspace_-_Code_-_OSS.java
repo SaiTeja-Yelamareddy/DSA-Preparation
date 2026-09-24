85a02b1a46a6a1926af7e61dc18260ef43553d42
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:07:14 +0530

    Add CodeChef OJJAR120 (Tabs.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR120_Tabs.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR120
+ * Problem: Tabs.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR120
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+// Receive currentActiveTab prop
+function Tabs({ currentActiveTab }) {
+  return (
+    <div className="tabs-container">
+      <div className="tab-headers">
+        {/* Add three <button> elements here */}
+        
+      </div>
+
+      <div className="tab-content">
