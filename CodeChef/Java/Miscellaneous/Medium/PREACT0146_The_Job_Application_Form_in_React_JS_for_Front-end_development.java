9e5f84efa181abf7bd73ea91c5dd253699cbbf05
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:03:55 +0530

    Add CodeChef PREACT0146 (The Job Application Form in React JS for Front-end development) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0146_The_Job_Application_Form_in_React_JS_for_Front-end_development.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0146
+ * Problem: The Job Application Form in React JS for Front-end development
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT034/problems/PREACT0146
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT034
+ * Status: ACCEPTED
+ */
+
+// your code goes here
