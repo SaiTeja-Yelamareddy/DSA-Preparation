eee9f1186d4c19c61d587493faa3e0cd891414f4
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 11:56:36 +0530

    Add CodeChef MKSMT (Scored) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/MKSMT_Scored.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: MKSMT
+ * Problem: Scored
+ * Problem Link: https://www.codechef.com/START257D/problems/MKSMT
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
