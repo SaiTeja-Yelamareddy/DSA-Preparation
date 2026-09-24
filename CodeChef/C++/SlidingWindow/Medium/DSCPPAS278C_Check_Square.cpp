d2a881b504541b8881116c7215955e8bca5a44ce
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:47:10 +0530

    Add CodeChef DSCPPAS278C (Check Square Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/C++/SlidingWindow/Medium/DSCPPAS278C_Check_Square.cpp
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSCPPAS278C
+ * Problem: Check Square Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/DSCPPAS278C
+ * Language: C++
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
