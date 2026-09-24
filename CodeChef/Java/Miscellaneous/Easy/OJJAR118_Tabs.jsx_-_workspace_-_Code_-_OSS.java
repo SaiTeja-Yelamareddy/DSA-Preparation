9e61ed5c4f38faa89232f8ea707a9a6ec849fc4e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:03:30 +0530

    Add CodeChef OJJAR118 (Tabs.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Easy/OJJAR118_Tabs.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR118
+ * Problem: Tabs.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR118
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Easy
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+function Tabs() {
+  return (
+    <div className="tabs-container">
+      <div className="tab-headers">
+        <span>Tab 1 Header</span>
+        {/* <-- ADD Header 2 here */}
+      </div>
+
+      <div className="tab-content">
+        <p>Tab Content Will Appear Here</p>
