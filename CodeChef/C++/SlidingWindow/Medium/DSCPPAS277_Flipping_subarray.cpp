7f5c8a367b66df563bc4523bb2b991cd6c3c78d0
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:52:02 +0530

    Add CodeChef DSCPPAS277 (Flipping subarray Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/C++/SlidingWindow/Medium/DSCPPAS277_Flipping_subarray.cpp
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSCPPAS277
+ * Problem: Flipping subarray Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/DSCPPAS277
+ * Language: C++
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
