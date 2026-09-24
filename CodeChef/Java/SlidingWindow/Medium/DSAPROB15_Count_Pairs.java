33051e8e964c3877956f0859a51d30131f607263
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:51:37 +0530

    Add CodeChef DSAPROB15 (Count Pairs Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/DSAPROB15_Count_Pairs.java
+++ CodeChef/Java/SlidingWindow/Medium/DSAPROB15_Count_Pairs.java
@@ -9,4 +9,31 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+
+public class Main {
+    public static long countPairsLessThanX(int[] arr, int x) {
+        long count = 0;
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] + arr[right] < x) {
+                count += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        System.out.println(countPairsLessThanX(arr, x));
+    }
+}
