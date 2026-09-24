a87becabd069a7eabd63cd627f4292df30e5893a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:04:15 +0530

    Add CodeChef PREACT0159 (Tabs.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0159_Tabs.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0159
+ * Problem: Tabs.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT034/problems/PREACT0159
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT034
+ * Status: ACCEPTED
+ */
+
+function Tabs() {
+  return (
+    <div className="tabs-container">
+      <div className="tab-headers">
+        <span>Tab 1 Header</span>
+        {/* <-- ADD Header 2 here */}
+      </div>
+
+      <div className="tab-content">
+        <p>Tab Content Will Appear Here</p>
