92ff176486d5fa3841b0c5fded048c98b39319bb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:52:26 +0530

    Add CodeChef OJJAR105 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR105_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR105
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA73/problems/OJJAR105
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA73
+ * Status: ACCEPTED
+ */
+
+import React, { useState, useEffect } from 'react';
+
+function WindowTracker() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  
+  // complete the useEffect hook here
+  useEffect(() => {
