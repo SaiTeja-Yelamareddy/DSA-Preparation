0b2908807a6f9fbad842fbeebcf030362d587ded
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:33:43 +0530

    Add GeeksforGeeks union-of-two-sorted-arrays-1587115621 (Union of 2 Sorted Arrays) solution

---
+++ GeeksforGeeks/Java/Arrays/Easy/union-of-two-sorted-arrays-1587115621_Union_of_2_Sorted_Arrays.java
@@ -0,0 +1,51 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: union-of-two-sorted-arrays-1587115621
+ * Problem: Union of 2 Sorted Arrays
+ * Problem Link: https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public ArrayList<Integer> findUnion(int[] a, int[] b) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+
+            if (a[i] < b[j]) {
+                add(result, a[i]);
+                i++;
+            }
+            else if (b[j] < a[i]) {
+                add(result, b[j]);
+                j++;
+            }
+            else {
+                add(result, a[i]);
+                i++;
+                j++;
+            }
+        }
+        while (i < a.length) {
+            add(result, a[i]);
+            i++;
+        }
+        while (j < b.length) {
+            add(result, b[j]);
+            j++;
+        }
+        return result;
+    }
+    static void add(ArrayList<Integer> result, int value) {
+        if (result.size() == 0 || result.get(result.size() - 1) != value) {
+            result.add(value);
+        }
+    }
+}
