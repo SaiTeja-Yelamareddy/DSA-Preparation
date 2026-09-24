cf031c7d54204b34d1022d215d3f05354957ad17
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:35:48 +0530

    Add GeeksforGeeks union-of-two-arrays-with-distinct-elements (Union of Two Arrays with Distinct Elements) solution

---
+++ GeeksforGeeks/Java/Arrays/Easy/union-of-two-arrays-with-distinct-elements_Union_of_Two_Arrays_with_Distinct_Elements.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem ID: union-of-two-arrays-with-distinct-elements
+ * Problem: Union of Two Arrays with Distinct Elements
+ * Problem Link: https://www.geeksforgeeks.org/problems/union-of-two-arrays-with-distinct-elements/1
+ * Language: Java
+ * Concept: Arrays
+ * Difficulty: Easy
+ * Status: ACCEPTED
+ */
+
+class Solution {
+    public ArrayList<Integer> findUnion(int[] a, int[] b) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < a.length; i++) {
+            set.add(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            set.add(b[i]);
+        }
+        return new ArrayList<>(set);
+    }
+}
