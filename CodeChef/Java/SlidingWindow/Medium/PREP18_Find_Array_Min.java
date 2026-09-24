ee65ec741ff4e6b23c4e7b8b3c9570e0eb6ea4e6
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:47:41 +0530

    Add CodeChef PREP18 (Find Array Min Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/PREP18_Find_Array_Min.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREP18
+ * Problem: Find Array Min Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/PREP18
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
