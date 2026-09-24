076ab9eecaef4c649f975471ce255c78bb5d3c46
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:14:33 +0530

    Add CodeChef OJJAR127 (Create States and Handlers in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR127_Create_States_and_Handlers_in_Full_Stack_Development.java
@@ -0,0 +1,29 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR127
+ * Problem: Create States and Handlers in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR127
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+                    className={`tab-header ${activeTabIndex === 0 ? 'active' : ''}`}
+                    onClick={() => onTabClick(0)}
+                >
+                    1. Personal Info
+                </button>
+                <button
+                    className={`tab-header ${activeTabIndex === 1 ? 'active' : ''}`}
+                    onClick={() => onTabClick(1)}
+                >
+                    2. Experience
+…            </div>
+        </div>
+    );
+}
+
+export default Tabs;
