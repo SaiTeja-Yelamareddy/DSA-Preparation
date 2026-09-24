182f9c747f9e3e2cf8d4b5b5f28034d2b1f60864
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:45:52 +0530

    Add CodeChef DSAPROB20 (Largest Common Element in Two Arrays Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/DSAPROB20_Largest_Common_Element_in_Two_Arrays.java
@@ -0,0 +1,14 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSAPROB20
+ * Problem: Largest Common Element in Two Arrays Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/DSAPROB20
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+5 6
+1 3 4 6 7
+2 3 5 6 7 8
