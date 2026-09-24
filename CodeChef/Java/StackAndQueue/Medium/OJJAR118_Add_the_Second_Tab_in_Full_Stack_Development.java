427c243eedb17d52eb30f20583b871e2130079ce
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:05:15 +0530

    Add CodeChef OJJAR118 (Add the Second Tab in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR118_Add_the_Second_Tab_in_Full_Stack_Development.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR118
+ * Problem: Add the Second Tab in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR118
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+                                                                                                  );
+                                                                                                  }
+
+                                                                                                  export default Tabs;
