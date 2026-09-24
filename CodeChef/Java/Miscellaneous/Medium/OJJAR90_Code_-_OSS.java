c6b5078da72ad624709bcb693493de6069c0bb48
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:42:49 +0530

    Add CodeChef OJJAR90 (workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/OJJAR90_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR90
+ * Problem: workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA69/problems/OJJAR90
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA69
+ * Status: ACCEPTED
+ */
+
+import { useState } from 'react';
+import './App.css';
+
+// Main component of the application
+function App() {
+  // State to keep track of the list of todos
+  const [todos, setTodos] = useState([]);
+
+  // State to manage the input for adding new todos
+  const [newTodo, setNewTodo] = useState('');
