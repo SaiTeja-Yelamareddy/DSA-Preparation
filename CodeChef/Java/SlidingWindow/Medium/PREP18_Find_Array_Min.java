d16401870921c2ff5256fb747fca21258c0823b3
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:50:07 +0530

    Add CodeChef PREP18 (Find Array Min Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/PREP18_Find_Array_Min.java
+++ CodeChef/Java/SlidingWindow/Medium/PREP18_Find_Array_Min.java
@@ -9,15 +9,42 @@
  * Status: ACCEPTED
  */
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for (int t = 0; t < T; t++) {
+            int NA = in.nextInt(), NB = in.nextInt(), NC = in.nextInt();
+            int[] A = new int[NA];
+            int[] B = new int[NB];
+            int[] C = new int[NC];
+            for (int i = 0; i < NA; i++) {
+                A[i] = in.nextInt();
+            }
+            for (int i = 0; i < NB; i++) {
+                B[i] = in.nextInt();
+            }
+            for (int i = 0; i < NC; i++) {
+                C[i] = in.nextInt();
+            }
+            int i = 0, j = 0, k = 0;
+            int ans = Integer.MAX_VALUE;
+            while (i < NA && j < NB && k < NC) {
+                int minVal = Math.min(A[i], Math.min(B[j], C[k]));
+                int maxVal = Math.max(A[i], Math.max(B[j], C[k]));
+                ans = Math.min(ans, maxVal - minVal);
+                if (minVal == A[i]) {
+                    i++;
+                } else if (minVal == B[j]) {
+                    j++;
+                } else {
+                    k++;
+                }
+            }
+            System.out.println(ans);
+        }
+        in.close();
+    }
 }
