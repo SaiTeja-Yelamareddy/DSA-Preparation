93c064e79726670791adbfd07d3e2d85e84bd81e
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:06:53 +0530

    Add CodeChef PREACT0160 (Tabs.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT0160_Tabs.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT0160
+ * Problem: Tabs.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT034/problems/PREACT0160
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT034
+ * Status: ACCEPTED
+ */
+
+// Receive currentActiveTab prop
+function Tabs({ currentActiveTab }) {
+  return (
+    <div className="tabs-container">
+      <div className="tab-headers">
+        {/* Add three <button> elements here */}
+        
+      </div>
+
+      <div className="tab-content">
