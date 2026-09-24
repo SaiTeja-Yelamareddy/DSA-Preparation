0fe0dcd578a714382f874e1c6302c82bcd8b6da9
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 20:00:17 +0530

    Add CodeChef OJJAR117 (File Structure with Basic Tabs in Full Stack Development) solution

---
+++ CodeChef/Java/StackAndQueue/Medium/OJJAR117_File_Structure_with_Basic_Tabs_in_Full_Stack_Development.java
@@ -0,0 +1,19 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: OJJAR117
+ * Problem: File Structure with Basic Tabs in Full Stack Development
+ * Problem Link: https://www.codechef.com/learn/course/vasavi-v23csse03-fsd-2026/JHASDA75/problems/OJJAR117
+ * Language: Java
+ * Concept: StackAndQueue
+ * Difficulty: Medium
+ * Course: Vasavi V23csse03 Fsd 2026
+ * Module: JHASDA75
+ * Status: ACCEPTED
+ */
+
+                                         <ExpensiveOperationChild data="hello" />
+                                                                                            </div>
+                                                                                              );
+                                                                                              }
+
+                                                                                              export default ParentComponent;
