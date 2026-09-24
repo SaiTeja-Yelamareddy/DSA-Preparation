e2588bce6152da3978a3c30b343deec3da8c7e6a
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:55:39 +0530

    Add CodeChef COLGLF5 (College Life 5 Practice Problem in Two Pointers and Sliding Window Technique) solution

--- CodeChef/Java/SlidingWindow/Medium/COLGLF5_College_Life_5.java
+++ CodeChef/Java/SlidingWindow/Medium/COLGLF5_College_Life_5.java
@@ -9,4 +9,56 @@
  * Status: ACCEPTED
  */
 
-# cook your dish here
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        int t = scanner.nextInt(); 
+
+        while (t-- > 0) {
+            solve(scanner);
+        }
+        
+        scanner.close();
+    }
+
+    private static void solve(Scanner scanner) {
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+
+        long[] f = new long[(int)n];
+        long[] c = new long[(int)m];
+        for (int i = 0; i < n; i++) {
+            f[i] = scanner.nextLong(); 
+        }
+
+        for (int i = 0; i < m; i++) {
+            c[i] = scanner.nextLong(); 
+            }
+
+        int l1 = 0, l2 = 0;
+        int flag = 0;
+        int ans = 0;
+
+        while (l1 < n && l2 < m) {
+            if (f[l1] < c[l2]) {
+                if (flag == 1) {
+                    ans++;
+                    flag = 0;
+                }
+                l1++;
+            } else {
+                if (flag == 0) {
+                    ans++;
+                    flag = 1;
+                }
+                l2++;
+            }
+        }
+
+        ans++; // Count the last segment
+        System.out.println(ans); // Output the result
+    }
+}
