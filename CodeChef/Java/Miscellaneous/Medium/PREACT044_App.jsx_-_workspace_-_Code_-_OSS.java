0fe217e77e6cfcb750dcb96819620d61acd6989c
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 19:45:58 +0530

    Add CodeChef PREACT044 (App.jsx - workspace - Code - OSS) solution

---
+++ CodeChef/Java/Miscellaneous/Medium/PREACT044_App.jsx_-_workspace_-_Code_-_OSS.java
@@ -0,0 +1,23 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: PREACT044
+ * Problem: App.jsx - workspace - Code - OSS
+ * Problem Link: https://www.codechef.com/learn/course/react-js/CREACT010/problems/PREACT044
+ * Language: Java
+ * Concept: Miscellaneous
+ * Difficulty: Medium
+ * Course: React Js
+ * Module: CREACT010
+ * Status: ACCEPTED
+ */
+
+import { useState } from "react";
+
+function App() {
+  // Lifted state in parent
+  // Initialize state for tasks here (empty array)
+
+  // Function to add a new task
+  function addTask() {
+    // This should add new task to tasks array
+    // Prevent adding empty tasks
