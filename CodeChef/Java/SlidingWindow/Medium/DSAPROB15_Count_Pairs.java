e88c6d07916affa9caedb25a3161b30e9afb52eb
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:50:29 +0530

    Add CodeChef DSAPROB15 (Count Pairs Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/Java/SlidingWindow/Medium/DSAPROB15_Count_Pairs.java
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSAPROB15
+ * Problem: Count Pairs Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/DSAPROB15
+ * Language: Java
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
