d6a99a1266f90df8011691813081d6c1ee276abe
SaiTeja-Yelamareddy <saitejayelamareddy@gmail.com>
2026-09-24 18:32:02 +0530

    Add CodeChef DSCPPAS276 (Check Palindrome Practice Problem in Two Pointers and Sliding Window Technique) solution

---
+++ CodeChef/C++/SlidingWindow/Medium/DSCPPAS276_Check_Palindrome.cpp
@@ -0,0 +1,12 @@
+/*
+ * Platform: CodeChef
+ * Problem ID: DSCPPAS276
+ * Problem: Check Palindrome Practice Problem in Two Pointers and Sliding Window Technique
+ * Problem Link: https://www.codechef.com/practice/course/two-pointers-new/TWOPOINT02/problems/DSCPPAS276
+ * Language: C++
+ * Concept: SlidingWindow
+ * Difficulty: Medium
+ * Status: ACCEPTED
+ */
+
+# cook your dish here
